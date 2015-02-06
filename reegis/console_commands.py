e6ded1db4ec9d8ccac19fb2803a7b5a9b0f15f2b
timo <t.sochert@gmail.com>
2015-02-06 11:23:26 +0100

    setup for oop version

--- reegis/console_commands.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8
-
-import os
-os.system('cd /home/likewise-open/RL-INSTITUT/marena.puetzschler/' +
-'git_repository2/reegis/')
-
-os.system('python main_1.py "parameter_set"')
