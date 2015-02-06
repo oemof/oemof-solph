903ea1dbbd9a4faa82282ee79b24de34793f6f8c
steffen <steffen.peleikis@web.de>
2015-02-06 16:08:56 +0100

    ADT/graph.py: docstrings: added epydoc crossreferences

--- ADT/graph.py
+++ ADT/graph.py
@@ -4,13 +4,14 @@ Author: steffen Peleikis
 
 This module contains the Graph-ADT.
 
-Graphs can be used to implement grids.
+L{Graphs<Graph>} can be used to implement grids.
+They are made of L{vertices>Vertex>} and L{edges<Edge>}
 """
 
 
 class Edge(object):
     """
-    defines and edge between to vertices.
+    defines and edge between to L{vertices<vertex>}.
     inherit from here to implement further functionality.
     """
     def __init__(self, weight=0):
@@ -34,10 +35,10 @@ class Graph(object):
 
     def create_vertex(self, vertex_id, vertex_type):
         """
-        creates a new vertex and adds it to the graph.
+        creates a new L{Vertex} and adds it to the graph.
         :param vertex_id: the vertex id.
         :param vertex_type: the vertex-type
-        :return: the new vertex
+        :return: the new L{Vertex}
         """
         self.num_vertices += 1
         new_vertex = Vertex(vertex_id, vertex_type)
@@ -46,17 +47,17 @@ class Graph(object):
 
     def add_vertex(self, vertex):
         """
-        adds an existing vertex to the graph.
-        :param vertex: the vertex to be added.
+        adds an existing L{Vertex} to the graph.
+        :param vertex: the L{Vertex} to be added.
         """
         self.num_vertices += 1
         self.vertice_list[vertex.id] = vertex
 
     def get_vertex(self, vertex_id):
         """
-        finds a vertex with the given id in the graph.
-        :param vertex_id: the vertex id to find.
-        :return: the found vertex.
+        finds a L{Vertex} with the given id in the graph.
+        :param vertex_id: the L{Vertex} id to find.
+        :return: the found L{Vertex}.
         """
         if vertex_id in self.vertice_list:
             return self.vertice_list[vertex_id]
@@ -65,31 +66,31 @@ class Graph(object):
 
     def __contains__(self, vertex_id):
         """
-        checks if the vertex with the given id exists within the graph.
+        checks if the L{Vertex} with the given id exists within the graph.
         :param vertex_id: the id to check.
         :return: true if found, false if not.
         """
         return vertex_id in self.vertice_list
 
-    def add_edge(self, source_vertex, target_vortex, edge=Edge()):
+    def add_edge(self, source_vertex, target_vertex, edge=Edge()):
         """
-        adds an edge between to vertices.
-        if a vertex does not exist in the graph yet, it will be added.
-        :param source_vertex: the first vertex to connect the edge to.
-        :param target_vortex: the second vertex to connect the edge to.
-        :param edge: the edge-object to add. None if just an abstract connection with no further information
+        adds an L{Edge} between to L{vertices<Vertex>}.
+        if a L{Vertex} does not exist in the graph yet, it will be added.
+        :param source_vertex: the first L{Vertex} to connect the L{Edge} to.
+        :param target_vertex: the second v to connect the L{Edge} to.
+        :param edge: the L{Edge}-object to add. None if just an abstract connection with no further information
         will be added.
         """
         if source_vertex not in self.vertice_list:
             self.add_vertex(source_vertex)
-        if target_vortex not in self.vertice_list:
-            self.add_vertex(target_vortex)
-        self.vertice_list[source_vertex].addNeighbor(self.vertice_list[target_vortex], edge)
+        if target_vertex not in self.vertice_list:
+            self.add_vertex(target_vertex)
+        self.vertice_list[source_vertex].addNeighbor(self.vertice_list[target_vertex], edge)
 
     def get_vertices(self):
         """
-        get all vertices of this graph.
-        :return: list of vertices.
+        get all L{vertices<Vertex>} of this graph.
+        :return: list of L{vertices<Vertex>}.
         """
         return self.vertice_list.values()
 
@@ -103,7 +104,7 @@ class Graph(object):
 
 class Vertex(object):
     """
-    Implements a Vertex that can be added to a graph.
+    Implements a Vertex that can be added to a L{Graph}.
     """
     def __init__(self, vertex_id, vertex_type):
         """
@@ -117,10 +118,9 @@ class Vertex(object):
 
     def add_neighbor(self, vertex, edge=Edge()):
         """
-        adds an edge between this vertex and the vertex provided.
+        adds an L{Edge} between this vertex and the vertex provided.
         :param vertex: the vertex to add a connection to.
-        :param edge: the edge object. adds abstract edge if no edge provided.
-        :return:
+        :param edge: the L{Edge} object. adds abstract L{Edge} if no edge provided.
         """
 
         self.connected_to[vertex] = edge
@@ -148,8 +148,8 @@ class Vertex(object):
 
     def get_edge(self, vertex):
         """
-        get an edge bewteen this vertex and the vertex provided.
-        :param vertex: the vertex the get the edge to.
+        get an l{Edge} bewteen this vertex and the vertex provided.
+        :param vertex: the vertex the get the L{Edge} to.
         :return: the edge.
         """
         return self.connected_to[vertex]
