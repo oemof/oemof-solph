58f5b542f2e1507018c74b3e1b301ad352c751cd
timo <t.sochert@gmail.com>
2015-02-10 16:42:44 +0100

    nuclear_powerplant.py: added

---
+++ entities/producer/nuclear_powerplant.py
@@ -0,0 +1,7 @@
+from entities.producer.powerplant import Powerplant
+
+
+class NuclearPowerplant:
+
+    def __init__(self, entity_id, position, production, nominal_power):
+        super(NuclearPowerplant, self).__init__(self, entity_id, position, production, nominal_power)
