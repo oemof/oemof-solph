3037384f90b384961de96e7b551978414bfaef46
timo <t.sochert@gmail.com>
2015-02-10 16:43:28 +0100

    photovoltaic_powerplant.py: added

---
+++ entities/producer/photovoltaic_powerplant.py
@@ -0,0 +1,6 @@
+from entities.producer.powerplant import Powerplant
+
+class PhotovoltaicPowerplant:
+
+    def __init__(self, entity_id, position, production, nominal_power, ser):
+        super(self).__init__(self, entity_id, position, production, nominal_power)
