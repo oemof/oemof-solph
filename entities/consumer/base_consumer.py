8a8dd9e8d3f4ae379c68c052e57a392e20ea72e0
timo <t.sochert@gmail.com>
2015-02-10 15:31:32 +0100

    entities/consumer/base_consumer.py: implemented basic function for every consumer

--- entities/consumer/base_consumer.py
+++ entities/consumer/base_consumer.py
@@ -1,5 +1,15 @@
 from entities.base_entity import Entity
 
+
 class Consumer(Entity):
-    pass
+
+    def __init__(self, entity_id, consumption):
+        self._id = entity_id
+        self._consumption = consumption
+
+    def get_consumption(self):
+        return self._consumption
+
+    def set_consumption(self, consumption):
+        self._consumption = consumption
 
