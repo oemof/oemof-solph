d61d3dd25b123fbfe1da9d3ebc29b12769b5ecad
timo <t.sochert@gmail.com>
2015-02-10 18:33:18 +0100

    base_grid.py: first new code, not much

--- grids/base_grid.py
+++ grids/base_grid.py
@@ -1,12 +1,15 @@
 class Grid(object):
+    def __init__(self):
+        self.node_list = []
+
+    def add_node(self, node):
+        self.node_list.append(node)
+
+
+class Connection(object):
     pass
 
 
-class Line(object):
-    pass
-
-
-class Node(object):
-    pass
-
-
+class Node(Grid):
+    def __init__(self):
+        super(Node, self).__init__(self)
