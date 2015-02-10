633197637496a648e2195b912f492568a3508f32
timo <t.sochert@gmail.com>
2015-02-10 16:56:40 +0100

    photovoltaic_powerplant.py: introduced a hypothetic suncurve

--- entities/producer/photovoltaic_powerplant.py
+++ entities/producer/photovoltaic_powerplant.py
@@ -2,5 +2,6 @@ from entities.producer.powerplant import Powerplant
 
 class PhotovoltaicPowerplant:
 
-    def __init__(self, entity_id, position, production, nominal_power, ser):
+    def __init__(self, entity_id, position, production, nominal_power, sun_curve):
         super(self).__init__(self, entity_id, position, production, nominal_power)
+        self.sun_curve = sun_curve
