de49e06aa9dd6bf667ef4aaf7aeb3cb2a3e9fc03
timo <t.sochert@gmail.com>
2015-02-10 15:29:18 +0100

    entities/base_entity.py: implemented basic function for every entity

--- entities/base_entity.py
+++ entities/base_entity.py
@@ -1,4 +1,12 @@
 
 
 class Entity(object):
-    pass
+
+    def __init__(self, entity_id):
+        self._id = entity_id
+
+    def get_entity_id(self):
+        return self._id
+
+    def set_entity_id(self, entity_id):
+        self._id = entity_id
