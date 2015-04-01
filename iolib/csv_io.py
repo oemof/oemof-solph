d77e732d956c1ceee0a609bb2f0dae14e4f3d1ba
steffen <steffen.peleikis@web.de>
2015-04-01 15:23:55 +0200

    iolib/csv_io.py: simple csv reader. mostly empty

---
+++ iolib/csv_io.py
@@ -0,0 +1,25 @@
+"""
+This Module provides ready to go csv2object creation for (hopefully) every object type.
+
+TODO: Nope, it doesn't.
+
+"""
+import csv
+import oemof.iolib.config as cfg
+
+print("/iolib/csv_io.py imported")
+
+DELIMITER = ''
+ROOT = ''
+
+
+def load_from_csv(file_name):
+    reader = csv.DictReader(ROOT + file_name, delimiter=DELIMITER)
+    return reader
+
+
+def _load_config():
+    global DELIMITER
+    global ROOT
+    DELIMITER = cfg.get('csv', 'delimiter')
+    ROOT = cfg.get('csv', 'root')
