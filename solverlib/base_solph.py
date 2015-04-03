eb6abe944cd3e76cd128f8578ee0a712a27e61ba
steffen <steffen.peleikis@web.de>
2015-04-03 03:31:41 +0200

    solverlib/base_solph.py: added with basic calc for minimal example

--- solverlib/base_solph.py
+++ solverlib/base_solph.py
@@ -4,4 +4,23 @@ class Solph(object):
 
     def solph(self, grid):
 
-        return "result returned"
+        #calculates sum of all entities
+
+        entities = grid.get_all_entities()
+
+        l = 8760
+
+        r = [None] * l
+
+        for i in range(0,l):
+            t = 0
+            for id,e in entities.items():
+                try:
+                    t += e["output"][i]
+                except:
+                    continue
+            r[i] = t
+            i+=1
+
+        result = {"sum" : r}
+        return result
