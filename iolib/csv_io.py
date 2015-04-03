fc99883f2e9034042ff4fe5c6de9da16ca551335
steffen <steffen.peleikis@web.de>
2015-04-03 02:17:18 +0200

    iolib/csv_io.py: fixed minor bug

--- iolib/csv_io.py
+++ iolib/csv_io.py
@@ -14,15 +14,23 @@ ROOT = ''
 
 
 def load_dict_from_csv(file_name):
+    _load_config()
     with open(ROOT + file_name) as f:
         reader = csv.DictReader(f, delimiter=DELIMITER)
         d = {}
+        i=0
         for line in reader:
-            d[line["id"]] = dict(line)
+            try:
+                d[line["id"]] = dict(line)
+            except:
+                d[i] = dict(line)
+                i+=1
     return d
 
 
 def write_dict_to_csv(file_name, dict):
+    _load_config()
+
     with open(ROOT + file_name, 'wf') as f:
         d = dict.values()
         writer = csv.DictWriter(f, delimiter=DELIMITER, fieldnames=d[0].keys())
