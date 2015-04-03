b6a4ac81ee6db8566002a0ab3334ecc4955d85ef
steffen <steffen.peleikis@web.de>
2015-04-03 03:30:46 +0200

    rawdatalib/prices.py: added for minimal example

--- rawdatalib/prices.py
+++ rawdatalib/prices.py
@@ -1,1 +1,16 @@
 __package__ = "rawdatalib"
+
+import oemof.iolib.csv_io as csv
+
+class CsvPrice(object):
+
+    def __init__(self, scenario):
+        self.weather = csv.load_dict_from_csv("weather.csv")
+
+    def get_timeseries(self, parameter):
+        ts = {}
+
+        for k,v in self.weather.items():
+            ts[k] = v[parameter]
+
+        return ts
