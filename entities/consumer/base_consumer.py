934ccc3b8f8d3d40a6987bfe66943033a88e1ef8
S. Günther <gnn.code@gmail.com>
2015-06-01 01:42:16 +0200

    Delete everything not needed by minimal example
    The file 'src/powerplants.py' contains is runnable with a minimal
    example in its '__main__' stanza. In order to start from a clean slate
    where things can be added as they become necessary, this commit removes
    everything which is not required to run this "test".

--- entities/consumer/base_consumer.py
+++
@@ -1,8 +0,0 @@
-from oemof.entities.base_entity import Entity
-
-
-class Consumer(Entity):
-
-    def __init__(self, entity_id, position, consumption):
-        super(Consumer, self).__init__(entity_id, position)
-        self.consumption = consumption
--- entities/consumer/industrial_area.py
+++
@@ -1,7 +0,0 @@
-from oemof.entities.consumer.base_consumer import Consumer
-
-
-class IndustrialArea(Consumer):
-
-    def __init__(self, entity_id, position, consumption):
-        super(IndustrialArea, self).__init__(entity_id, position)
