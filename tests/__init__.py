b75277a1b075145ff17bd95693e2f08963ad817b
clemens <clemens.wingenbach@uni-flensburg.de>
2015-06-02 12:14:25 +0200

    Add the test library for the minimal example

---
+++ tests/__init__.py
@@ -0,0 +1,12 @@
+from nose.tools import raises
+
+import models as model
+import powerplants as plant
+
+class ModelsPowerplantsInteraction_Tests:
+
+    @raises(AttributeError)
+    def test_required(self):
+        plant.Photovoltaic(nominal_power = 0,
+                           model = model.Photovoltaic(["missing"]))
+                                                      
