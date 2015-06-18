46e5272b38a0517609912ea8d59aba075ba23dba
S. Günther <gnn.code@gmail.com>
2015-06-18 02:08:59 +0200

    Add example of how to use the ORM
    The db 'module' added in this patch does not contain any functionality
    but for now just acts as a script demonstrating how SQLAlchemy could be
    used. In order to run it one would have to have a fitting database and
    change the connection string accordingly.
    A good way to move on would probably be to create a mock up CSV file
    with few lines of imaginary data, read that into an in memory database
    and let the example work on that. This setup could then also be used to
    write tests for the interaction between ORM and 'Components'.

---
+++ src/db.py
@@ -0,0 +1,43 @@
+from sqlalchemy import (create_engine, text, Column, MetaData, Numeric, String,
+                        Table)
+from sqlalchemy.orm import mapper, sessionmaker
+
+import components
+
+if __name__ == "__main__":
+  coal = components.Bus(uid = "Coal", type = "coal")
+  power = components.Bus(uid = "Power", type = "electricity")
+
+  # And example of how to connect to a PostgreSQL database
+  engine = create_engine("postgresql+psycopg2://USER:PASSOWRD@HOST/DATABASE")
+  Session = sessionmaker(bind=engine)
+  session = Session()
+
+  # This example is for a spefic table from the "auswertung" database
+  cpps = Table("geo_power_plant_bnetza_2014", MetaData(schema="vn"),
+               Column("bnetza_id", String(), primary_key=True),
+               Column("inbetriebnahme", String()),
+               Column("energietraeger", String()),
+               Column("el_nennleistung",  Numeric(10,1)))
+
+  mapper(components.Transformer, cpps, properties={
+      "uid": cpps.c.bnetza_id,
+      "year": cpps.c.inbetriebnahme,
+      "type": cpps.c.energietraeger,
+      "eta_el": cpps.c.el_nennleistung
+  })
+
+  old_coal_plants = session.query(components.Transformer
+      ).filter(components.Transformer.year < "1950"
+      ).filter(components.Transformer.type.like("%kohle%")
+      ).all()
+
+  for ocp in old_coal_plants:
+    print("Transformer: id => {0}, built => {1}, type => {2} Nel = {3}".format(
+          ocp.uid, ocp.year, ocp.type, ocp.eta_el))
+
+
+  for ocp in old_coal_plants:
+      ocp.inputs = [coal]
+      ocp.outputs = [power]
+
