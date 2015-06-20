157a4a429ffe28f9481319edc7ee2992b2a7af9c
Hilpert <simon.hilpert@fh-flensburg.de>
2015-06-20 15:08:34 +0200

    Add solve_opt_model() and replace pyomo.Set() objects by list/dicts
    with solve_opt_model() a created pyomo instance can be solved. Note: solver
    must be installed.
    
    I looked at building time of a pyomo model, which was really long, so I
    tried to replace set object of pyomo be simple lists for simple sets and
    dicts for indexed sets. It turned out, that building the model was over ten
    times faster. (hope this is right)

--- src/optimization_model.py
+++ src/optimization_model.py
@@ -2,26 +2,36 @@
 import pyomo.environ as po
 
 
-def opt_model(busses, s_transformers, s_chps):
+def opt_model(entities, timesteps=[t for t in range(1,8760)]):
+  """
+  :param entities: dictionary containing all entities grouped by classtypes
+  """
+
+  busses = entities['busses']
+  s_transformers = entities['s_transformers']
+  s_chps = entities['s_chps']
   # create pyomo model instance
   m = po.ConcreteModel()
 
   # create pyomo sets
-  m.busses = po.Set(initialize=[b.uid for b in busses])
-  m.s_transformers = po.Set(initialize=[t.uid for t in s_transformers])
-  m.s_chps = po.Set(initialize=[t.uid for t in s_chps])
+  # timesteps
+  m.timesteps = timesteps
+  # entities
+  m.busses = [b.uid for b in busses]
+  m.s_transformers = [t.uid for t in s_transformers]
+  m.s_chps = [t.uid for t in s_chps]
   m.edges = po.Set(dimen=2, initialize=edges)
 
-  # create variable for edges
-  m.w = po.Var(m.edges)
+  # create variable for edges all >= 0 ?
+  m.w = po.Var(m.edges, m.timesteps, within=po.NonNegativeReals)
 
   ## bus balance forall b in busses
-  def bus_rule(m, e):
+  def bus_rule(m, e, t):
     expr = 0
-    expr += -sum(m.w[(i,j)] for (i,j) in m.edges if i==e)
-    expr += +sum(m.w[(i,j)] for (i,j) in m.edges if j==e)
+    expr += -sum(m.w[(i,j),t] for (i,j) in m.edges if i==e)
+    expr += +sum(m.w[(i,j),t] for (i,j) in m.edges if j==e)
     return(expr, 0)
-  m.bus_constr = po.Constraint(m.busses, rule=bus_rule)
+  m.bus_constr = po.Constraint(m.busses, m.timesteps, rule=bus_rule)
 
 
   # simple transformer model containing the constraints for simple transformers
@@ -30,58 +40,76 @@ def opt_model(busses, s_transformers, s_chps):
     :param m: pyomo model instance
     """
     # temp set with input uids for every simple chp e in s_transformers
-    # (does not need to be a pyomo object of class Set(), but can be a dict m.I
-    # as attribute of m as well. Depends of how it is used again. Same for m.O)
-    m.I = po.Set(m.s_transformers,
-                  initialize={t.uid:[t.inputs[0].uid] for t in s_transformers})
+    I ={t.uid:t.inputs[0].uid for t in s_transformers}
     # set with output uids for every simple transformer e in s_transformers
-    m.O = po.Set(m.s_transformers,
-                  initialize={t.uid:[t.outputs[0].uid] for t in s_transformers})
-    m.eta = {t.uid:t.eta for t in s_transformers}
-    def eta_rule(m, e):
+    O = {t.uid:t.outputs[0].uid for t in s_transformers}
+    eta = {t.uid:t.eta for t in s_transformers}
+    def eta_rule(m, e, t):
       expr = 0
-      expr += m.w[m.I[e], e] * m.eta[e]
-      expr += - m.w[e, m.O[e]]
+      expr += m.w[I[e], e, t] * eta[e]
+      expr += - m.w[e, O[e], t]
       return(expr,0)
-    m.s_transformer_eta_constr = po.Constraint(m.s_transformers, rule=eta_rule)
+    m.s_transformer_eta_constr = po.Constraint(m.s_transformers, m.timesteps,
+                                               rule=eta_rule)
     # to avoid pyomo warnings if m.I is already defined for other components
-    del m.I, m.O
 
   # simple chp model containing the constraints for simple chps
   def simple_chp_model(m):
     """
     """
     # temp set with input uids for every simple chp e in s_chps
-    m.I = po.Set(m.s_chps,
-                  initialize={t.uid:[t.inputs[0].uid] for t in s_chps})
+    I = {t.uid:t.inputs[0].uid for t in s_chps}
     # set with output uids for every simple chp e in s_chps
-    m.O = po.Set(m.s_chps, ordered=True,
-                  initialize={t.uid : [o.uid for o in t.outputs[:]]
-                              for t in s_chps})
+    O = {t.uid : [o.uid for o in t.outputs[:]] for t in s_chps}
     # constraint for transformer energy balance
-    def eta_rule(m, e):
+    def eta_rule(m, e, t):
       expr = 0
-      expr += m.w[m.I[e], e]
-      expr += -sum(m.w[e, o] for o in m.O[e]) / 0.8
+      expr += m.w[I[e], e, t]
+      expr += -sum(m.w[e, o, t] for o in O[e]) / 0.8
       return(expr,0)
-    m.s_chp_eta_constr = po.Constraint(m.s_chps, rule=eta_rule)
+    m.s_chp_eta_constr = po.Constraint(m.s_chps, m.timesteps, rule=eta_rule)
 
     # additional constraint for power to heat ratio of simple chp components
-    def power_to_heat_rule(m, e):
+    def power_to_heat_rule(m, e, t):
       expr = 0
-      expr += m.w[e, m.O[e][0]]
-      expr += -m.w[e, m.O[e][1]] * 0.6
+      expr += m.w[e, O[e][0], t]
+      expr += -m.w[e, O[e][1], t] * 0.6
       return(expr,0)
-    m.s_chp_pth_constr = po.Constraint(m.s_chps, rule=power_to_heat_rule)
+    m.s_chp_pth_constr = po.Constraint(m.s_chps, m.timesteps,
+                                       rule=power_to_heat_rule)
+  def objective(m):
 
-    # to avoid pyomo warnings if m.I is already defined for other components
-    del m.I, m.O
+    def obj_rule(m):
+      expr = 0
+      expr += sum(m.w[i,j,t] for (i,j) in m.edges for t in m.timesteps)
+      return(expr)
+    m.objective = po.Objective(rule=obj_rule)
 
   # "call" the models to add the constraints to opt-problem
   simple_chp_model(m)
   simple_transformer_model(m)
+  objective(m)
 
   return(m)
+
+def solve_opt_model(model, solver='glpk',
+                      options={'stream':False}):
+ """
+ :param model: pyomo concreteModel() instance
+ :param str solver: solver specification as string 'glpk','gurobi','clpex' ...
+ """
+ from pyomo.opt import SolverFactory
+ # create model instance
+ instance = model.create()
+ # solve instance
+ opt = SolverFactory(solver, solver_io='lp')
+ # store results
+ results = opt.solve(instance, tee=options['stream'])
+ # load results back in instance
+ instance.load(results)
+
+ return(instance)
+
 
 if __name__ == "__main__":
   # create energy system components
@@ -95,9 +123,10 @@ if __name__ == "__main__":
   t4 = cp.Transformer(uid="t4",inputs=[bus1], outputs=[bus2,bus3])
   t5 = cp.SimpleTransformer(uid="Boiler",inputs=[bus1],outputs=[bus3], eta=0.9)
   # store entities of same class in lists
-  busses = [bus1, bus2, bus3]
-  s_transformers = [t1, t2, t5]
-  s_chps = [t3, t4]
+
+  entities = {'busses':[bus1, bus2, bus3],
+              's_transformers':[t1, t2, t5],
+              's_chps':[t3, t4]}
 
   # should be calculated automatically of course
   edges = [('b1','t1'),('b1','t2'),('b1','t3'), ('t1','b2'),('t2','b2'),
@@ -105,5 +134,25 @@ if __name__ == "__main__":
            ('t4','b3'),('b1','Boiler'),('Boiler','b3')]
 
   # create optimization model
-  om = opt_model(busses=busses,s_transformers=s_transformers, s_chps=s_chps)
-  om.pprint()
+  from time import time
+
+  #print('building model')
+  print('Building model...')
+  t0 = time()
+  om = opt_model(entities=entities)
+
+  t1= time()
+  building_time = t1 - t0
+  print('Building Time:', building_time)
+
+  print('Solving model...')
+  # solve model
+  t0 = time()
+  instance = solve_opt_model(model=om, solver='gurobi',
+                             options={'stream':True})
+  t1 = time()
+  solving_time = t1 - t0
+  print('Solving time:', solving_time)
+
+  #for idx in instance.w:
+  #  print('Edge:'+str(idx), ', weight:'+str(instance.w[idx].value))
