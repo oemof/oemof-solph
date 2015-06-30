8babf275c9b84ab517d0c9e204055c6c864caa31
CordKaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2015-06-30 09:33:34 +0200

    Add class SimpleTransport

--- src/components.py
+++ src/components.py
@@ -161,7 +161,21 @@ class Transport(Component):
       raise ValueError("Transport must have exactly one output.\n" +
                        "Got: {0!r}".format([str(x) for x in self.outputs]))
 
+class SimpleTransport(Transport):
+  """
+  Simple Transport connects two busses with a constant efficiency
+  """
+  def __init__(self,**kwargs):
+    """
+    :param eta: eta as constant efficiency for simple transport
+    """
+    super().__init__(**kwargs)
+    self.eta = kwargs.get('eta', None)
 
+    if(self.in_max is None and self.out_max is not None):
+      self.in_max = self.out_max / self.eta
+    if(self.out_max is None and self.in_max is not None):
+      self.out_max = self.in_max * self.eta
 
 
 if __name__ == "__main__":
