ddd5728e112726ee6809807725f2b24602edb006
uwe_ <uwe.krien@rl-institut.de>
2015-07-24 19:20:39 +0200

    add the empty demand classes

---
+++ src/demand.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 24 19:11:38 2015
+
+@author: uwe
+"""
+
+class electrical_demand():
+    # Das Gebäudeprofil kommt aus der Datenbank einer Datei oder einer anderen
+    # Funktion.
+    # Zunächst wir die normalisierte Tabelle für alle Regionen erzeugt.
+    pass
+
+class heat_demand():
+    # Das Gebäudeprofil kommt aus der Datenbank einer Datei oder einer anderen
+    # Funktion.
+    pass
