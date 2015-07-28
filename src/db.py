f5ad4f969f34492ecdb687ba5bb95f32b3de8f99
uwe_ <uwe.krien@rl-institut.de>
2015-07-28 17:52:31 +0200

    modify db.py to get connection

--- src/db.py
+++ src/db.py
@@ -1,14 +1,53 @@
-from sqlalchemy import (create_engine, text, Column, MetaData, Numeric, String,
-                        Table)
+from sqlalchemy import (create_engine, Column, MetaData, Numeric, String,
+                        Table, engine)
 from sqlalchemy.orm import mapper, sessionmaker
 import keyring
 
 import config as cfg
-import components
+from network.entities import components
+
+
+CONN = None
+
+
+def __engine__():
+    engine = create_engine(
+        "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}".format(
+            user=cfg.get("postGIS", "username"),
+            passwd=keyring.get_password(
+                cfg.get("postGIS", "database"),
+                cfg.get("postGIS", "username")),
+            host=cfg.get("postGIS", "host"),
+            db=cfg.get("postGIS", "database"),
+            port=int(cfg.get("postGIS", "port"))))
+    return engine
+
+
+def __internal_connection__(engine):
+
+    conn = engine.connect()
+#    Session = sessionmaker(bind=engine)
+#    session = Session()
+    return conn
+
+
+def connection():
+    return CONN
+
+
+def db_engine():
+    return ENG
+
+
+if not isinstance(CONN, engine.base.Connection):
+    print(isinstance(CONN, engine.base.Connection))
+    ENG = __engine__()
+    CONN = __internal_connection__(ENG)
 
 if __name__ == "__main__":
-    coal = components.Bus(uid="Coal", type="coal")
-    power = components.Bus(uid="Power", type="electricity")
+    print(connection())
+#    coal = components.Bus(uid="Coal", type="coal")
+#    power = components.Bus(uid="Power", type="electricity")
 
     # And example of how to connect to a PostgreSQL database
     engine = create_engine(
@@ -30,6 +69,7 @@ if __name__ == "__main__":
         Column("inbetriebnahme", String()),
         Column("energietraeger", String()),
         Column("el_nennleistung",  Numeric(10, 1)))
+    print(cpps)
 
     mapper(components.Transformer, cpps, properties={
         "uid": cpps.c.bnetza_id,
@@ -50,6 +90,6 @@ if __name__ == "__main__":
         print("Transformer => id: {0}, built: {1}, type: {2} Nel: {3}".format(
             ocp.uid, ocp.year, ocp.type, ocp.eta_el))
 
-    for ocp in old_coal_plants:
-        ocp.inputs = [coal]
-        ocp.outputs = [power]
+#    for ocp in old_coal_plants:
+#        ocp.inputs = [coal]
+#        ocp.outputs = [power]
