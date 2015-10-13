f8dd23f75897c1bbb70b2ae8ac4b241fcc7aa6be
uwe <uwe.krien@rl-institut.de>
2015-10-13 19:13:36 +0200

    change doc style

--- doc/conf.py
+++ doc/conf.py
@@ -107,16 +107,17 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'agogo'
+#html_theme = 'agogo'
+html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-	"sidebarwidth": "25em",
-	"documentwidth":"50em",
-	"pagewidth": "75em",
-	}
+#html_theme_options = {
+#	"sidebarwidth": "25em",
+#	"documentwidth":"50em",
+#	"pagewidth": "75em",
+#	}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
