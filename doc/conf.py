24d9510262cf6405fb113b24bf34af895c65dba4
Stephan Günther <gnn.code@gmail.com>
2015-10-25 21:09:08 +0100

    Fix: load extension required for `.. autosummary`

--- doc/conf.py
+++ doc/conf.py
@@ -30,6 +30,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
     'sphinx.ext.viewcode',
     'sphinx.ext.pngmath',
     'numpydoc' 
