adad3a5c329d98abee7d1e42700b8723e8c5422c
Caroline Moeller <caroline.moeller@rl-institut.de>
2015-10-30 18:22:54 +0100

    adjust example file and add results print

--- examples/example_app_sc160.py
+++ examples/example_app_sc160.py
@@ -26,7 +26,7 @@ from oemof.core.network.entities.components import transports as transport
 import pandas as pd
 
 data = pd.read_csv("example_data_sc160.csv", sep=",")
-timesteps = [t for t in range(24)]
+timesteps = [t for t in range(20)]
 
 # emission factors in t/MWh
 em_lig = 0.111 * 3.6
@@ -35,7 +35,9 @@ em_gas = 0.0556 * 3.6
 em_oil = 0.0750 * 3.6
 
 # resources
-bgas = Bus(uid="gas", type="gas", price=70, excess=False)
+#bgas = Bus(uid="gas", type="gas", price=70, excess=False)
+#bgas = Bus(uid="gas", type="gas", price=70, sum_out_limit=0)
+bgas = Bus(uid="gas", type="gas", price=70)
 
 # electricity and heat
 b_el = Bus(uid="b_el", type="el", excess=True)
@@ -45,16 +47,17 @@ wind_on = source.FixedSource(uid="wind_on",outputs=[b_el],
                                 val=data['wind'],
                                 cap_max={b_el.uid: 1000000},
                                 out_max={b_el.uid: 1000000},
+                                add_out_limit=0,
                                 capex=1000,
                                 opex_fix=20,
                                 lifetime=25,
-                                add_out_limit=0,
                                 crf=0.08)
 #                                wacc={b_el.uid: 0.07})
 
 pv = source.FixedSource(uid="pv", outputs=[b_el], val=data['pv'],
                            cap_max={b_el.uid: 582000},
                            out_max={b_el.uid: 582000},
+                           add_out_limit=0,
                            capex=900,
                            opex_fix=15,
                            lifetime=25,
@@ -68,7 +71,7 @@ demand_el = sink.Simple(uid="demand_el", inputs=[b_el],
 # Simple Transformer for b_el
 pp_gas = transformer.Simple(uid='pp_gas', inputs=[bgas], outputs=[b_el],
                             in_max={bgas.uid: None},
-                            out_max={b_el.uid: 41000}, eta=[0.45])
+                            out_max={b_el.uid: 10e10}, eta=[0.58])
 
 # chp (not from BNetzA) eta_el=0.3, eta_th=0.3
 #pp_chp = transformer.CHP(uid='pp_chp', inputs=[bgas], outputs=[b_el, b_th],
@@ -80,15 +83,18 @@ pp_gas = transformer.Simple(uid='pp_gas', inputs=[bgas], outputs=[b_el],
 # storage
 sto_simple = transformer.Storage(uid='sto_simple', inputs=[b_el],
                                  outputs=[b_el],
-                                 eta_in=0.8, eta_out=0.8, cap_loss=0.00,
-                                 opex_fix=35, opex_var=2, co2_var=None,
+                                 eta_in=1, eta_out=0.8, cap_loss=0.00,
+                                 opex_fix=35, opex_var=10e10, co2_var=None,
                                  capex=1000,
-                                 cap_initial=100000,
-                                 cap_max=10000,
-                                 in_max={b_el.uid: 100000},
-                                 out_max={b_el.uid: 200000},
+                                 cap_max=0,
+                                 cap_initial=300000,
                                  c_rate_in = 1/6,
                                  c_rate_out = 1/6)
+
+
+#                                 in_max={b_el.uid: 100000},
+#                                 out_max={b_el.uid: 200000},
+
 #sto_simple = transformer.Storage(uid='sto_simple', inputs=[b_el],
 #                                 outputs=[b_el],
 #                                 eta_in=1, eta_out=0.8,
@@ -128,6 +134,7 @@ if __name__ == "__main__":
         import matplotlib.cm as cm
 
         plot_data = renew_sources+transformers+storages
+
 #        plot_data = renew_sources+transformers+transports+storages
 
         # data preparation
@@ -139,20 +146,32 @@ if __name__ == "__main__":
                 y.append(c.results['out'][bus_to_plot])
                 labels.append(c.uid)
 
-        # plotting
+        # plot production
         fig, ax = plt.subplots()
+#        sp = ax.stackplot(x, y,
+#                          colors=cm.rainbow(np.linspace(0, 1, len(plot_data))),
+#                          linewidth=0)
+
         sp = ax.stackplot(x, y,
-                          colors=cm.rainbow(np.linspace(0, 1, len(plot_data))))
+                          colors=('yellow', 'blue', 'grey', 'red'),
+                          linewidth=0)
+
         proxy = [mpl.patches.Rectangle((0, 0), 0, 0,
                                        facecolor=
                                        pol.get_facecolor()[0]) for pol in sp]
-        # demand
+        # plot demand
         ax.step(x, demand_el.results['in'][demand_el.inputs[0].uid],
                 c="black", lw=2)
+
         # storage soc (capacity at every timestep)
-        ax.step(x, sto_simple.results['cap'], c='green', lw=2.4)
-        # storage input
-        ax.step(x, sto_simple.results['in'][sto_simple.inputs[0].uid], c='green',ls='--', lw=2)
+#        ax.step(x, sto_simple.results['cap'], c='green', lw=2.4)
+
+        # plot storage input
+        ax.step(x, (np.asarray(
+            sto_simple.results['in'][sto_simple.inputs[0].uid])
+            + np.asarray(
+                demand_el.results['in'][demand_el.inputs[0].uid])),
+            c='red', ls='-', lw=2)
 
         ax.legend(proxy, labels)
         ax.grid()
@@ -161,5 +180,34 @@ if __name__ == "__main__":
         ax.set_ylabel('Power in kW')
         ax.set_title('Dispatch')
 
+    def print_results(bus_to_print):
+        import numpy as np
+
+        print_data = renew_sources+transformers+storages
+
+        # production results
+        sum_production = np.array([])
+        for c in print_data:
+            print(c)
+            res = np.asarray(c.results['out'][bus_to_print])
+            sum_production = np.append(sum_production, res)
+            print('sum: ', res.sum())
+            print('maximum value: ', res.max())
+
+        # storage state and capacity
+        storage_soc = np.asarray(sto_simple.results['cap'])
+        print('storage capacity: ', storage_soc.max())
+
+        # storage load
+        storage_load = np.asarray(
+            sto_simple.results['in'][sto_simple.inputs[0].uid])
+        print('sum storage load: ', storage_load.sum())
+        print('maximum storage load: ', storage_load.max())
+
+        # autarky degree
+        print(sum_production.sum())
+
     plot_dispatch('b_el')
     plt.show()
+
+    print_results('b_el')
