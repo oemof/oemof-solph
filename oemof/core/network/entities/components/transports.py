1042e7f55b8e36cd0a0171cf93939f872ae0d455
Stephan Günther <gnn.code@gmail.com>
2015-11-12 16:35:19 +0100

    Add `in_max` and `out_max` to `Simple(Transport)`

--- oemof/core/network/entities/components/transports.py
+++ oemof/core/network/entities/components/transports.py
@@ -10,9 +10,17 @@ class Simple(Transport):
     def __init__(self, **kwargs):
         """
         :param eta: eta as constant efficiency for simple transport
+        :param in_max: Maximum input the transport can handle, in $MW$.
+        :param out_max: Maximum output which can possibly be obtained when
+                        using the transport, in $MW$.
         """
+        # TODO: Add check for what combination of parameters is given and
+        #       calculate the missing ones accordingly. Also write down the
+        #       relationshiph between the three parameters in the doctstring.
         super().__init__(**kwargs)
         self.eta = kwargs.get('eta', None)
+        self.in_max = kwargs.get('in_max')
+        self.out_max = kwargs.get('out_max')
 
         if(self.in_max is None and self.out_max is not None):
             self.in_max = self.out_max / self.eta
