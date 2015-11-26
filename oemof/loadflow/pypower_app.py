caf9eb79d0aa66eb6d339225cb4fc952f94492d5
clemens <clemens.wingenbach@uni-flensburg.de>
2015-11-26 14:57:20 +0100

    Adjust import of oemof_modules to v0.0.1

--- oemof/loadflow/pypower_app.py
+++ oemof/loadflow/pypower_app.py
@@ -5,8 +5,8 @@ Example application that uses pypower for load flow simulation.
 pypo stands for an abbreviation for pypower
 Mainly developed by Christian Fleischer
 """
-from network.entities import Bus
-from network.entities.components import Transport
+from oemof.core.network.entities import Bus
+from oemof.core.network.entities.components import Transport
 from numpy import array
 from pypower.api import  runpf, printpf
 
@@ -36,7 +36,7 @@ class BusPypo(Bus):
         self.zone = kwargs.get("zone", None)
         self.vmax = kwargs.get("vmax", None)
         self.vmin = kwargs.get("vmin", None)
-        
+
         # Generator Data attributes
         #self.gen_bus = kwargs("gen_bus", None) #could possibly be replaced with bus_id
 #        self.gen_bus = kwargs.get("gen_bus",None)
@@ -52,8 +52,8 @@ class BusPypo(Bus):
 
     def __iter__(self):
         return iter(self.bus_id)
-        
-        
+
+
 # define a new transport class for the grid
 class CablePypo(Transport):
     """
@@ -63,7 +63,7 @@ class CablePypo(Transport):
         """
         here come information on the attributes and methods
         """
-    
+
         super().__init__(**kwargs)
         # Branch Data attributes
         self.f_bus = kwargs.get('f_bus', None)
@@ -78,17 +78,17 @@ class CablePypo(Transport):
         self.shift = kwargs.get('shift', None)
         self.br_status = kwargs.get('br_status', None)
 
- 
+
 #  bus initialization
-b_el1 = BusPypo(uid="b_el1", type="el", bus_id = 1, bus_type = 1, PD = 30, QD = 10, GS = 0, BS =0 , bus_area = 1, VM =1 , VA =0 , base_kv = 135, zone = 1, vmax = 1.05, vmin = 0.95 ) 
-b_el2 = BusPypo(uid="b_el2", type="el", bus_id = 2, bus_type = 2, PD = 30, QD = 15, GS = 0, BS =0 , bus_area = 1, VM =1 , VA =0 , base_kv = 135, zone = 1, vmax =1.1 , vmin =0.95 ) 
-b_el3 = BusPypo(uid="b_el3", type="el", bus_id = 3, bus_type = 3, PD = 0, QD = 0, GS = 0, BS = 0, bus_area = 1, VM = 1, VA = 0, base_kv = 135, zone = 1, vmax = 1.05, vmin = 0.95 ) 
+b_el1 = BusPypo(uid="b_el1", type="el", bus_id = 1, bus_type = 1, PD = 30, QD = 10, GS = 0, BS =0 , bus_area = 1, VM =1 , VA =0 , base_kv = 135, zone = 1, vmax = 1.05, vmin = 0.95 )
+b_el2 = BusPypo(uid="b_el2", type="el", bus_id = 2, bus_type = 2, PD = 30, QD = 15, GS = 0, BS =0 , bus_area = 1, VM =1 , VA =0 , base_kv = 135, zone = 1, vmax =1.1 , vmin =0.95 )
+b_el3 = BusPypo(uid="b_el3", type="el", bus_id = 3, bus_type = 3, PD = 0, QD = 0, GS = 0, BS = 0, bus_area = 1, VM = 1, VA = 0, base_kv = 135, zone = 1, vmax = 1.05, vmin = 0.95 )
 
 busses = [b_el1, b_el2, b_el3]
 
 # generator initialization
-g_el1 = BusPypo(uid="generator1", bus_id = 2, PG =30, QG =0, qmax = 62.5, qmin = -15, VG = 1, mbase = 100, gen_status = 1, pmax = 60, pmin = 0 ) 
-g_el2 = BusPypo(uid="generator2", bus_id = 3, PG = 0, QG =0, qmax = 0 , qmin = -15, VG = 1, mbase = 100, gen_status = 1, pmax = 0, pmin = 0 ) 
+g_el1 = BusPypo(uid="generator1", bus_id = 2, PG =30, QG =0, qmax = 62.5, qmin = -15, VG = 1, mbase = 100, gen_status = 1, pmax = 60, pmin = 0 )
+g_el2 = BusPypo(uid="generator2", bus_id = 3, PG = 0, QG =0, qmax = 0 , qmin = -15, VG = 1, mbase = 100, gen_status = 1, pmax = 0, pmin = 0 )
 
 generators = [g_el1,g_el2]
 
@@ -98,7 +98,7 @@ cable2 = CablePypo(uid="cable2", inputs=[b_el2], outputs=[b_el3], f_bus = 1, t_b
 cable3 = CablePypo(uid="cable3", inputs=[b_el3], outputs=[b_el1], f_bus = 2, t_bus = 3, br_r = 0.06, br_x = 0.17, br_b = 0.02, rate_a = 65, rate_b = 65, rate_c = 65, tap = 0, shift = 0, br_status = 1)
 
 branches = [cable1, cable2, cable3]
-#make bus array 
+#make bus array
 my_bus_array = []
 for bus in busses:
     my_bus_array.append([bus.bus_id, bus.bus_type, bus.PD, bus.QD, bus.GS, bus.BS, bus.bus_area, bus.VM, bus.VA, bus.base_kv, bus.zone, bus.vmax, bus.vmin])
@@ -117,10 +117,10 @@ for cables in branches:
 
 
 #print (my_bus_array)
-#create casefile     
+#create casefile
 def examplecase():
     """Power flow data for 3 bus, 2 generators.
-   
+
 
     Based on data from ...
     """
@@ -128,14 +128,14 @@ def examplecase():
 
     ##-----  Power Flow Data  -----##
     ## system MVA base
-    ppc["baseMVA"] = 100.0  
+    ppc["baseMVA"] = 100.0
     ppc["bus"] = array(my_bus_array)
-    ppc ["gen"] = array(my_gen_array)  
+    ppc ["gen"] = array(my_gen_array)
     ppc["branch"] = array(my_branch_array)
-    
+
 
     return ppc
-    
+
 ppc = examplecase()
 
 results = runpf(ppc)
