378a12b7e99f90dc341a398ed9926d4e3d24efb9
uwe <uwe.krien@rl-institut.de>
2015-12-02 15:42:50 +0100

    add requirements to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@
 
 from distutils.core import setup
 
-setup( name='oemof_base'
-     , version='0.0.1'
-     , description='The open energy modelling framework'
-     , package_dir = {'oemof': 'oemof'}
-     )
-
+setup(name='oemof_base',
+      version='0.0.1',
+      description='The open energy modelling framework',
+      package_dir={'oemof': 'oemof'},
+      install_requires=['numpy >= 1.7.0',
+                        'pandas >= 0.13.1',
+                        'pyomo >= 4.0.0',
+                        'shapely >= 1.4.0']
+      )
