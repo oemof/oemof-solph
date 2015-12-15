3a497225f661a264ca11c596537c7ae85b52e41b
clemens <clemens.wingenbach@uni-flensburg.de>
2015-12-15 14:26:39 +0100

    PEP08

--- oemof/loadflow/pypower_app.py
+++ oemof/loadflow/pypower_app.py
@@ -12,7 +12,8 @@ import pandas as pd
 import pg8000
 
 # TODO: con = sqlalchemy.create_engine('postgresql+pg8000://postgres:user123@localhost:5432/oemof')
-con = sqlalchemy.create_engine('postgresql+pg8000://student:user123@localhost:5432/oemof')
+con = sqlalchemy.create_engine('postgresql+pg8000://'+
+                               'student:user123@localhost:5432/oemof')
 
 #For conventional nearest neoghbour:
 #  SELECT g1.gid As gref_gid,
@@ -72,8 +73,9 @@ sql = '''SELECT gid,
 branch_data = pd.read_sql_query(sql, con)
 
 # choose only branches for existing buses
-branch_sub = [x&y for (x,y) in zip(list(branch_data.f_bus.isin(bus_data.bus_i)),
-                                  list(branch_data.t_bus.isin(bus_data.bus_i)))]
+branch_sub = [x & y for (x, y) in
+              zip(list(branch_data.f_bus.isin(bus_data.bus_i)),
+                  list(branch_data.t_bus.isin(bus_data.bus_i)))]
 branch_data = branch_data[branch_sub]
 
 # initailize positions for plotting
@@ -85,13 +87,13 @@ buses = {}
 # choose ref buses, the rest will be initialized as PQ-bus
 ref_buses = ["8205", "3227", "4075", "1978", "3285", "7766", "7847", "7879"]
 for index, row in bus_data.iterrows():
-    bus_type=1
+    bus_type = 1
     if row['bus_i'] in ref_buses:
         bus_type = 3
     bus_temp = BusPypo(uid=row['bus_i'], type="el",
                        bus_id=int(row['bus_i']),
                        bus_type=bus_type,
-                       PD = 200, QD = 0,
+                       PD=200, QD=0,
                        GS = 0, BS =0 , bus_area = 1, VM =1 , VA = 0,
                        base_kv = 380, zone = 1, vmax = 1.1, vmin = 0.9)
     buses[bus_temp.uid] = bus_temp
@@ -105,11 +107,12 @@ for index, row in branch_data.iterrows():
                              outputs=[buses[row['t_bus']]],
                              f_bus=buses[row['f_bus']].bus_id,
                              t_bus=buses[row['t_bus']].bus_id,
-                             br_r = 0.02, br_x = 0.06,
-                             br_b = 0.03, rate_a = 130, rate_b = 130, rate_c = 130,
+                             br_r = row["br_r"], br_x = row['br_x'],
+                             br_b = row['br_b'], rate_a = 130, rate_b = 130, rate_c = 130,
                              tap = 0, shift = 0, br_status = 1)
     branches[branch_temp.uid] = branch_temp
     positions[branch_temp] = [row['lon'], row['lat']]
+
 
 # function to initialize dummy generators
 def create_dummy_gen(bus):
@@ -139,5 +142,6 @@ energysystem.plot_as_graph(labels=False, positions=positions)
 # simulate loadflow
 # if resultsfile already exists an error will be raised
 # without the resultsfile argument the output will be in the console
-results = energysystem.simulate_loadflow(max_iterations=20,
-                                         resultsfile="app_results.txt")
+results = energysystem.simulate_loadflow(max_iterations=20)
+
+# TODO:                                          resultsfile="app_results.txt")
