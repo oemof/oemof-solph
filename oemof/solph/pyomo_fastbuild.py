e3b7f8cf8ef9239cb2d12870b2eececbc1356be3
Hilpert <simon.hilpert@fh-flensburg.de>
2016-01-25 10:04:38 +0100

    Add mutate_variable to solph module

--- oemof/solph/pyomo_fastbuild.py
+++ oemof/solph/pyomo_fastbuild.py
@@ -9,6 +9,7 @@
 
 
 from pyomo.environ import Constraint
+from pyomo.environ import Var
 
 import pyomo
 
@@ -74,3 +75,44 @@ def l_constraint(model, name, constraints, *args):
             v._data[i]._lower = pyomo.core.base.numvalue.NumericConstant(c[2])
             v._data[i]._upper = None
     #v.construct()
+
+
+def mutate_variable(var=None, value=None, bounds=(0, None), fix=False,
+                    index=None):
+    r""" Mutates existing pyomo variable, could be used to extend an emtpy
+    Var() object or an existing indexed pyomo variable.
+    It's possible to overwrite information for existing indices.
+
+    Parameters
+    ----------
+    var : pyomo.core.base.var.Var() object
+       pyomo variable to be extended
+    value : list
+       values of variable in list
+    bounds : list
+       lower and upper bounds of variable as tuples in list
+    index : type of var._index elements (most likely tuple)
+       list of indices for extension (Warning: if existing indices are used,
+       data for for these indices will be overwritten.)
+    fix : list
+       list with boolean elements, if true, variable value is fixed, i.e.
+       converted to parameter in opt-problem
+
+    Example
+    -------
+    extend_variable(om.w, value=[10], bounds=[(0,10)], fix=[True],
+                    index=[('bel','sink',155)])
+    """
+
+    bounds = dict(zip(index, bounds))
+    value = dict(zip(index, value))
+    fix = dict(zip(index, fix))
+
+    for i in index:
+        var._data[i] = pyomo.core.base.var._VarData(var)
+        var._data[i].value = value[i]
+        var._data[i].setlb(bounds[i][0])
+        var._data[i].setub(bounds[i][1])
+
+        if fix[i]:
+            var._data[i].fix()
