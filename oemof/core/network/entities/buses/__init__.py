1933abb71c119a3488020121f9a82dee17470af3
uwe <uwe.krien@rl-institut.de>
2016-02-19 19:31:02 +0100

    add return temperature to heat bus

--- oemof/core/network/entities/buses/__init__.py
+++ oemof/core/network/entities/buses/__init__.py
@@ -3,13 +3,18 @@ from .. import Bus
 
 class HeatBus(Bus):
     r"""
+    A bus containing a temperature and optionally a return temperature.
+
     Parameters
     ----------
-    temp_kelvin : float or array-like
-        Temperature of the Bus
+    temperature : float or array-like
+        Temperature of the Bus [Kelvin]
+    re_temperature : float or array-like
+        Return temperature of the Bus [Kelvin]
 
     """
 
-    def __init__(self, temp_kelvin, **kwargs):
+    def __init__(self, temperature, re_temperature=None, **kwargs):
         super().__init__(**kwargs)
-        self.temp_kelvin = temp_kelvin
+        self.temperature = temperature
+        self.re_temperature = re_temperature
