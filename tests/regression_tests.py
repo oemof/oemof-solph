c42bc384ad0e3700fbb5f7499bd528f1920c69f4
Stephan Günther <gnn.code@gmail.com>
2016-02-25 18:49:02 +0100

    Squelch some pylint complaints

--- tests/regression_tests.py
+++ tests/regression_tests.py
@@ -1,3 +1,8 @@
+import logging
+
+from nose.tools import eq_, ok_
+import pandas as pd
+
 from oemof.core.energy_system import EnergySystem as ES, Simulation
 from oemof.core.network.entities import Bus
 from oemof.core.network.entities.components.sinks import Simple as Sink
@@ -7,11 +12,6 @@ from oemof.outputlib.to_pandas import ResultsDataFrame as RDF
 from oemof.solph.optimization_model import OptimizationModel as OM
 from oemof.solph import predefined_objectives as po
 
-from nose.tools import eq_, ok_
-import pandas as pd
-
-import logging
-
 class TestSolphAndItsResults:
     def test_issue_74(self):
         logging.disable(logging.CRITICAL)
@@ -19,16 +19,16 @@ class TestSolphAndItsResults:
 
         Storage.optimization_options.update({'investment': True})
         sim = Simulation(timesteps=[0],
-                        objective_options={'function': po.minimize_cost})
-        es  = ES(simulation=sim)
+                         objective_options={'function': po.minimize_cost})
+        es = ES(simulation=sim)
         bus = Bus(uid="bus")
         store = Storage(uid="store", inputs=[bus], outputs=[bus], c_rate_out=0.1,
                         c_rate_in=0.1)
-        sink  = Sink(uid="sink", inputs=[bus], val=[1])
+        sink = Sink(uid="sink", inputs=[bus], val=[1])
 
         om = OM(es)
         om.objective.set_value(-1)
-        es.results=om.results()
+        es.results = om.results()
 
         try:
             es.dump()
@@ -42,20 +42,20 @@ class TestSolphAndItsResults:
         failed = False
 
         sim = Simulation(timesteps=[0],
-                        objective_options={'function': po.minimize_cost})
-        tix = time_index=pd.period_range('1970-01-01', periods=1, freq='H')
-        es  = ES(simulation=sim, time_idx=tix)
+                         objective_options={'function': po.minimize_cost})
+        tix = time_index = pd.period_range('1970-01-01', periods=1, freq='H')
+        es = ES(simulation=sim, time_idx=tix)
         bus = Bus(uid="bus")
         source = FS(uid="source", outputs=[bus], val=[0.5], out_max=[1])
-        sink  = Sink(uid="sink", inputs=[bus], val=[1])
+        sink = Sink(uid="sink", inputs=[bus], val=[1])
 
         om = OM(es)
-        es.results=om.results()
+        es.results = om.results()
         es.results[bus][sink] = [0.7]
         rdf = RDF(energy_system=es)
         try:
-            eq_(rdf.loc[(slice(None), slice(None), slice(None), "sink"),:
-                        ].val[0],
+            eq_(rdf.loc[(slice(None), slice(None), slice(None), "sink"), :
+                       ].val[0],
                 0.7,
                 "Output from bus to sink does not have the correct value.")
         except KeyError:
@@ -67,8 +67,8 @@ class TestSolphAndItsResults:
         es.results[bus][bus] = [-1]
         rdf = RDF(energy_system=es)
         try:
-            eq_(rdf.loc[(slice(None), slice(None), slice(None), "sink"),:
-                        ].val[0],
+            eq_(rdf.loc[(slice(None), slice(None), slice(None), "sink"), :
+                       ].val[0],
                 0.7,
                 "Output from bus to sink does not have the correct value.")
         except KeyError:
