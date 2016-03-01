28d117d76fa3bfe0d21619425fb7a46def53ae29
Hilpert <simon.hilpert@fh-flensburg.de>
2016-03-01 13:21:01 +0100

    Adapt heat_demand_example

--- examples/development_examples/heat_demand_example.py
+++ examples/development_examples/heat_demand_example.py
@@ -1,52 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb 24 14:51:34 2016
 
-@author: uwe
 """
-import numpy as np
-from matplotlib import pyplot as plt
+import pandas as pd
 from oemof.demandlib import energy_buildings as eb
 from oemof.tools import helpers
+from oemof.demandlib import bdew_heatprofile as bdew_heat
 
-# Erstellung eines Temperaturvektors
-periods = 8760
-temperature = np.ones(periods) * 263 + (
-    np.random.rand(periods) * 40).round(0)
+# read example temperature series
+temperature = pd.read_csv("example_data.csv")["temperature"]
 
-# Hilsfunktionen für die Standortbestimmung für die Feiertage
-print(helpers.abbreviation_of_state('Schleswig-Holstein'))
-print(helpers.fetch_admin_from_coord_osm((7.4, 55)))
+# get german holidays
+holidays = helpers.get_german_holidays(2010, ['Germany', 'SH'])
 
-# Bestimmung der Feiertage über Standort oder Ortsangabe
-try:
-    feiertage = helpers.get_german_holidays(
-        2010, helpers.fetch_admin_from_coord_osm(7.4, 55))
-except:
-    feiertage = helpers.get_german_holidays(2010, ['Germany', 'SH'])
-
-# Jetzt kannst Du mit oder ohne Feiertage arbeiten. Den UNterschied siehst Du
-# in der "weekday"-Spalte (0=Feiertag, 1=Montag, 7=Sonntag). Bei der Berechnung
-# werden Feiertag und Sonntag gleich behandelt, aber das lässt sich auch
-# ändern.
-time_df = helpers.create_basic_dataframe(2010)
-print(time_df[:5])
-time_df = helpers.create_basic_dataframe(2010, holidays=feiertage)
-print(time_df[:5])
-
-# Mögliche shlp-Typen sind:
-# shlp = standardized heat load profile
-# GMF, GPD, GHD, GWA, GGB, EFH, GKO, MFH, GBD, GBA, GMK, GBH, GGA, GHA
-
-# Windklassen gibt es nur 2: windiger Standort 1, sonst 0
-
-# Baualtersklassen 1-11, da finde ich gerade die Erklärung nicht zu
 
 # Define default building
-default_efh = eb.HeatBuilding(time_df, temperature, shlp_type='EFH',
-                              building_class=1, wind_class=0,
-                              annual_heat_demand=150)
+efh = eb.Building()
+# calculate heat load based on bdew profiles
+efh.heat_load = efh.hourly_heat_demand(datapath="../../oemof/demandlib/data",
+                                       year=2010, holiday=holidays,
+                                       temperature=temperature,
+                                       shlp_type='EFH',
+                                       building_class=10,
+                                       wind_class=1,
+                                       annual_heat_demand=10000,
+                                       function=bdew_heat.create_bdew_profile)
 
 # Plot demand of building
-default_efh.load.plot()
-plt.show()
+ax = efh.heat_load.plot()
+ax.set_xlabel("Hour of the year")
+ax.set_ylabel("Heat demand in MW)
