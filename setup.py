17b1cc68ebccfc369c126942f2f5dfcfb3fcb28a
Guido Pleßmann <guido.plessmann@rl-institut.de>
2016-05-10 15:55:41 +0200

    Add nose to setup.py

--- setup.py
+++ setup.py
@@ -23,5 +23,6 @@ setup(name='oemof',
       install_requires=['dill',
                         'numpy >= 1.7.0',
                         'pandas >= 0.17.0',
-                        'pyomo >= 4.2.0, != 4.3.11377']
+                        'pyomo >= 4.2.0, != 4.3.11377',
+			'nose']
      )
