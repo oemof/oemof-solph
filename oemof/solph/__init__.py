aa1b0d8e08d50dd43ccf41065ba50e639c533f9d
Hilpert <simon.hilpert@fh-flensburg.de>
2016-05-13 08:59:42 +0200

    Add Flow class to solph __init__.py

--- oemof/solph/__init__.py
+++ oemof/solph/__init__.py
@@ -5,4 +5,17 @@ Solph depend on pyomo.
 
 """
 
+class Flow:
+    def __init__(self, actual_value, nominal_value, variable_costs, min, max,
+                 fixed_costs, summed_min, summed_max):
+        """
 
+        """
+        self.min = min
+        self.max = max
+        self.actual_value = actual_value
+        self.nominal_value = nominal_value
+        self.variable_costs =  variable_costs
+        self.fixed_costs = fixed_costs
+        self.summed_min = summed_min
+        self.summed_max =  summed_min
