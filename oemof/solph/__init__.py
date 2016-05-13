9468cf465b12308b76857d257aede5b90433e482
Hilpert <simon.hilpert@fh-flensburg.de>
2016-05-13 09:33:34 +0200

    Add network subclassesof for solph

--- oemof/solph/__init__.py
+++ oemof/solph/__init__.py
@@ -7,7 +7,7 @@ Solph depend on pyomo.
 
 class Flow:
     def __init__(self, actual_value, nominal_value, variable_costs, min, max,
-                 fixed_costs, summed_min, summed_max):
+                 fixed_costs, summed_min, summed_max, fixed=False):
         """
 
         """
@@ -19,13 +19,41 @@ class Flow:
         self.fixed_costs = fixed_costs
         self.summed_min = summed_min
         self.summed_max =  summed_min
+        self.fixed = fixed
 
 
 
 # TODO: create solph sepcific energysystem subclassed from core energy system
-def EnergySystem():
+class EnergySystem():
 
     def __init__(self):
         """
         """
         pass
+
+from oemof.network import Source, Sink, Transformer
+
+class Sink(Sink):
+    """
+    """
+    def __init__(self):
+        super().__init__()
+
+class Source(Source):
+    """
+    """
+    def __init__(self):
+        super().__init__()
+
+class LinearTransformer(Transformer):
+    """
+    """
+    def __init__(self):
+        super().__init__()
+
+class Storage(Transformer):
+    """
+    """
+    def __init__(self):
+
+        super().__init__()
