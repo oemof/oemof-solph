0254b4085998711cb5c15c2fd669bf55e993b019
Stephan Günther <gnn.code@gmail.com>
2016-05-13 11:07:49 +0200

    Fix typo: depend --> depends

--- oemof/solph/__init__.py
+++ oemof/solph/__init__.py
@@ -1,7 +1,7 @@
 """
 The solph-package contains funtionalities for creating and solving an
 optimizaton problem. The problem is created from oemof base classes.
-Solph depend on pyomo.
+Solph depends on pyomo.
 
 """
 import oemof.network as on
