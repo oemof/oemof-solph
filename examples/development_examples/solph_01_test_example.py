0f51f290dca0cc31eb8093ef4d79ce3f06ba3bab
uwe <uwe.krien@rl-institut.de>
2016-05-20 12:00:55 +0200

    add EnergySystem and OperationalModel to example

--- examples/development_examples/solph_01_test_example.py
+++ examples/development_examples/solph_01_test_example.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import logging
+
 from oemof.tools import logger
+from oemof.core import energy_system as core_es
 
 from oemof.solph import Bus
 from oemof.solph import Source
@@ -10,17 +12,22 @@ from oemof.solph import Flow
 from oemof.solph import Investment
 from oemof.solph import LinearTransformer
 from oemof.solph import Storage
+from oemof.solph import constraint_grouping
+from oemof.solph import objective_grouping
+from oemof.solph import OperationalModel
 
-# Define logger
+
 logger.define_logging()
+
+es = core_es.EnergySystem(groupings=[constraint_grouping, objective_grouping],
+                       time_idx=[1, 2, 3])
 
 ebus = Bus(label="el")
 
 gasbus = Bus(label="gas")
 
-so = Source(outputs={ebus: Flow(
-    actual_value=[10, 5, 10], fixed=True, investement=Investment(
-    maximum=1000))})
+so = Source(outputs={ebus: Flow(actual_value=[10, 5, 10], fixed=True,
+                                investement=Investment(maximum=1000))})
 
 si = Sink(inputs={ebus: Flow(
     min=[0, 0, 0], max=[0.1, 0.2, 0.9], nominal_value=10, fixed=True)})
@@ -34,5 +41,9 @@ estorage = Storage(
     nominal_capacity=500, capacity_loss=0.1, nominal_input_capacity_ratio=0.2,
     nominal_output_capacity_ratio=0.5, inflow_conversion_factor=1,
     outflow_conversion_factor=1)
+
+om = OperationalModel(es)
+
+om.pprint()
 
 logging.info('Done!')
