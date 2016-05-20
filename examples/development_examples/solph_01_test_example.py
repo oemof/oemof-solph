a437fad81eac44b2530c69c33e5fc99033688832
Hilpert <simon.hilpert@fh-flensburg.de>
2016-05-20 12:15:15 +0200

    Adapt developing solph_example_01 to latest solph changes

--- examples/development_examples/solph_01_test_example.py
+++ examples/development_examples/solph_01_test_example.py
@@ -4,45 +4,45 @@ import logging
 
 from oemof.tools import logger
 from oemof.core import energy_system as core_es
+import oemof.solph as solph
+from oemof.solph import (Bus, Source, Sink, Flow, Investment, LinearTransformer,
+                        Storage)
 
-from oemof.solph import Bus
-from oemof.solph import Source
-from oemof.solph import Sink
-from oemof.solph import Flow
-from oemof.solph import Investment
-from oemof.solph import LinearTransformer
-from oemof.solph import Storage
-from oemof.solph import constraint_grouping
-from oemof.solph import objective_grouping
-from oemof.solph import OperationalModel
 
 
 logger.define_logging()
 
-es = core_es.EnergySystem(groupings=[constraint_grouping, objective_grouping],
-                       time_idx=[1, 2, 3])
+es = core_es.EnergySystem(groupings=solph.GROUPINGS,
+                          time_idx=[1, 2, 3])
 
 ebus = Bus(label="el")
 
 gasbus = Bus(label="gas")
 
-so = Source(outputs={ebus: Flow(actual_value=[10, 5, 10], fixed=True,
-                                investement=Investment(maximum=1000))})
+so = Source(
+    label="source",
+    outputs={ebus: Flow(actual_value=[10, 5, 10], fixed=True,
+                        investement=Investment(maximum=1000))})
 
-si = Sink(inputs={ebus: Flow(
-    min=[0, 0, 0], max=[0.1, 0.2, 0.9], nominal_value=10, fixed=True)})
+si = Sink(
+    label="sink",
+    inputs={ebus: Flow(min=[0, 0, 0], max=[0.1, 0.2, 0.9], nominal_value=10,
+                       fixed=True)})
 
 ltransf = LinearTransformer(
+    label="trsf",
     inputs={gasbus: Flow()},
-    outputs={ebus: Flow(max=[1.0, 0.8, 0.9], nominal_value=100)})
+    outputs={ebus: Flow(max=[1.0, 0.8, 0.9], nominal_value=100)},
+    conversion_factors={ebus: 0.5})
 
 estorage = Storage(
+    label="storage",
     inputs={ebus: Flow()}, outputs={ebus: Flow(nominal_value=100)},
     nominal_capacity=500, capacity_loss=0.1, nominal_input_capacity_ratio=0.2,
     nominal_output_capacity_ratio=0.5, inflow_conversion_factor=1,
     outflow_conversion_factor=1)
 
-om = OperationalModel(es)
+om = solph.OperationalModel(es)
 
 om.pprint()
 
