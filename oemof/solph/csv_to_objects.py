8d027da6560da832d2becd9580fc2783bf1a30c6
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2016-05-30 15:00:38 +0200

    Add basic interface

--- oemof/solph/csv_to_objects.py
+++ oemof/solph/csv_to_objects.py
@@ -1,54 +1,38 @@
 # -*- coding: utf-8 -*-
 
-from oemof.core import energy_system as oces
 import pandas as pd
-
-nodes = pd.read_csv('nodes.csv', sep=',')
-
-
-
-nodes.index = pd.date_range('1/1/2015', periods=len(df), freq='H')
+import oemof.network as on
+from oemof.solph import Flow
 
 
+bel = on.Bus(label='el_balance')
+bcoal = on.Bus(label='coalbus')
 
-es = oces.EnergySystem(groupings=GROUPINGS,
-                       time_idx=[1,2,3])
+so = on.Source(label='coalsource', outputs={bcoal: Flow()})
 
-lt = len(es.time_idx)
+wind = on.Source(label='wind',
+                 outputs={bel: Flow(actual_value=[1, 1, 2], nominal_value=2,
+                                    fixed_costs=25)})
+si = on.Sink(label='sink',
+             inputs={bel: Flow(max=[0.1, 0.2, 0.9], nominal_value=10,
+                               fixed=True, actual_value=[1, 2, 3])})
 
-bel = Bus(label="el_balance")
-bcoal = Bus(label="coalbus")
+nodes_flows = pd.read_csv('nodes_flows.csv', sep=',')
 
-so = Source(label="coalsource",
-            outputs={bcoal: Flow()})
+nodes_flows_seq = pd.read_csv('nodes_flows_seq.csv', sep=',', header=None)
+nodes_flows_seq.drop(0, axis=1, inplace=True)
+nodes_flows_seq = nodes_flows_seq.transpose()
 
-wind = Source(label="wind", outputs={
-    bel:Flow(actual_value=[1, 1, 2],
-             nominal_value=2,
-             fixed_costs=25,
-             investment=Investment(maximum=100, epc=200))
-    }
-)
 
-si = Sink(label="sink", inputs={bel: Flow(max=[0.1, 0.2, 0.9],
-                                          nominal_value=10, fixed=True,
-                                          actual_value=[1, 2, 3])})
-
-trsf = LinearTransformer(label='trsf', inputs={bcoal:Flow()},
-                         outputs={bel:Flow(nominal_value=10,
-                                           fixed_costs=5,
-                                           variable_costs=10,
-                                           summed_max=4,
-                                           summed_min=2)},
-                         conversion_factors={bel: 0.4})
-stor = Storage(label="stor", inputs={bel: Flow()}, outputs={bel:Flow()},
-               nominal_capacity=50, inflow_conversion_factor=0.9,
-               outflow_conversion_factor=0.8, initial_capacity=0.5,
-               capacity_loss=0.001)
-
-date_time_index = pd.date_range('1/1/2011', periods=3, freq='60min')
-om = OperationalModel(es, timeindex=date_time_index)
-om.solve(solve_kwargs={'tee': True})
-om.write('optimization_problem.lp',
-         io_options={'symbolic_solver_labels': True})
-#om.pprint()
+#trsf = on.LinearTransformer(label='trsf', inputs={bcoal:Flow()},
+#                         outputs={bel:Flow(nominal_value=10,
+#                                           fixed_costs=5,
+#                                           variable_costs=10,
+#                                           summed_max=4,
+#                                           summed_min=2)},
+#                         conversion_factors={bel: 0.4})
+#
+#stor = on.Storage(label='stor', inputs={bel: Flow()}, outputs={bel:Flow()},
+#               nominal_capacity=50, inflow_conversion_factor=0.9,
+#               outflow_conversion_factor=0.8, initial_capacity=0.5,
+#               capacity_loss=0.001)
