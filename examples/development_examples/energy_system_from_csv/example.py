94eec661d75cd910f86d56e973f387cf0a8f7e7c
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2016-06-07 18:32:15 +0200

    Create optimisation problem

--- examples/development_examples/energy_system_from_csv/example.py
+++ examples/development_examples/energy_system_from_csv/example.py
@@ -11,8 +11,30 @@ from oemof.solph import OperationalModel
 from oemof.solph.options import NodesFromCSV
 
 
+logger.define_logging()
+
+timesteps_max = 8760
+
+es = core_es.EnergySystem(groupings=solph.GROUPINGS,
+                          time_idx=[i for i in range(0, timesteps_max)])
+
+datetime_index = pd.date_range('1/1/2016', periods=timesteps_max, freq='60min')
+
+
 nodes = NodesFromCSV(file_nodes_flows='nodes_flows.csv',
                      file_nodes_flows_sequences='nodes_flows_seq.csv',
                      delimiter=';')
+
+om = OperationalModel(es, timeindex=datetime_index)
+
+om.solve(solve_kwargs={'tee': True})
+
+om.write('optimization_problem.lp',
+         io_options={'symbolic_solver_labels': True})
+
+om.pprint()
+
+logging.info('Done!')
+
 
 print(nodes)
