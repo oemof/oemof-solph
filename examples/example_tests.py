a0aaa28c07ce62b79df7df0525f99fb7b25209c8
uwe <uwe.krien@rl-institut.de>
2016-06-16 11:49:00 +0200

    move example run into try block again
    This code was moved outside the try block due to debugging.

--- examples/example_tests.py
+++ examples/example_tests.py
@@ -48,14 +48,14 @@ testdict['stor_inv'] = {'name': "Storage invest example",
 
 number_of_timesteps = 8760
 
-esys = storage_invest.initialise_energysystem(number_of_timesteps)
-filepath = os.path.join('solph/storage_optimization', 'storage_invest.csv')
-esys = storage_invest.optimise_storage_size(
-    esys, filename=filepath, solvername=testdict['stor_inv']['solver'])
-results = storage_invest.get_result_dict(esys)
-stor_invest_run = True
 try:
-    pass
+    esys = storage_invest.initialise_energysystem(number_of_timesteps)
+    filepath = os.path.join('solph/storage_optimization', 'storage_invest.csv')
+    esys = storage_invest.optimise_storage_size(
+        esys, filename=filepath, solvername=testdict['stor_inv']['solver'])
+    results = storage_invest.get_result_dict(esys)
+    stor_invest_run = True
+
 except Exception as e:
     testdict['stor_inv']['messages'] = {'error': e}
     stor_invest_run = False
