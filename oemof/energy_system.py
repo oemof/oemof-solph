9f20f0d2189d9aa89042cd0615e9db26b47f246b
uwe <uwe.krien@rl-institut.de>
2016-07-11 12:11:00 +0200

    move energy_system module to oemof root source path

--- oemof/energy_system.py
+++ oemof/energy_system.py
@@ -12,8 +12,7 @@ import os
 
 import dill as pickle
 
-from oemof.core.network import Entity
-from oemof.core.network.entities.components import transports as transport
+from oemof.network import Entity
 from oemof.groupings import (DEFAULT as BY_UID, Grouping as GroupingBase,
                              Nodes as Grouping)
 from oemof.network import Node
@@ -102,10 +101,10 @@ class EnergySystem:
     <oemof.core.network.Entity>` will always be grouped by their :attr:`uid
     <oemof.core.network.Entity.uid>`:
 
-    >>> from oemof.core.network import Entity
-    >>> from oemof.core.network.entities import Bus, Component
+    >>> from oemof.network import Entity
+    >>> from oemof.network import Bus, Sink
     >>> es = EnergySystem()
-    >>> bus = Bus(uid='electricity')
+    >>> bus = Bus(label='electricity')
     >>> bus is es.groups['electricity']
     True
 
@@ -117,12 +116,12 @@ class EnergySystem:
     their `type`:
 
     >>> es = EnergySystem(groupings=[type])
-    >>> buses = set(Bus(uid="Bus {}".format(i)) for i in range(9))
-    >>> components = set( Component(uid="Component {}".format(i))
+    >>> buses = set(Bus(label="Bus {}".format(i)) for i in range(9))
+    >>> components = set(Sink(label="Component {}".format(i))
     ...                   for i in range(9))
     >>> buses == es.groups[Bus]
     True
-    >>> components == es.groups[Component]
+    >>> components == es.groups[Sink]
     True
 
     """
@@ -207,124 +206,3 @@ class EnergySystem:
         logging.debug(msg)
         return msg
 
-
-class Region:
-    r"""Defining a region within an energy supply system.
-
-    Note
-    ----
-    The list of regions is not necessary to use the energy system with solph.
-
-    Parameters
-    ----------
-    entities : list of core.network objects
-        List of all objects of the energy system. All class descriptions can
-        be found in the :py:mod:`oemof.core.network` package.
-    name : string
-        A unique name to identify the region. If possible use typical names for
-        regions and english names for countries.
-    code : string
-        A short unique name to identify the region.
-    geom : shapely.geometry object
-        The geometry representing the region must be a polygon or a multi
-        polygon.
-
-    Attributes
-    ----------
-    entities : list of core.network objects
-        List of all objects of the energy system. All class descriptions can
-        be found in the :py:mod:`oemof.core.network` package.
-    name : string
-        A unique name to identify the region. If possible use typical names for
-        regions and english names for countries.
-    geom : shapely.geometry object
-        The geometry representing the region must be a polygon or a multi
-        polygon.
-    """
-    def __init__(self, **kwargs):
-        self.entities = []  # list of entities
-        self.add_entities(kwargs.get('entities', []))
-
-        self.name = kwargs.get('name')
-        self.geom = kwargs.get('geom')
-        self._code = kwargs.get('code')
-
-    # TODO: oder sollte das ein setter sein? Yupp.
-    def add_entities(self, entities):
-        """Add a list of entities to the existing list of entities.
-
-        For every entity added to a region the region attribute of the entity
-        is set
-
-        Parameters
-        ----------
-        entities : list of core.network objects
-            List of all objects of the energy system that belongs to area
-            covered by the polygon of the region. All class descriptions can
-            be found in the :py:mod:`oemof.core.network` package.
-        """
-
-        # TODO: prevent duplicate entries
-        self.entities.extend(entities)
-        for entity in entities:
-            if self not in entity.regions:
-                entity.regions.append(self)
-
-    @property
-    def code(self):
-        """Creating a short code based on the region name if no code is set."""
-        if self._code is None:
-            name_parts = self.name.replace('_', ' ').split(' ', 1)
-            self._code = ''
-            for part in name_parts:
-                self._code += part[:1].upper() + part[1:3]
-        return self._code
-
-
-class Simulation:
-    r"""Defining the simulation related parameters according to the solver lib.
-
-    Parameters
-    ----------
-    solver : string
-        Name of the solver supported by the used solver library.
-        (e.g. 'glpk', 'gurobi')
-    debug : boolean
-        Set the chosen solver to debug (verbose) mode to get more information.
-    verbose : boolean
-        If True, solver output etc. is streamed in python console
-    duals : boolean
-        If True, results of dual variables and reduced costs will be saved
-    objective_options : dictionary
-        'function': function to use from
-                    :py:mod:`oemof.solph.predefined_objectives`
-        'cost_objects': list of str(`class`) elements. Objects of type  `class`
-                        are include in cost terms of objective function.
-        'revenue_objects': list of str(`class`) elements. . Objects of type
-                           `class` are include in revenue terms of
-                           objective function.
-    timesteps : list or sequence object
-         Timesteps to be simulated or optimized in the used library
-    relaxed : boolean
-        If True, integer variables will be relaxed
-        (only relevant for milp-problems)
-    fast_build : boolean
-        If True, the standard way of pyomo constraint building is skipped and
-        a different function is used.
-        (Warning: No guarantee that all expected 'standard' pyomo model
-        functionalities work for the constructed model!)
-    """
-    def __init__(self, **kwargs):
-        ''
-        self.solver = kwargs.get('solver', 'glpk')
-        self.debug = kwargs.get('debug', False)
-        self.verbose = kwargs.get('verbose', False)
-        self.objective_options = kwargs.get('objective_options', {})
-        self.duals = kwargs.get('duals', False)
-        self.timesteps = kwargs.get('timesteps')
-        self.relaxed = kwargs.get('relaxed', False)
-        self.fast_build = kwargs.get('fast_build', False)
-        self.solve_kwargs = kwargs.get('solve_kwargs', {})
-
-        if self.timesteps is None:
-            raise ValueError('No timesteps defined!')
