f6859c4ec70a60ad0a1a6226b4c7dc283a08cb96
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2016-07-18 16:10:58 +0200

    Uncomment plot and fix error with clean NTCs

--- examples/solph/nodes_from_csv/renpass_gis.py
+++ examples/solph/nodes_from_csv/renpass_gis.py
@@ -120,73 +120,73 @@ for cc in country_codes:
                                     date_from=date_from, date_to=date_to,
                                     formatted=True)
 
-#    # data from model in MWh
-#    model_data = pd.concat([inputs, outputs], axis=1)
-#
-#    powerline_cols = [col for col in model_data.columns if 'powerline' in col]
-#    powerlines = model_data[powerline_cols]
-#
-#    exports = powerlines[
-#        [col for col in powerlines.columns if cc+'_' in col]].sum(axis=1)
-#    exports = exports.to_frame()
-#    imports = powerlines[
-#        [col for col in powerlines.columns if '_'+cc+'_' in col]].sum(axis=1)
-#    imports = imports.to_frame()
-#    imports_exports = imports-exports
-#    imports_exports.columns = ['import_export']
-#
-#    model_data = pd.concat([model_data, imports_exports], axis=1)
-#    model_data = model_data[
-#        [col for col in model_data.columns
-#         if 'powerline' not in col
-#         if 'shortage' not in col
-#         if 'excess' not in col]]
-#    model_data.rename(columns=lambda x: x.replace(cc+'_', ''), inplace=True)
-#    model_data = model_data/1000
-#    model_data = model_data.resample('1A').sum()
-#
-#    # exclude AT as its pps are connected to the german electricity bus
-#    if cc not in ['AT', 'LU']:
-#        model_data = model_data[
-#             ['load', 'solar', 'wind', 'pp_uranium', 'pp_lignite',
-#              'pp_hard_coal', 'pp_gas', 'pp_oil', 'pp_mixed_fuels',
-#              'pp_biomass', 'run_of_river', 'storage_phs_out',
-#              'import_export']]
-#
-#    # data from ENTSO-E in GWh
-#    idx = 'ENTSOE/' + cc + '_PROD'
-#    entsoe_data = Quandl.get(idx,
-#                             trim_start="2014-01-01",
-#                             trim_end="2014-12-31",
-#                             authtoken=auth_tok)
-#    entsoe_data.rename(columns=new_colnames, inplace=True)
-#    entsoe_data = entsoe_data[['load', 'solar', 'wind', 'uranium', 'lignite',
-#                               'hard_coal', 'gas', 'oil', 'mixed_fuels',
-#                               'biomass', 'run_of_river', 'pumped_hydro',
-#                               'import_export', 'other_fossil',
-#                               'other_hydro', 'generation_not_clearly']]
-#    entsoe_data.index = pd.date_range(entsoe_data.index[0], periods=12,
-#                                      freq='M')
-#    entsoe_data = entsoe_data.resample('1A').sum()
-#
-#    # plotting
-#    fig, axes = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True)
-#    fig.suptitle('Validation for 2014'+' ('+cc+')', fontsize=16)
-#
-#    model_plot = model_data.plot(kind='bar', stacked=False, ax=axes[0])
-#    model_plot.set_ylabel('Energy in GWh')
-#    model_plot.set_xlabel('Model Results')
-#    model_plot.set_xticklabels([])
-#    model_plot.legend(loc='upper right', ncol=1, fontsize=6)
-#
-#    entsoe_plot = entsoe_data.plot(kind='bar', stacked=False, ax=axes[1])
-#    entsoe_plot.set_ylabel('Energy in GWh')
-#    entsoe_plot.set_xlabel('ENTSO-E Data')
-#    entsoe_plot.set_xticklabels([])
-#    entsoe_plot.legend(loc='upper right', ncol=1, fontsize=6)
-#
-#    plt.savefig('validation_'+cc+'.pdf', orientation='landscape')
-#    plt.close()
+    # data from model in MWh
+    model_data = pd.concat([inputs, outputs], axis=1)
+
+    powerline_cols = [col for col in model_data.columns if 'powerline' in col]
+    powerlines = model_data[powerline_cols]
+
+    exports = powerlines[
+        [col for col in powerlines.columns if cc+'_' in col]].sum(axis=1)
+    exports = exports.to_frame()
+    imports = powerlines[
+        [col for col in powerlines.columns if '_'+cc+'_' in col]].sum(axis=1)
+    imports = imports.to_frame()
+    imports_exports = imports-exports
+    imports_exports.columns = ['import_export']
+
+    model_data = pd.concat([model_data, imports_exports], axis=1)
+    model_data = model_data[
+        [col for col in model_data.columns
+         if 'powerline' not in col
+         if 'shortage' not in col
+         if 'excess' not in col]]
+    model_data.rename(columns=lambda x: x.replace(cc+'_', ''), inplace=True)
+    model_data = model_data/1000
+    model_data = model_data.resample('1A').sum()
+
+    # exclude AT as its pps are connected to the german electricity bus
+    if cc not in ['AT', 'LU']:
+        model_data = model_data[
+             ['load', 'solar', 'wind', 'pp_uranium', 'pp_lignite',
+              'pp_hard_coal', 'pp_gas', 'pp_oil', 'pp_mixed_fuels',
+              'pp_biomass', 'run_of_river', 'storage_phs_out',
+              'import_export']]
+
+    # data from ENTSO-E in GWh
+    idx = 'ENTSOE/' + cc + '_PROD'
+    entsoe_data = Quandl.get(idx,
+                             trim_start="2014-01-01",
+                             trim_end="2014-12-31",
+                             authtoken=auth_tok)
+    entsoe_data.rename(columns=new_colnames, inplace=True)
+    entsoe_data = entsoe_data[['load', 'solar', 'wind', 'uranium', 'lignite',
+                               'hard_coal', 'gas', 'oil', 'mixed_fuels',
+                               'biomass', 'run_of_river', 'pumped_hydro',
+                               'import_export', 'other_fossil',
+                               'other_hydro', 'generation_not_clearly']]
+    entsoe_data.index = pd.date_range(entsoe_data.index[0], periods=12,
+                                      freq='M')
+    entsoe_data = entsoe_data.resample('1A').sum()
+
+    # plotting
+    fig, axes = plt.subplots(nrows=1, ncols=2, sharex=True, sharey=True)
+    fig.suptitle('Validation for 2014'+' ('+cc+')', fontsize=16)
+
+    model_plot = model_data.plot(kind='bar', stacked=False, ax=axes[0])
+    model_plot.set_ylabel('Energy in GWh')
+    model_plot.set_xlabel('Model Results')
+    model_plot.set_xticklabels([])
+    model_plot.legend(loc='upper right', ncol=1, fontsize=6)
+
+    entsoe_plot = entsoe_data.plot(kind='bar', stacked=False, ax=axes[1])
+    entsoe_plot.set_ylabel('Energy in GWh')
+    entsoe_plot.set_xlabel('ENTSO-E Data')
+    entsoe_plot.set_xticklabels([])
+    entsoe_plot.legend(loc='upper right', ncol=1, fontsize=6)
+
+    plt.savefig('validation_'+cc+'.pdf', orientation='landscape')
+    plt.close()
 
     # plotting of prices for Germany
     if cc is 'DE':
