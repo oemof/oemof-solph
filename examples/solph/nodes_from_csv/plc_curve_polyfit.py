95ca3a5a6a9168c2dbce49348c9d12e3ad1882d8
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2016-07-22 16:43:45 +0200

    Add GARCH model and plots

--- examples/solph/nodes_from_csv/plc_curve_polyfit.py
+++ examples/solph/nodes_from_csv/plc_curve_polyfit.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from numpy import arange
+from arch import arch_model
 
 # global plotting options
 plt.rcParams.update(plt.rcParamsDefault)
@@ -27,76 +28,93 @@ df_raw = pd.read_csv(file)
 df_raw.head()
 df_raw.columns
 
-# %% create price volatility
+## %% create price volatility
+#
+#df_raw = df_raw[['eex_day_ahead_2014', 'power_price_model']]
+#
+#
+#def price_volatility(price, factor):
+#
+#    price **= factor
+#
+#    if price >= 100.:
+#        print('real: ', price)
+#        price = 100
+#        print('scaled: ', price)
+#
+#    return price
+#
+#df_raw['price_scaled'] = df_raw['power_price_model'].apply(price_volatility,
+#                                                           args=[1.275])
+#
+## adjust scaled prices by mean value difference
+#diff = df_raw['price_scaled'].mean() - df_raw['power_price_model'].mean()
+#df_raw['price_scaled'] -= diff
+#
+#df_raw[:].plot(subplots=True, sharey=True)
+#
+#plt.show()
 
-df_raw = df_raw[['eex_day_ahead_2014', 'power_price_model']]
+
+# %% polynom fitting
+
+# prepare dataframe for fit
+residual_load = df_raw['DE_load'] + df_raw['AT_load'] + df_raw['LU_load'] - \
+                df_raw['DE_wind'] - df_raw['AT_wind'] - df_raw['LU_wind'] - \
+                df_raw['DE_solar'] - df_raw['AT_solar'] - df_raw['LU_solar']
+df_polyfit = pd.concat([residual_load, df_raw['eex_day_ahead_2014'],
+                       df_raw['power_price_model']], axis=1)
+df_polyfit.columns = ['res_load', 'price_real', 'price_model']
+
+# fit polynom of 3rd degree
+z = np.polyfit(df_polyfit['res_load'], df_polyfit['price_real'], 3)
+p = np.poly1d(z)
+
+# save and plot results
+df_polyfit['price_polynom'] = p(df_polyfit['res_load'])
 
 
-def price_volatility(price, factor):
+# %% ARMA-GARCH approach
 
-    price **= factor
+df_polyfit['residuals'] = df_polyfit['price_real'] - \
+                          df_polyfit['price_model']
 
-    if price >= 80.:
-        print('real: ', price)
-        price = 80
-        print('scaled: ', price)
+am = arch_model(df_polyfit['residuals'])
 
-    return price
+res = am.fit(update_freq=5)
 
-df_raw['price_scaled'] = df_raw['power_price_model'].apply(price_volatility,
-                                                           args=[1.25])
+print(res.summary())
 
-diff = df_raw['price_scaled'].mean() - df_raw['power_price_model'].mean()
-df_raw['price_scaled'] -= diff
+df_polyfit['cond_volatility'] = res.conditional_volatility
 
-df_raw[:].plot(subplots=True, sharey=True)
+df_polyfit['sum_price_model_cond_volatility_GARCH'] = \
+    df_polyfit['price_model'] + df_polyfit['cond_volatility']
+
+print(df_polyfit[['price_real', 'price_model',
+                  'sum_price_model_cond_volatility_GARCH']].corr())
+
+# %% plotting
+
+#df_polyfit.plot(kind='scatter', x='res_load', y='price_real')
+
+df_polyfit[:][['price_real', 'price_model',
+               'sum_price_model_cond_volatility_GARCH']].plot(linewidth=1.2,
+                                                              subplots=True,
+                                                              drawstyle='steps',
+                                                              color=['grey', 'r', 'b'],
+                                                              ylim=[-100, 100])
+
+df_polyfit[0:24 * 31 * 3][['price_real', 'price_model',
+                           'sum_price_model_cond_volatility_GARCH']].plot(linewidth=1.2,
+                                                                          subplots=False,
+                                                                          drawstyle='steps',
+                                                                          color=['grey', 'r', 'b'],
+                                                                          ylim=[-100, 100])
+
+df_polyfit.plot(kind='scatter',
+                x='price_real', y='price_model')
+
+df_polyfit.plot(kind='scatter',
+                x='price_real', y='sum_price_model_cond_volatility_GARCH')
 
 plt.show()
-
-
-## %% polynom fitting
-#
-## prepare dataframe for fit
-#residual_load = df_raw['DE_load'] + df_raw['AT_load'] + df_raw['LU_load'] - \
-#                df_raw['DE_wind'] - df_raw['AT_wind'] - df_raw['LU_wind'] - \
-#                df_raw['DE_solar'] - df_raw['AT_solar'] - df_raw['LU_solar']
-#df_polyfit = pd.concat([residual_load, df_raw['eex_day_ahead_2014'],
-#                       df_raw['power_price_model']], axis=1)
-#df_polyfit.columns = ['res_load', 'price_real', 'price_model']
-#
-## fit polynom of 3rd degree
-#z = np.polyfit(df_polyfit['res_load'], df_polyfit['price_real'], 3)
-#p = np.poly1d(z)
-#
-## save and plot results
-#df_polyfit['price_regression_res_load'] = p(df_polyfit['res_load'])
-#
-#p2 = np.poly1d(np.polyfit(df_polyfit['price_model'],
-#                          df_polyfit['price_real'], 2))
-#
-## show values
-#df_polyfit.plot(kind='scatter', x='res_load', y='price_real')
-#df_polyfit.plot(kind='scatter', x='price_model', y='price_real')
-#
-#
-#df_polyfit['price_regression_price_model'] = p2(df_polyfit['price_real'])
-#
-#df_polyfit[0:24*31][['price_real',
-#                     'price_model']].plot(linewidth=1.2, subplots=True,
-#                                          drawstyle='steps', ylim=[-100, 100])
-#
-#df_polyfit[0:24*31][['price_real',
-#                     'price_regression_res_load']].plot(linewidth=1.2,
-#                                                        subplots=True,
-#                                                        drawstyle='steps',
-#                                                        ylim=[-100, 100])
-#
-#df_polyfit[0:24*31][['price_real',
-#                     'price_regression_price_model']].plot(linewidth=1.2,
-#                                                           subplots=True,
-#                                                           drawstyle='steps',
-#                                                           ylim=[-100, 100])
-#
-#df_bla = pd.DataFrame(p2(range(10, 80, 1)))
-#df_bla.plot()
-#plt.show()
