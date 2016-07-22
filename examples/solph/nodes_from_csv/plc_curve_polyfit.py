9c3bc73287ee0df16ef86ffda5c4441b4a5be247
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2016-07-22 14:13:57 +0200

    Add approach to scale prices with factor

--- examples/solph/nodes_from_csv/plc_curve_polyfit.py
+++ examples/solph/nodes_from_csv/plc_curve_polyfit.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from numpy import arange
 
 # global plotting options
 plt.rcParams.update(plt.rcParamsDefault)
@@ -18,52 +19,81 @@ plt.rcParams.update({'font.size': 10})
 plt.rcParams['image.cmap'] = 'Spectral'
 
 # read file
-df_raw = pd.read_csv('results/results_dispatch_prices_DE_nep_2014_aggr.csv')
+file = ('results/'
+        'results_dispatch_prices_DE_2016-07-21 17:10:21.901285nep_2014_aggr'
+        '.csv')
+
+df_raw = pd.read_csv(file)
 df_raw.head()
 df_raw.columns
 
+# %% create price volatility
 
-# prepare dataframe for fit
-residual_load = df_raw['DE_load'] + df_raw['AT_load'] + df_raw['LU_load'] - \
-                df_raw['DE_wind'] - df_raw['AT_wind'] - df_raw['LU_wind'] - \
-                df_raw['DE_solar'] - df_raw['AT_solar'] - df_raw['LU_solar']
-df_polyfit = pd.concat([residual_load, df_raw['eex_day_ahead_2014'],
-                       df_raw['power_price_model']], axis=1)
-df_polyfit.columns = ['res_load', 'price_real', 'price_model']
-
-# fit polynom of 3rd degree
-z = np.polyfit(df_polyfit['res_load'], df_polyfit['price_real'], 3)
-p = np.poly1d(z)
-
-# save and plot results
-df_polyfit['price_regression_res_load'] = p(df_polyfit['res_load'])
-
-p2 = np.poly1d(np.polyfit(df_polyfit['price_model'],
-                          df_polyfit['price_real'], 2))
-
-# show values
-df_polyfit.plot(kind='scatter', x='res_load', y='price_real')
-df_polyfit.plot(kind='scatter', x='price_model', y='price_real')
+df_raw = df_raw[['eex_day_ahead_2014', 'power_price_model']]
 
 
-df_polyfit['price_regression_price_model'] = p2(df_polyfit['price_real'])
+def price_volatility(price, factor):
+    price **= factor
+    if price >= 80.:
+        print('real: ', price)
+        price = 80
+        print('scaled: ', price)
+    return price
 
-df_polyfit[0:24*31][['price_real',
-                     'price_model']].plot(linewidth=1.2, subplots=True,
-                                          drawstyle='steps', ylim=[-100, 100])
+df_raw['price_scaled'] = df_raw['power_price_model'].apply(price_volatility,
+                                                           args=[1.25])
 
-df_polyfit[0:24*31][['price_real',
-                     'price_regression_res_load']].plot(linewidth=1.2,
-                                                        subplots=True,
-                                                        drawstyle='steps',
-                                                        ylim=[-100, 100])
+diff = df_raw['price_scaled'].mean() - df_raw['power_price_model'].mean()
+df_raw['price_scaled'] -= diff
 
-df_polyfit[0:24*31][['price_real',
-                     'price_regression_price_model']].plot(linewidth=1.2,
-                                                           subplots=True,
-                                                           drawstyle='steps',
-                                                           ylim=[-100, 100])
+df_raw[:].plot(subplots=True, sharey=True)
 
-df_bla = pd.DataFrame(p2(range(10, 80, 1)))
-df_bla.plot()
 plt.show()
+
+
+## %% polynom fitting
+#
+## prepare dataframe for fit
+#residual_load = df_raw['DE_load'] + df_raw['AT_load'] + df_raw['LU_load'] - \
+#                df_raw['DE_wind'] - df_raw['AT_wind'] - df_raw['LU_wind'] - \
+#                df_raw['DE_solar'] - df_raw['AT_solar'] - df_raw['LU_solar']
+#df_polyfit = pd.concat([residual_load, df_raw['eex_day_ahead_2014'],
+#                       df_raw['power_price_model']], axis=1)
+#df_polyfit.columns = ['res_load', 'price_real', 'price_model']
+#
+## fit polynom of 3rd degree
+#z = np.polyfit(df_polyfit['res_load'], df_polyfit['price_real'], 3)
+#p = np.poly1d(z)
+#
+## save and plot results
+#df_polyfit['price_regression_res_load'] = p(df_polyfit['res_load'])
+#
+#p2 = np.poly1d(np.polyfit(df_polyfit['price_model'],
+#                          df_polyfit['price_real'], 2))
+#
+## show values
+#df_polyfit.plot(kind='scatter', x='res_load', y='price_real')
+#df_polyfit.plot(kind='scatter', x='price_model', y='price_real')
+#
+#
+#df_polyfit['price_regression_price_model'] = p2(df_polyfit['price_real'])
+#
+#df_polyfit[0:24*31][['price_real',
+#                     'price_model']].plot(linewidth=1.2, subplots=True,
+#                                          drawstyle='steps', ylim=[-100, 100])
+#
+#df_polyfit[0:24*31][['price_real',
+#                     'price_regression_res_load']].plot(linewidth=1.2,
+#                                                        subplots=True,
+#                                                        drawstyle='steps',
+#                                                        ylim=[-100, 100])
+#
+#df_polyfit[0:24*31][['price_real',
+#                     'price_regression_price_model']].plot(linewidth=1.2,
+#                                                           subplots=True,
+#                                                           drawstyle='steps',
+#                                                           ylim=[-100, 100])
+#
+#df_bla = pd.DataFrame(p2(range(10, 80, 1)))
+#df_bla.plot()
+#plt.show()
