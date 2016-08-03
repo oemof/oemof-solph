8d649611dda2c98ba5f40cb1450f4b87d4267dec
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2016-08-03 14:00:48 +0200

    Add duration curve plots

--- examples/solph/nodes_from_csv/dlsk_plotting.py
+++ examples/solph/nodes_from_csv/dlsk_plotting.py
@@ -107,9 +107,13 @@ imports = powerlines[[c for c in powerlines.columns
 
 # exports
 dispatch['imports'] = imports.sum(axis=1)
-dispatch['exports'] = -exports.sum(axis=1)
-dispatch['load'] = -dispatch['load']
-dispatch['storage_in'] = -dispatch['storage_in']
+dispatch['exports'] = exports.sum(axis=1)
+
+# translation
+dispatch_de = dispatch[
+    ['run_of_river', 'biomass', 'solar', 'wind', 'uranium', 'lignite',
+     'hard_coal', 'gas', 'storage_out', 'imports', 'exports']]
+dispatch_de = dispatch_de.divide(1000)
 
 # dict with new column names
 en_de = {'run_of_river': 'Laufwasser',
@@ -123,21 +127,44 @@ en_de = {'run_of_river': 'Laufwasser',
          'mixed_fuels': 'Sonstiges',
          'oil': 'Öl',
          'storage_out': 'Pumpspeicher',
-         'imports': 'Importe'}
-
-# translation
-dispatch_de = dispatch[
-    ['run_of_river', 'biomass', 'solar', 'wind', 'uranium', 'lignite',
-     'hard_coal', 'gas', 'storage_out', 'imports']]
-dispatch_de = dispatch_de.divide(1000)
+         'imports': 'Import',
+         'exports': 'Export',
+         'load': 'Last'}
 dispatch_de.rename(columns=en_de, inplace=True)
 
-# plot
+# area plot. gute woche: '2014-01-21':'2014-01-27'
 dispatch_de['2014-01-21':'2014-01-27'].plot(kind='area', stacked=True,
                                             linewidth=0, legend='reverse',
                                             cmap=cm.get_cmap('Spectral'))
 plt.xlabel('Datum')
 plt.ylabel('Leistung in  GW')
 plt.ylim(0, max(dispatch_de.sum(axis=1)) * 1.3)
+plt.show()
 
+# duration curves (sort columns individually)
+curves = pd.concat(
+    [dispatch_de[col].sort_values(ascending=False).reset_index(drop=True)
+     for col in dispatch_de], axis=1)
+curves[['Solar', 'Wind', 'Kernenergie', 'Braunkohle',
+        'Steinkohle', 'Pumpspeicher']].plot(linewidth=2)
+plt.xlabel('Stunden des Jahres')
+plt.ylabel('Leistung in GW')
+plt.show()
+
+# duration curves ordered by load (stacked)
+curves_stacked = pd.concat([dispatch_de,
+                            dispatch['load'].divide(1000),
+                            dispatch['imports'].divide(1000)],
+                           axis=1)
+curves_stacked = curves_stacked.sort_values(by=['load'], ascending=False)
+curves_stacked.reset_index(drop=True, inplace=True)
+
+curves_stacked[['Kernenergie', 'Braunkohle',
+                'Steinkohle', 'Gas', 'Solar',
+                'Wind', 'Import', 'Export']].plot(kind='area', stacked=True,
+                                                  legend='reverse',
+                                                  cmap=cm.get_cmap('Spectral'))
+plt.plot(curves_stacked['load'], linewidth=2)
+plt.xlabel('Stunden des Jahres geordnet nach der Last (rot)')
+plt.ylabel('Leistung in GW')
 plt.show()
