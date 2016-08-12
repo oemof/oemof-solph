73d238ddeb4dcd5cb4704c87ee078a6e05b78458
Martin <martin.sthe@uni-flensburg.de>
2016-08-12 13:15:43 +0200

    Comment out postprocessing

--- examples/solph/nodes_from_csv/renpass_gis_main.py
+++ examples/solph/nodes_from_csv/renpass_gis_main.py
@@ -82,55 +82,55 @@ results.to_csv(os.path.join(results_path, file_name))
 
 # %% postprocessing: write dispatch and prices for all regions to file system
 
-# country codes
-country_codes = ['AT', 'BE', 'CH', 'CZ', 'DE', 'DK', 'FR', 'LU', 'NL', 'NO',
-                 'PL', 'SE']
-
-for cc in country_codes:
-
-    # build single dataframe for electric busses
-    inputs = results.slice_unstacked(bus_label=cc + '_bus_el', type='input',
-                                     date_from=date_from, date_to=date_to,
-                                     formatted=True)
-
-    outputs = results.slice_unstacked(bus_label=cc + '_bus_el', type='output',
-                                      date_from=date_from, date_to=date_to,
-                                      formatted=True)
-
-    other = results.slice_unstacked(bus_label=cc + '_bus_el', type='other',
-                                    date_from=date_from, date_to=date_to,
-                                    formatted=True)
-
-    # AT, DE and LU are treated as one bidding area
-    if cc == 'DE':
-        for c in ['DE', 'AT', 'LU']:
-            # rename redundant columns
-            inputs.rename(columns={c + '_storage_phs':
-                                   c + '_storage_phs_out'},
-                          inplace=True)
-            outputs.rename(columns={c + '_storage_phs':
-                                    c + '_storage_phs_in'},
-                           inplace=True)
-            other.rename(columns={c + '_storage_phs':
-                                  c + '_storage_phs_level'},
-                         inplace=True)
-
-            # data from model in MWh
-            country_data = pd.concat([inputs, outputs, other], axis=1)
-    else:
-        # rename redundant columns
-        inputs.rename(columns={cc + '_storage_phs': cc + '_storage_phs_out'},
-                      inplace=True)
-        outputs.rename(columns={cc + '_storage_phs': cc + '_storage_phs_in'},
-                       inplace=True)
-        other.rename(columns={cc + '_storage_phs': cc + '_storage_phs_level'},
-                     inplace=True)
-
-        # data from model in MWh
-        country_data = pd.concat([inputs, outputs, other], axis=1)
-
-    # sort columns and save as csv file
-    file_name = 'scenario_' + nodes_flows.replace('.csv', '_') + date + '_' + \
-                cc + '.csv'
-    country_data.sort_index(axis=1, inplace=True)
-    country_data.to_csv(os.path.join(results_path, file_name))
+# # country codes
+# country_codes = ['AT', 'BE', 'CH', 'CZ', 'DE', 'DK', 'FR', 'LU', 'NL', 'NO',
+#                  'PL', 'SE']
+#
+# for cc in country_codes:
+#
+#     # build single dataframe for electric busses
+#     inputs = results.slice_unstacked(bus_label=cc + '_bus_el', type='input',
+#                                      date_from=date_from, date_to=date_to,
+#                                      formatted=True)
+#
+#     outputs = results.slice_unstacked(bus_label=cc + '_bus_el', type='output',
+#                                       date_from=date_from, date_to=date_to,
+#                                       formatted=True)
+#
+#     other = results.slice_unstacked(bus_label=cc + '_bus_el', type='other',
+#                                     date_from=date_from, date_to=date_to,
+#                                     formatted=True)
+#
+#     # AT, DE and LU are treated as one bidding area
+#     if cc == 'DE':
+#         for c in ['DE', 'AT', 'LU']:
+#             # rename redundant columns
+#             inputs.rename(columns={c + '_storage_phs':
+#                                    c + '_storage_phs_out'},
+#                           inplace=True)
+#             outputs.rename(columns={c + '_storage_phs':
+#                                     c + '_storage_phs_in'},
+#                            inplace=True)
+#             other.rename(columns={c + '_storage_phs':
+#                                   c + '_storage_phs_level'},
+#                          inplace=True)
+#
+#             # data from model in MWh
+#             country_data = pd.concat([inputs, outputs, other], axis=1)
+#     else:
+#         # rename redundant columns
+#         inputs.rename(columns={cc + '_storage_phs': cc + '_storage_phs_out'},
+#                       inplace=True)
+#         outputs.rename(columns={cc + '_storage_phs': cc + '_storage_phs_in'},
+#                        inplace=True)
+#         other.rename(columns={cc + '_storage_phs': cc + '_storage_phs_level'},
+#                      inplace=True)
+#
+#         # data from model in MWh
+#         country_data = pd.concat([inputs, outputs, other], axis=1)
+#
+#     # sort columns and save as csv file
+#     file_name = 'scenario_' + nodes_flows.replace('.csv', '_') + date + '_' + \
+#                 cc + '.csv'
+#     country_data.sort_index(axis=1, inplace=True)
+#     country_data.to_csv(os.path.join(results_path, file_name))
