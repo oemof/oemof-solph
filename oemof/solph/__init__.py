40c4d2c49e3b9d89d5202c6b9e6854649c9ee133
uwe <uwe.krien@rl-institut.de>
2016-10-20 14:52:35 +0200

    rename groupings module to system module
    As I moved the EnergySystem into this module the old name was confusing.

--- oemof/solph/__init__.py
+++ oemof/solph/__init__.py
@@ -5,6 +5,6 @@
 from oemof.solph.network import (
     Sink, Source, LinearTransformer, Storage, Bus, Flow)
 from oemof.solph.models import OperationalModel
-from oemof.solph.groupings import (GROUPINGS, EnergySystem)
+from oemof.solph.system import (GROUPINGS, EnergySystem)
 from oemof.solph.options import (Investment, BinaryFlow, DiscreteFlow)
 from oemof.solph.plumbing import NodesFromCSV
