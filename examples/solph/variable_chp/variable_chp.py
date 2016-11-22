6ee8f3c0a1ac4d2aa46b38c0b0edc4ccc8ce49a5
uwe <uwe.krien@rl-institut.de>
2016-11-22 18:33:19 +0100

    clean example

--- examples/solph/variable_chp/variable_chp.py
+++ examples/solph/variable_chp/variable_chp.py
@@ -103,7 +103,6 @@ def optimise_storage_size(energysystem, filename="variable_chp.csv",
         inputs={bgas: solph.Flow(nominal_value=10e10)},
         outputs={bel: solph.Flow(), bth: solph.Flow()},
         conversion_factors={bel: 0.3, bth: 0.5},
-        # main_flow_loss_index={bel: 0.4},
         efficiency_condensing={bel: 0.5}
         )
 
