6a44b7a88923c4fe6439ce8bce69cd170262ec16
simnh <simon.hilpert@uni-flensburg.de>
2017-02-22 09:14:10 +0100

    Add docstring to annuity method

--- oemof/tools/economics.py
+++ oemof/tools/economics.py
@@ -6,5 +6,14 @@ Module to collect useful functions for economic calculation.
 
 def annuity(capex, n, wacc):
     """
+    Parameters
+    ----------
+    capex : float
+        Captial expenditure (NPV of investment)
+    n : int
+        Number of years that the investment is used (economic lifetime)
+    wacc : float
+        Weighted average cost of capital
+
     """
     return capex * (wacc * (1 + wacc) ** n) / ((1 + wacc) ** n - 1)
