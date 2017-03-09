b1509a3a8d9af65ce08a75ef3a14bcceae03df72
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2017-03-09 16:06:32 +0100

    Change name to LinearN1Transformer

--- oemof/solph/blocks.py
+++ oemof/solph/blocks.py
@@ -840,32 +840,32 @@ class LinearTransformer(SimpleBlock):
         self.relation_build = BuildAction(rule=_input_output_relation)
 
 
-class LinearM1Transformer(SimpleBlock):
+class LinearN1Transformer(SimpleBlock):
     """Block for the linear relation of nodes with type
-    class:`.LinearM1Transformer`
+    class:`.LinearN1Transformer`
 
 
     **The following constraints are created:**
 
-    Linear relation :attr:`om.LinearM1Transformer.relation[i,o,t]`
+    Linear relation :attr:`om.LinearN1Transformer.relation[i,o,t]`
         .. math::
             flow(i, n, t) \\cdot conversion_factor(i, n, t) = \
             flow(n, o, t), \\\\
             \\forall t \\in \\textrm{TIMESTEPS}, \\\\
-            \\forall n \\in \\textrm{LINEAR\_M1\_TRANSFORMERS}, \\\\
+            \\forall n \\in \\textrm{LINEAR\_N1\_TRANSFORMERS}, \\\\
             \\forall i \\in \\textrm{INPUTS(n)}.
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def _create(self, group=None):
-        """ Creates the linear constraint for the class:`LinearM1Transformer`
+        """ Creates the linear constraint for the class:`LinearN1Transformer`
         block.
 
         Parameters
         ----------
         group : list
-            List of oemof.solph.LinearM1Transformers (trsf) objects for which
+            List of oemof.solph.LinearN1Transformers (trsf) objects for which
             the linear relation of inputs and outputs is created
             e.g. group = [trsf1, trsf2, trsf3, ...]. Note that the relation
             is created for all existing relations of the inputs and all outputs
