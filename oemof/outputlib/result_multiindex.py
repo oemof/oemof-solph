326bb69e2d46b191a1dfc6c4b39ef47e69239b9a
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2017-07-27 15:06:03 +0200

    Create multi-indexed df for flows

--- oemof/outputlib/result_multiindex.py
+++ oemof/outputlib/result_multiindex.py
@@ -6,12 +6,40 @@ import numpy as np
 import pandas as pd
 
 
-def results_to_multiindex(om):
+def results_to_multiindex(es, om):
     """ Returns a multi-indexed dataframe of the results of an optimization
     model.
     """
-    df = pd.DataFrame(np.random.rand(3, 3))
+
+    rows = []
+
+    for i, o in om.flows:  # .items() ?
+
+        row = dict()
+        row['source'] = i
+        row['target'] = o
+        row['datetime'] = es.timeindex
+        row['val'] = [om.flow[i, o, t].value for t in om.TIMESTEPS]
+
+        rows.append(row)
+
+    # split date and value lists to tuples
+    tuples = [
+        (item['source'], item['target'], date, val)
+        for item in rows for date, val
+        in zip(item['datetime'], item['val'])
+    ]
+
+    # print(rows)
+    # print(tuples)
+
+    # create MultiIndex DataFrame
+    index = ['source', 'target', 'datetime']
+    columns = index + ['val']
+    df = pd.DataFrame(tuples, columns=columns)
+    df.set_index(index, inplace=True)
+    df.sort_index(inplace=True)
 
     print(df)
 
-    return df
+    return None
