ef4c69821595404b93ca4b3b050a5ffbc3d172a2
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2017-07-28 17:21:19 +0200

    Add working dict/dataframe API prototype

--- oemof/outputlib/result_multiindex.py
+++ oemof/outputlib/result_multiindex.py
@@ -2,9 +2,7 @@ from collections import UserDict, UserList
 from itertools import groupby
 from ..solph.network import Storage
 from ..solph.options import Investment
-import numpy as np
 import pandas as pd
-import itertools
 
 
 def results_to_multiindex(es, om):
@@ -12,29 +10,12 @@ def results_to_multiindex(es, om):
     Returns a multi-indexed dataframe of the results of an optimization model.
     """
 
-    # create dataframe
-    tuples = [(l, s, n) for l, s in om.flows for n in es.timeindex]
-    levels = ['source', 'target', 'datetime']
-    df = pd.DataFrame(tuples, columns=levels)
+    # create empty dict with flows
+    results = {(source, target): None for source, target in om.flows}
 
-    # add columns
-    df['actual_value'] = [om.flow[i, o, t].value
-                          for i, o in om.flows for t in om.TIMESTEPS]
+    # add data
+    for source, target in om. flows:
+        data = [om.flow[source, target, t].value for t in om.TIMESTEPS]
+        results[(source, target)] = pd.DataFrame(data, index=es.timeindex)
 
-    # this already adds a new column or level which is empty (NaN)
-    # for nodes/flows of other types
-    #if isinstance(i, Storage)
-    #   if i.investment is None -> [om.Storage.capacity[i, t].value for t in om.TIMESTEPS]
-    #   else -> [om.InvestmentStorage.capacity[i, t].value for t in om.TIMESTEPS]
-
-    # scalars for investment
-    #if isinstance(om.flows[i, o].investment, Investment)
-    # -> om.InvestmentFlow.invest[i, o].value
-    #if isinstance(i, Storage)
-    # -> om.InvestmentStorage.invest[i].value
-
-    # set multi-index
-    df.set_index(levels, inplace=True)
-    df.sort_index(inplace=True)
-
-    return print(df)
+    return print(results)
