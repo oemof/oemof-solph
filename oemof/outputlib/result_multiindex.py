df2f7de4fe71793a09516eeed2f9acbe0a3083b8
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2017-07-28 18:38:54 +0200

    Add investment results

--- oemof/outputlib/result_multiindex.py
+++ oemof/outputlib/result_multiindex.py
@@ -1,5 +1,6 @@
 from collections import UserDict, UserList
 from itertools import groupby
+from ..solph.network import Bus
 from ..solph.network import Storage
 from ..solph.options import Investment
 import pandas as pd
@@ -16,23 +17,36 @@ def results_to_multiindex(es, om):
                 'sequences': pd.DataFrame(index=es.timeindex)}
                for k, v in dict.fromkeys(om.flows)}
 
-    # add data for sequences
+    # add data
     for source, target in om.flows:
 
-        # flows
+        # flows (sequences)
         data = [om.flow[source, target, t].value for t in om.TIMESTEPS]
         results[(source, target)]['sequences']['value'] = data
 
-        # storages
+        # storages (sequences)
         if isinstance(source, Storage):
+            results[(source, source)] = \
+                {'sequences': pd.DataFrame(index=es.timeindex)}
             if source.investment is None:
                 data = [om.Storage.capacity[source, t].value
                         for t in om.TIMESTEPS]
             else:
                 data = [om.InvestmentStorage.capacity[source, t].value
                         for t in om.TIMESTEPS]
-            results[(source, source)] = data
-            print('Hallo', results[(source, source)])
-            #['sequences']['soc'] = data
+            results[(source, source)]['sequences']['soc'] = data
+
+        # investment (scalars)
+        if isinstance(om.flows[source, target].investment, Investment):
+            results[(source, target)]['scalars']['investment'] = \
+                om.InvestmentFlow.invest[source, target].value
+            if isinstance(source, Storage):
+                results[(source, source)]['scalars']['investment'] = \
+                    om.InvestmentStorage.invest[source].value
+
+        # # buses
+        # if hasattr(om, 'dual'):
+        #     if isinstance(source, Bus):
+
 
     return results
