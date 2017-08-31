93fa6deea9f58bac2f14ac13acb095e0dfc527b6
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2017-08-31 16:55:54 +0200

    Add init for imports

--- oemof/outputlib/__init__.py
+++ oemof/outputlib/__init__.py
@@ -1,3 +1,3 @@
 from .result_dataframe import ResultsDataFrame, DataFramePlot
 from .result_dictionary import results_to_dict
-from .result_multiindex import results_to_multiindex
+from .result_dict import results_to_dict
