3cec54b7f53613bea55e47cc3c16cbab965dba28
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2017-08-31 15:26:56 +0200

    Clean up and add docstrings

--- oemof/outputlib/result_multiindex.py
+++ oemof/outputlib/result_multiindex.py
@@ -1,19 +1,67 @@
-from collections import UserDict, UserList
-from itertools import groupby
-from ..solph.network import Bus
-from ..solph.network import Storage
-from ..solph.options import Investment
-from oemof.network import Node
+# -*- coding: utf-8 -*-
+"""
+Modules for providing a convenient data structure for solph results.
+
+Information about the possible usage is provided within the examples.
+"""
+
 import pandas as pd
-from pyomo.core.base.var import _VarData, Var
-from pyomo.core.base.constraint import Constraint
+from oemof.network import Node
+from pyomo.core.base.var import Var
+
+
+def get_tuple(x):
+    """
+    Get oemof tuple within iterable or create it.
+
+    Tuples from Pyomo are of type `(n, n, int)`, `(n, n)` and `(n, int)`.
+    For single nodes `n` a tuple with one object `(n,)` is created.
+    """
+    for i in x:
+        if isinstance(i, tuple):
+            return i
+        elif issubclass(type(i), Node):
+            return (i,)
+        else:
+            pass
+
+
+def get_timestep(x):
+    """
+    Get the timestep from oemof tuples.
+
+    The timestep from tuples `(n, n, int)`, `(n, n)`, `(n, int)` and (n,)
+    is fetched as the last element. For time-independent data (scalars)
+    zero ist returned.
+    """
+    if all(issubclass(type(n), Node) for n in x):
+        return 0
+    else:
+        return x[-1]
+
+
+def remove_timestep(x):
+    """
+    Remove the timestep from oemof tuples.
+
+    The timestep is removed from tuples of type `(n, n, int)` and `(n, int)`.
+    """
+    if all(issubclass(type(n), Node) for n in x):
+        return x
+    else:
+        return x[:-1]
 
 
 def results_to_multiindex(es, om):
     """
-    Returns a multi-indexed dataframe of the results of an optimization model.
-    """
+    Create a result dictionary with all optimization data.
 
+    Results from Pyomo are written into a dictionary of pandas objects where
+    a Series holds all scalar values and a dataframe all sequences for nodes
+    and flows.
+    The dictionary is keyed by the nodes e.g. `results[(n,)]['scalars']`
+    and flows e.g. `results[(n,n)]['sequences']`.
+    """
     # get all pyomo variables including their block
     block_vars = []
     for bv in om.component_data_objects(Var):
@@ -21,53 +69,36 @@ def results_to_multiindex(es, om):
     block_vars = list(set(block_vars))
 
     # write them into a dict with tuples as keys
-    dc = {(str(bv).split('.')[0], str(bv).split('.')[-1], i): bv[i].value
-          for bv in block_vars for i in getattr(bv, '_index')}
+    var_dict = {(str(bv).split('.')[0], str(bv).split('.')[-1], i): bv[i].value
+                for bv in block_vars for i in getattr(bv, '_index')}
 
     # use this to create a pandas dataframe
-    df = pd.DataFrame(list(dc.items()), columns=['pyomo_tuple', 'value'])
+    df = pd.DataFrame(list(var_dict.items()), columns=['pyomo_tuple', 'value'])
     df['variable_name'] = df['pyomo_tuple'].str[1]
 
-    def get_tuple(v):
-        for i in v:
-            if isinstance(i, tuple):
-                return i
-            elif issubclass(type(i), Node):
-                return (i,)
-            else:
-                pass
-
+    # adapt the dataframe by separating tuple data into columns depending
+    # on which dimension the variable/parameter has (scalar/sequence).
+    # columns for the oemof tuple and timestep are created
     df['oemof_tuple'] = df['pyomo_tuple'].map(get_tuple)
-
-    def get_timestep(v):
-        if all(issubclass(type(x), Node) for x in v):
-            return 0
-        else:
-            return v[-1]
-
     df['timestep'] = df['oemof_tuple'].map(get_timestep)
-
-    def remove_timestep(v):
-        if all(issubclass(type(x), Node) for x in v):
-            return v
-        else:
-            return v[:-1]
-
     df['oemof_tuple'] = df['oemof_tuple'].map(remove_timestep)
 
-    df.sort_values(['oemof_tuple', 'timestep'], ascending=[True, True],
-                   inplace=True)
+    # order the data by oemof tuple and timestep
+    df = df.sort_values(['oemof_tuple', 'timestep'], ascending=[True, True])
 
-    results = {k: v[['timestep', 'variable_name', 'value']]
+    # create a dict of dataframes keyed by oemof tuples
+    df_dict = {k: v[['timestep', 'variable_name', 'value']]
                for k, v in df.groupby('oemof_tuple')}
 
-    my_dc = {}
-    for k, v in results.items():
-        results[k].set_index('timestep', inplace=True)
-        results[k] = results[k].pivot(columns='variable_name', values='value')
-        results[k].index = es.timeindex
-        scalars = results[k].loc[:, results[k].isnull().any()].dropna().iloc[0]
-        sequences = results[k].loc[:, ~(results[k].isnull().any())]
-        my_dc[k] = {'scalars': scalars, 'sequences': sequences}
+    # create final result dictionary by splitting up the dataframes in the
+    # dataframe dict into a series for scalar data and dataframe for sequences
+    results = {}
+    for k, v in df_dict.items():
+        df_dict[k].set_index('timestep', inplace=True)
+        df_dict[k] = df_dict[k].pivot(columns='variable_name', values='value')
+        df_dict[k].index = es.timeindex
+        scalars = df_dict[k].loc[:, df_dict[k].isnull().any()].dropna().iloc[0]
+        sequences = df_dict[k].loc[:, ~(df_dict[k].isnull().any())]
+        results[k] = {'scalars': scalars, 'sequences': sequences}
 
-    return my_dc
+    return results
