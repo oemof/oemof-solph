fa9363364aed6fa55d28a37102fd5ff55d662521
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2017-09-19 15:36:22 +0200

    Simplify tuple list comprehension

--- oemof/outputlib/result_dict.py
+++ oemof/outputlib/result_dict.py
@@ -129,8 +129,7 @@ def node_results(results, node):
     # create a dataframe with tuples as column labels for sequences
     sequences = {k: v['sequences'] for k, v in results.items()
                  if node in k and not (v['sequences'].empty)}
-    tuples = ['(%s)' % ', '.join([j.label for j in i])
-              for i in sequences.keys()]
+    tuples = [str(tup) for tup in sequences.keys()]
     filtered['sequences'] = pd.concat(sequences.values(), axis=1)
     filtered['sequences'].columns = zip(tuples, filtered['sequences'].columns)
     filtered['sequences'].name = 'sequences'
@@ -139,8 +138,7 @@ def node_results(results, node):
     # create a series with tuples as index labels for scalars
     scalars = {k: v['scalars'] for k, v in results.items()
                if node in k and not (v['scalars'].empty)}
-    tuples = ['(%s)' % ', '.join([j.label for j in i])
-              for i in scalars.keys()]
+    tuples = [str(tup) for tup in scalars.keys()]
     filtered['scalars'] = pd.concat(scalars.values(), axis=0)
     filtered['scalars'].index = zip(tuples, filtered['scalars'].index)
     filtered['scalars'].name = 'scalars'
