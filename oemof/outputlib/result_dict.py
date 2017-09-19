929146210fb4ae07b8c0c077e77e49853fb91ba8
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2017-09-19 16:02:10 +0200

    Change order

--- oemof/outputlib/result_dict.py
+++ oemof/outputlib/result_dict.py
@@ -126,14 +126,6 @@ def node_results(results, node):
     """
     filtered = {}
 
-    # create a dataframe with tuples as column labels for sequences
-    sequences = {k: v['sequences'] for k, v in results.items()
-                 if node in k and not (v['sequences'].empty)}
-    tuples = [str(tup) for tup in sequences.keys()]
-    filtered['sequences'] = pd.concat(sequences.values(), axis=1)
-    filtered['sequences'].columns = zip(tuples, filtered['sequences'].columns)
-    filtered['sequences'].sort_index(axis=1, inplace=True)
-
     # create a series with tuples as index labels for scalars
     scalars = {k: v['scalars'] for k, v in results.items()
                if node in k and not (v['scalars'].empty)}
@@ -141,5 +133,13 @@ def node_results(results, node):
     filtered['scalars'] = pd.concat(scalars.values(), axis=0)
     filtered['scalars'].index = zip(tuples, filtered['scalars'].index)
     filtered['scalars'].sort_index(axis=0, inplace=True)
+
+    # create a dataframe with tuples as column labels for sequences
+    sequences = {k: v['sequences'] for k, v in results.items()
+                 if node in k and not (v['sequences'].empty)}
+    tuples = [str(tup) for tup in sequences.keys()]
+    filtered['sequences'] = pd.concat(sequences.values(), axis=1)
+    filtered['sequences'].columns = zip(tuples, filtered['sequences'].columns)
+    filtered['sequences'].sort_index(axis=1, inplace=True)
 
     return filtered
