697125c330eedd1bcae5cfa7f738ad918ab0d4af
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2017-10-06 16:59:17 +0200

    Adapt parametrisation for counter pressure line

--- examples/solph/generic_chp/generic_chp.py
+++ examples/solph/generic_chp/generic_chp.py
@@ -15,7 +15,7 @@ from oemof.outputlib import processing, views
 data = pd.read_csv('data.csv', sep=",")
 
 # select periods
-periods = len(data[1:24])
+periods = len(data[1:24*365])
 
 # create an energy system
 idx = pd.date_range('1/1/2017', periods=periods, freq='H')
@@ -29,8 +29,8 @@ rgas = solph.Source(label='rgas', outputs={bgas: solph.Flow()})
 # heat
 bth = solph.Bus(label='bth')
 
-source_th = solph.Source(label='source_th', outputs={bth: solph.Flow(
-                         variable_costs=1000)})
+source_th = solph.Sink(label='source_th',
+                       outputs={bth: solph.Flow(variable_costs=15)})
 
 demand_th = solph.Sink(label='demand_th', inputs={bth: solph.Flow(fixed=True,
                        actual_value=data['demand_el'], nominal_value=100)})
@@ -38,22 +38,16 @@ demand_th = solph.Sink(label='demand_th', inputs={bth: solph.Flow(fixed=True,
 # power
 bel = solph.Bus(label='bel')
 
-source_el = solph.Source(label='source_el', outputs={bel: solph.Flow(
-                         variable_costs=1000)})
-
-demand_el = solph.Sink(label='demand_el', inputs={bel: solph.Flow(fixed=True,
-                       actual_value=data['demand_el'], nominal_value=187)})
+demand_el = solph.Sink(label='demand_el', inputs={bel: solph.Flow(
+                       variable_costs=data['price_el'])})
 
 ccgt = solph.custom.GenericCHP(label='pp_generic_chp',
                                inputs={bgas: solph.Flow()},
-                               outputs={bel: solph.Flow(nominal_value=187,
-                                                        variable_costs=300),
+                               outputs={bel: solph.Flow(nominal_value=187),
                                         bth: solph.Flow()},
                                P_max_woDH=187, P_min_woDH=80,
                                Eta_el_max_woDH=0.49, Eta_el_min_woDH=0.41,
-                               Q_CW_min=28,
-                               electrical_bus=bel,
-                               heat_bus=bth,
+                               Q_CW_min=60, electrical_bus=bel, heat_bus=bth,
                                Beta=0.21)
 
 # create a optimization problem and solve it
@@ -64,10 +58,12 @@ om = solph.OperationalModel(es)
 om.write('my_model.lp', io_options={'symbolic_solver_labels': True})
 
 # solve model
-#om.solve(solver='gurobi', solve_kwargs={'tee': True})
+om.solve(solver='gurobi', solve_kwargs={'tee': True})
 
 # create result object
 results = processing.results(es, om)
+
+results[(ccgt,)]['sequences'].to_csv('CCET.csv')
 
 results[(ccgt,)]['sequences']['PQ'] = \
     results[(ccgt,)]['sequences']['P'] / results[(ccgt,)]['sequences']['Q']
@@ -75,15 +71,26 @@ results[(ccgt,)]['sequences']['PQ'] = \
 print(results[(ccgt,)]['sequences'].describe())
 print(results[(ccgt,)]['sequences'].head())
 
-# results[(ccgt,)]['sequences'].plot(kind='line', drawstyle='steps-post')
-# plt.show()
 
-# # plot results
+# plot CCET
+ax = results[(ccgt,)]['sequences'].plot(kind='scatter', x='Q', y='P',
+                                        grid=True)
+ax.set_xlabel('Q (MW)')
+ax.set_ylabel('P (MW)')
+plt.show()
+
+# # plot bus
 # data = views.node(results, 'bel')
-# data['sequences'][(('bel', 'demand'), 'flow')] = \
-#     data['sequences'][(('bel', 'demand'), 'flow')] * -1
 # ax = data['sequences'].plot(kind='line', drawstyle='steps-post', grid=True)
 # ax.set_title('Dispatch')
 # ax.set_xlabel('')
-# ax.set_ylabel('Power in MW')
+# ax.set_ylabel('Power (MW)')
+# plt.show()
+#
+# # plot bus
+# data = views.node(results, 'bth')
+# ax = data['sequences'].plot(kind='line', drawstyle='steps-post', grid=True)
+# ax.set_title('Dispatch')
+# ax.set_xlabel('')
+# ax.set_ylabel('Heat flow (MW)')
 # plt.show()
