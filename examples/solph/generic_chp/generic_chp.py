010b6221d57d24c7d1604e981f7f6ca24418cd96
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2017-10-11 15:08:12 +0200

    Improve outline of missing functionality

--- examples/solph/generic_chp/generic_chp.py
+++ examples/solph/generic_chp/generic_chp.py
@@ -29,7 +29,7 @@ rgas = solph.Source(label='rgas', outputs={bgas: solph.Flow()})
 bth = solph.Bus(label='bth')
 
 source_th = solph.Source(label='source_th',
-                         outputs={bth: solph.Flow(variable_costs=1000)})
+                         outputs={bth: solph.Flow()})
 
 demand_th = solph.Sink(label='demand_th', inputs={bth: solph.Flow(fixed=True,
                        actual_value=data['demand_el'], nominal_value=100)})
@@ -40,18 +40,25 @@ bel = solph.Bus(label='bel')
 demand_el = solph.Sink(label='demand_el', inputs={bel: solph.Flow(
                        variable_costs=data['price_el'])})
 
-# power plant
-pp_gas = solph.LinearTransformer(label='fixed_chp_gas',
-                                 conversion_factors={bel: 0.5, bth: 0.5})
+# power plant (working as expected)
+pp_working = solph.LinearTransformer(
+        label='chp_gas_working',
+        inputs={bgas: solph.Flow()},
+        outputs={bel: solph.Flow(nominal_value=100, variable_costs=2),
+                 bth: solph.Flow(nominal_value=100, variable_costs=2)},
+        conversion_factors={bel: 0.3, bth: 0.5})
 
-
-# outputs seem to be set correctly
-pp_gas.outputs.update({bel: solph.Flow(), bth: solph.Flow()})
-print('Outputs: ', {k.label: v for k, v in pp_gas.outputs.items()})
-
-# inputs are changed via bus output and thus empty (and they work)
-pp_gas.inputs.update({bgas: solph.Flow()})
-print('Inputs: ', {k.label: v for k, v in pp_gas.inputs.items()})
+# power plant (error with subsequently set outputs)
+pp_not_working = solph.LinearTransformer(label='chp_gas_not_working',
+                                         conversion_factors={bel: 0.5, bth: 0.5})
+# in/outputs seem to be set correctly
+# but only input flows occur in bus balances
+# see LP file!
+pp_not_working.outputs.update({bel: solph.Flow(nominal_value=100, variable_costs=2),
+                               bth: solph.Flow(nominal_value=100, variable_costs=2)})
+pp_not_working.inputs.update({bgas: solph.Flow()})
+print('Outputs: ', {k.label: v for k, v in pp_not_working.outputs.items()})
+print('Inputs: ', {k.label: v for k, v in pp_not_working.inputs.items()})
 
 # create an optimization problem and solve it
 om = solph.OperationalModel(es)
@@ -66,5 +73,4 @@ om.solve(solver='glpk', solve_kwargs={'tee': True})
 # @gnn, simnh: have a look at the LP-file!
 # why are the flows into the heat and electrical bus not included in the
 # respective balances?! because inputs and outputs are changed subsequently!?
-# -> see in the network classes constructor (custom.py)
 # weirdly, the flows are existent and included in the objective function...
