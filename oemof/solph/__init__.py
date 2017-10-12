6604bf5281958b9a2670378a3882142c5277ae98
uwe <uwe.krien@rl-institut.de>
2017-10-12 22:10:15 +0200

    add custom module to init

--- oemof/solph/__init__.py
+++ oemof/solph/__init__.py
@@ -10,3 +10,4 @@ from oemof.solph.models import OperationalModel
 from oemof.solph.groupings import GROUPINGS
 from oemof.solph.options import (Investment, BinaryFlow, DiscreteFlow)
 from oemof.solph.inputlib.csv_tools import nodes_from_csv
+from oemof.solph import custom
