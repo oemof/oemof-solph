38b1cf5e3652d9239d6d3fef8ad9f4021e790935
uwe <uwe.krien@rl-institut.de>
2017-10-19 21:59:17 +0200

    replace network class of LinearTransformer with Transformer

--- oemof/solph/network.py
+++ oemof/solph/network.py
@@ -10,7 +10,7 @@ connected.
 import warnings
 import oemof.network as on
 import oemof.energy_system as es
-from .plumbing import sequence
+from oemof.solph.plumbing import sequence
 
 
 class EnergySystem(es.EnergySystem):
@@ -195,8 +195,8 @@ class Source(on.Source):
     pass
 
 
-class LinearTransformer(on.Transformer):
-    """A Linear Transformer object.
+class Transformer(on.Transformer):
+    """A Linear Transformer object with n inputs and n outputs.
 
     Parameters
     ----------
@@ -209,12 +209,11 @@ class LinearTransformer(on.Transformer):
     Examples
     --------
     Defining an linear transformer:
-
     >>> bel = Bus()
     >>> bth = Bus()
     >>> bng = Bus()
-    >>> trsf = LinearTransformer(conversion_factors={bel: 0.4,
-    ...                                              bth: [1, 2, 3]},
+    >>> trsf = Transformer(conversion_factors={bel: 0.4,
+    ...                                        bth: [1, 2, 3]},
     ...                          inputs={bng: Flow()})
     >>> trsf.conversion_factors[bel][3]
     0.4
@@ -222,13 +221,21 @@ class LinearTransformer(on.Transformer):
     Notes
     -----
     The following sets, variables, constraints and objective parts are created
-     * :py:class:`~oemof.solph.blocks.LinearTransformer`
+     * :py:class:`~oemof.solph.blocks.Transformer`
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         self.conversion_factors = {
             k: sequence(v)
             for k, v in kwargs.get('conversion_factors', {}).items()}
+
+        missing_conversion_factor_keys = (
+            (set(self.outputs) | set(self.inputs)) -
+            set(self.conversion_factors))
+
+        for cf in missing_conversion_factor_keys:
+            self.conversion_factors[cf] = sequence(1)
 
 
 class LinearN1Transformer(on.Transformer):
