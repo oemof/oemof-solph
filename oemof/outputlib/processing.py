4157b5a030d04f4795aa25e1df2fd8f3a27bfabd
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2017-10-20 14:56:58 +0200

    Fix outputlib error with empty vars

--- oemof/outputlib/processing.py
+++ oemof/outputlib/processing.py
@@ -85,6 +85,9 @@ def create_dataframe(om):
     # order the data by oemof tuple and timestep
     df = df.sort_values(['oemof_tuple', 'timestep'], ascending=[True, True])
 
+    # drop empty decision variables
+    df = df.dropna(subset=['value'])
+
     return df
 
 
@@ -99,6 +102,8 @@ def results(om):
     and flows e.g. `results[(n,n)]['sequences']`.
     """
     df = create_dataframe(om)
+
+    df.to_csv('df.csv')
 
     # create a dict of dataframes keyed by oemof tuples
     df_dict = {k: v[['timestep', 'variable_name', 'value']]
@@ -119,9 +124,7 @@ def results(om):
         except IndexError:
             error_message = ('Cannot access index on result data. ' +
                              'Did the optimization terminate' +
-                             ' without errors? ' +
-                             'Are decision variables used correctly in ' +
-                             '(newly added) components?')
+                             ' without errors?')
             raise IndexError(error_message)
 
     # add dual variables for bus constraints
