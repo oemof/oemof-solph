3994640592c86e69975094e6b867f1ee021dc3d7
uwe <uwe.krien@rl-institut.de>
2017-10-20 21:53:37 +0200

    remove obsolete classes

--- oemof/solph/network.py
+++ oemof/solph/network.py
@@ -236,38 +236,3 @@ class Transformer(on.Transformer):
 
         for cf in missing_conversion_factor_keys:
             self.conversion_factors[cf] = sequence(1)
-
-
-class LinearN1Transformer(on.Transformer):
-    """A Linear N:1 Transformer object.
-
-    Parameters
-    ----------
-
-    conversion_factors : dict
-        Dictionary containing conversion factors for conversion of inflow(s)
-        to specified outflow. Keys are output bus objects.
-        The dictionary values can either be a scalar or a sequence with length
-        of time horizon for simulation.
-
-    Examples
-    --------
-    Defining an linear transformer:
-
-    >>> gas = Bus()
-    >>> biomass = Bus()
-    >>> trsf = LinearN1Transformer(conversion_factors={gas: 0.4,
-    ...                                                biomass: [1, 2, 3]})
-    >>> trsf.conversion_factors[gas][3]
-    0.4
-
-    Notes
-    -----
-    The following sets, variables, constraints and objective parts are created
-     * :py:class:`~oemof.solph.blocks.LinearN1Transformer`
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.conversion_factors = {
-            k: sequence(v)
-            for k, v in kwargs.get('conversion_factors', {}).items()}
