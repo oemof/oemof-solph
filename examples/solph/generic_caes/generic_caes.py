1225b59bba4c305ea767c28b23ce379bc6a91cfb
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2017-10-23 16:04:39 +0200

    Add working example with dispatch plot

--- examples/solph/generic_caes/generic_caes.py
+++ examples/solph/generic_caes/generic_caes.py
@@ -12,10 +12,10 @@ from oemof.outputlib import processing, views
 
 # read sequence data
 file_name = 'generic_caes.csv'
-data = pd.read_csv(file_name, sep=",")
+input_data = pd.read_csv(file_name, sep=",")
 
 # select periods
-periods = len(data)-1
+periods = len(input_data[0:24*7*2])-1
 
 # create an energy system
 idx = pd.date_range('1/1/2017', periods=periods, freq='H')
@@ -24,13 +24,12 @@ es = solph.EnergySystem(timeindex=idx)
 # resources
 bgas = solph.Bus(label='bgas')
 
-rgas = solph.Source(label='rgas', outputs={bgas: solph.Flow()})
+rgas = solph.Source(label='rgas', outputs={bgas: solph.Flow(variable_costs=20)})
 
 # heat
 bth = solph.Bus(label='bth')
 
-source_th = solph.Source(label='source_th',
-                         outputs={bth: solph.Flow(variable_costs=100)})
+source_th = solph.Source(label='source_th', outputs={bth: solph.Flow()})
 
 demand_th = solph.Sink(label='demand_th', inputs={bth: solph.Flow()})
 
@@ -38,20 +37,49 @@ demand_th = solph.Sink(label='demand_th', inputs={bth: solph.Flow()})
 bel = solph.Bus(label='bel')
 
 source_el = solph.Source(label='source_el',
-                         outputs={bel: solph.Flow(variable_costs=100)})
+                         outputs={bel: solph.Flow(
+                            variable_costs=input_data['price_el_source'])})
 
 # demand_el = solph.Sink(label='demand_el', inputs={bel: solph.Flow(
 #                        variable_costs=data['price_el'])})
-demand_el = solph.Sink(label='demand_el', inputs={bel: solph.Flow(fixed=True,
-                       actual_value=data['demand_el'], nominal_value=100)})
+demand_el = solph.Sink(label='demand_el',
+                       inputs={bel: solph.Flow(
+                            variable_costs=input_data['price_el_demand'])})
+
+# dictionary with parameters for a specific CAES plant
+# based on thermal modelling and linearization techniques
+concept = {
+    "cav_e_in_b": 0,
+    "cav_e_in_m": 0.6457267578,
+    "cav_e_out_b": 0,
+    "cav_e_out_m": 0.3739636077,
+    "cav_eta_temp": 1.0,
+    "cav_level_max": 211.11,
+    "cmp_p_max_b": 86.0918959849,
+    "cmp_p_max_m": 0.0679999932,
+    "cmp_p_min": 1,
+    "cmp_q_out_b": -19.3996965679,
+    "cmp_q_out_m": 1.1066036114,
+    "cmp_q_tes_share": 0,
+    "exp_p_max_b": 46.1294016678,
+    "exp_p_max_m": 0.2528340303,
+    "exp_p_min": 1,
+    "exp_q_in_b": -2.2073411014,
+    "exp_q_in_m": 1.129249765,
+    "exp_q_tes_share": 0,
+    "tau": 0.25,
+    "tes_eta_temp": 1.0,
+    "tes_level_max": 1000000.0
+}
 
 # generic caes plant
 caes = solph.custom.GenericCAES(
     label='compressed_air_energy_storage',
+    electrical_input={bel: solph.Flow()},
     fuel_input={bgas: solph.Flow()},
     electrical_output={bel: solph.Flow()},
     heat_output={bth: solph.Flow()},
-    fixed_costs=0)
+    params=concept, fixed_costs=0)
 
 # create an optimization problem and solve it
 om = solph.OperationalModel(es)
@@ -66,19 +94,20 @@ om.solve(solver='gurobi', solve_kwargs={'tee': True})
 results = processing.results(om)
 
 # store as csv
-#data = results[(caes,)]['sequences'].to_csv('results_' + file_name)
 
-# plot CAES (line)
+# create dataframe with CAES results
 data = results[(caes,)]['sequences']
-ax = data.plot(kind='line', drawstyle='steps-post', grid=True)
-ax.set_xlabel('Time')
-ax.set_ylabel('(MW)')
-plt.show()
+print(data.describe())  # internal decision variables
 
-# plot bus
-data = views.node(results, 'bel')
-ax = data['sequences'].plot(kind='line', drawstyle='steps-post', grid=True)
-ax.set_title('Dispatch')
-ax.set_xlabel('')
-ax.set_ylabel('Power (MW)')
+# add prices
+other = pd.DataFrame()
+other['eex'] = input_data[0:len(data)]['price_el_source']
+other.index = data.index
+data = pd.concat([data, other], axis=1)
+print(data.columns)
+
+# plot dispatch
+columns = ['cav_level', 'cmp_p', 'exp_p', 'eex']
+ax = data[columns].plot(kind='line', drawstyle='steps-post',
+                        grid=True, subplots=True)
 plt.show()
--- oemof/solph/custom.py
+++ oemof/solph/custom.py
@@ -34,6 +34,9 @@ class GenericCAES(Transformer):
 
     Parameters
     ----------
+    electrical_input : dict
+        Dictionary with key-value-pair of `oemof.Bus` and `oemof.Flow` object
+        for the electrical input.
     fuel_input : dict
         Dictionary with key-value-pair of `oemof.Bus` and `oemof.Flow` object
         for the fuel input.
@@ -53,12 +56,14 @@ class GenericCAES(Transformer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.electrical_input = kwargs.get('electrical_input')
         self.fuel_input = kwargs.get('fuel_input')
         self.electrical_output = kwargs.get('electrical_output')
         self.heat_output = kwargs.get('electrical_output')
         self.params = kwargs.get('params')
 
         # map specific flows to standard API
+        self.inputs.update(kwargs.get('electrical_input'))
         self.inputs.update(kwargs.get('fuel_input'))
         self.outputs.update(kwargs.get('electrical_output'))
         self.outputs.update(kwargs.get('heat_output'))
@@ -97,18 +102,254 @@ class GenericCAESBlock(SimpleBlock):
 
         self.GENERICCAES = Set(initialize=[n for n in group])
 
-        # variables
-        self.H_F = Var(self.GENERICCAES, m.TIMESTEPS, within=NonNegativeReals)
+        # Compression: Binary variable for operation status
+        self.cmp_st = Var(self.GENERICCAES, m.TIMESTEPS, within=Binary)
 
-        def _h_flow_connection_rule(block, n, t):
-            """Link fuel consumption to component inflow."""
-            expr = 0
-            expr += self.H_F[n, t]
-            expr += -5
-            expr += - m.flow[list(n.fuel_input.keys())[0], n, t]
-            return expr == 0
-        self.h_flow_connection = Constraint(self.GENERICCAES, m.TIMESTEPS,
-                                            rule=_h_flow_connection_rule)
+        # Compression: Realized capacity
+        self.cmp_p = Var(self.GENERICCAES, m.TIMESTEPS,
+                         within=NonNegativeReals)
+
+        # Compression: Max. Capacity
+        self.cmp_p_max = Var(self.GENERICCAES, m.TIMESTEPS,
+                             within=NonNegativeReals)
+
+        # Compression: Heat flow
+        self.cmp_q_out_sum = Var(self.GENERICCAES, m.TIMESTEPS,
+                                 within=NonNegativeReals)
+
+        # Compression: Waste heat
+        self.cmp_q_waste = Var(self.GENERICCAES, m.TIMESTEPS,
+                               within=NonNegativeReals)
+
+        # Expansion: Binary variable for operation status
+        self.exp_st = Var(self.GENERICCAES, m.TIMESTEPS, within=Binary)
+
+        # Expansion: Realized capacity
+        self.exp_p = Var(self.GENERICCAES, m.TIMESTEPS,
+                         within=NonNegativeReals)
+
+        # Expansion: Max. Capacity
+        self.exp_p_max = Var(self.GENERICCAES, m.TIMESTEPS,
+                             within=NonNegativeReals)
+
+        # Expansion: Heat flow of natural gas co-firing
+        self.exp_q_in_sum = Var(self.GENERICCAES, m.TIMESTEPS,
+                                within=NonNegativeReals)
+
+        # Expansion: Heat flow of natural gas co-firing
+        self.exp_q_fuel_in = Var(self.GENERICCAES, m.TIMESTEPS,
+                                 within=NonNegativeReals)
+
+        # Expansion: Heat flow of additional firing
+        self.exp_q_add_in = Var(self.GENERICCAES, m.TIMESTEPS,
+                                within=NonNegativeReals)
+
+        # Cavern: Filling levelh
+        self.cav_level = Var(self.GENERICCAES, m.TIMESTEPS,
+                             within=NonNegativeReals)
+
+        # Cavern: Energy inflow
+        self.cav_e_in = Var(self.GENERICCAES, m.TIMESTEPS,
+                            within=NonNegativeReals)
+
+        # Cavern: Energy outflow
+        self.cav_e_out = Var(self.GENERICCAES, m.TIMESTEPS,
+                             within=NonNegativeReals)
+
+        # TES: Filling levelh
+        self.tes_level = Var(self.GENERICCAES, m.TIMESTEPS,
+                             within=NonNegativeReals)
+
+        # TES: Energy inflow
+        self.tes_e_in = Var(self.GENERICCAES, m.TIMESTEPS,
+                            within=NonNegativeReals)
+
+        # TES: Energy outflow
+        self.tes_e_out = Var(self.GENERICCAES, m.TIMESTEPS,
+                             within=NonNegativeReals)
+
+        # Spot market: Positive capacity
+        self.exp_p_spot = Var(self.GENERICCAES, m.TIMESTEPS,
+                              within=NonNegativeReals)
+
+        # Spot market: Negative capacity
+        self.cmp_p_spot = Var(self.GENERICCAES, m.TIMESTEPS,
+                              within=NonNegativeReals)
+
+        # Compression: Capacity on markets
+        def cmp_p_constr_rule(block, n, t):
+            return (self.cmp_p[n, t] == m.flow[list(n.electrical_input.keys())[0], n, t])
+        self.cmp_p_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=cmp_p_constr_rule)
+
+        # Compression: Max. capacity depending on cavern filling level
+        def cmp_p_max_constr_rule(block, n, t):
+            if t != 0:
+                return (self.cmp_p_max[n, t] ==
+                        n.params['cmp_p_max_m'] * self.cav_level[n, t-1] +
+                        n.params['cmp_p_max_b'])
+            else:
+                return (self.cmp_p_max[n, t] == n.params['cmp_p_max_b'])
+        self.cmp_p_max_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=cmp_p_max_constr_rule)
+
+        def cmp_p_max_area_constr_rule(block, n, t):
+            return (self.cmp_p[n, t] <= self.cmp_p_max[n, t])
+        self.cmp_p_max_area_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=cmp_p_max_area_constr_rule)
+
+        # Compression: Status of operation (on/off)
+        def cmp_st_p_min_constr_rule(block, n, t):
+            return (self.cmp_p[n, t] >= n.params['cmp_p_min'] * self.cmp_st[n, t])
+        self.cmp_st_p_min_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=cmp_st_p_min_constr_rule)
+
+        def cmp_st_p_max_constr_rule(block, n, t):
+            return (self.cmp_p[n, t] <=
+                    (n.params['cmp_p_max_m'] * n.params['cav_level_max'] +
+                    n.params['cmp_p_max_b']) * self.cmp_st[n, t])
+        self.cmp_st_p_max_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=cmp_st_p_max_constr_rule)
+
+        # Compression: Heat flow out
+        def cmp_q_out_constr_rule(block, n, t):
+            return (self.cmp_q_out_sum[n, t] ==
+                    n.params['cmp_q_out_m'] * self.cmp_p[n, t] +
+                    n.params['cmp_q_out_b'] * self.cmp_st[n, t])
+        self.cmp_q_out_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=cmp_q_out_constr_rule)
+
+        # Compression: Definition of single heat flows
+        def cmp_q_out_sum_constr_rule(block, n, t):
+            return (self.cmp_q_out_sum[n, t] == self.cmp_q_waste[n, t] +
+                    self.tes_e_in[n, t])
+        self.cmp_q_out_sum_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=cmp_q_out_sum_constr_rule)
+
+        # Compression: Heat flow out ratio
+        def cmp_q_out_shr_constr_rule(block, n, t):
+            return (self.cmp_q_waste[n, t] * n.params['cmp_q_tes_share'] ==
+                    self.tes_e_in[n, t] * (1 - n.params['cmp_q_tes_share']))
+        self.cmp_q_out_shr_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=cmp_q_out_shr_constr_rule)
+
+        # Expansion: Capacity on markets
+        def exp_p_constr_rule(block, n, t):
+            return (self.exp_p[n, t] ==
+                    m.flow[n, list(n.electrical_output.keys())[0], t])
+        self.exp_p_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=exp_p_constr_rule)
+
+        # Expansion: Max. capacity depending on cavern filling level
+        def exp_p_max_constr_rule(block, n, t):
+            if t != 0:
+                return (self.exp_p_max[n, t] ==
+                        n.params['exp_p_max_m'] * self.cav_level[n, t-1] +
+                        n.params['exp_p_max_b'])
+            else:
+                return (self.exp_p_max[n, t] == n.params['exp_p_max_b'])
+        self.exp_p_max_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=exp_p_max_constr_rule)
+
+        def exp_p_max_area_constr_rule(block, n, t):
+            return (self.exp_p[n, t] <= self.exp_p_max[n, t])
+        self.exp_p_max_area_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=exp_p_max_area_constr_rule)
+
+        # Expansion: Status of operation (on/off)
+        def exp_st_p_min_constr_rule(block, n, t):
+            return (self.exp_p[n, t] >= n.params['exp_p_min'] * self.exp_st[n, t])
+        self.exp_st_p_min_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=exp_st_p_min_constr_rule)
+
+        def exp_st_p_max_constr_rule(block, n, t):
+            return (self.exp_p[n, t] <=
+                    (n.params['exp_p_max_m'] * n.params['cav_level_max'] +
+                     n.params['exp_p_max_b']) * self.exp_st[n, t])
+        self.exp_st_p_max_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=exp_st_p_max_constr_rule)
+
+        # Expansion: Heat flow in
+        def exp_q_in_constr_rule(block, n, t):
+            return (self.exp_q_in_sum[n, t] ==
+                    n.params['exp_q_in_m'] * self.exp_p[n, t] +
+                    n.params['exp_q_in_b'] * self.exp_st[n, t])
+        self.exp_q_in_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=exp_q_in_constr_rule)
+
+        # Expansion: Fuel allocation
+        def exp_q_fuel_constr_rule(block, n, t):
+            return (self.exp_q_fuel_in[n, t] ==
+                    m.flow[list(n.fuel_input.keys())[0], n, t])
+        self.exp_q_fuel_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=exp_q_fuel_constr_rule)
+
+        # Expansion: Definition of single heat flows
+        def exp_q_in_sum_constr_rule(block, n, t):
+            return (self.exp_q_in_sum[n, t] == self.exp_q_fuel_in[n, t] +
+                    self.tes_e_out[n, t] + self.exp_q_add_in[n, t])
+        self.exp_q_in_sum_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=exp_q_in_sum_constr_rule)
+
+        # Expansion: Heat flow in ratio
+        def exp_q_in_shr_constr_rule(block, n, t):
+            return (n.params['exp_q_tes_share'] * self.exp_q_fuel_in[n, t] ==
+                    (1 - n.params['exp_q_tes_share']) *
+                    (self.exp_q_add_in[n, t] + self.tes_e_out[n, t]))
+        self.exp_q_in_shr_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=exp_q_in_shr_constr_rule)
+
+        # Cavern: Energy inflow
+        def cav_e_in_constr_rule(block, n, t):
+            return (self.cav_e_in[n, t] ==
+                    n.params['cav_e_in_m'] * self.cmp_p[n, t] +
+                    n.params['cav_e_in_b'])
+        self.cav_e_in_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=cav_e_in_constr_rule)
+
+        # Cavern: Energy outflow
+        def cav_e_out_constr_rule(block, n, t):
+            return (self.cav_e_out[n, t] ==
+                    n.params['cav_e_out_m'] * self.exp_p[n, t] +
+                    n.params['cav_e_out_b'])
+        self.cav_e_out_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=cav_e_out_constr_rule)
+
+        # Cavern: Storage balance
+        def cav_eta_constr_rule(block, n, t):
+            if t != 0:
+                return (n.params['cav_eta_temp'] * self.cav_level[n, t] ==
+                        self.cav_level[n, t-1] + n.params['tau'] *
+                        (self.cav_e_in[n, t] - self.cav_e_out[n, t]))
+            else:
+                return (n.params['cav_eta_temp'] * self.cav_level[n, t] ==
+                        n.params['tau'] * (self.cav_e_in[n, t] - self.cav_e_out[n, t]))
+        self.cav_eta_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=cav_eta_constr_rule)
+
+        # Cavern: Upper bound
+        def cav_ub_constr_rule(block, n, t):
+            return (self.cav_level[n, t] <= n.params['cav_level_max'])
+        self.cav_ub_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=cav_ub_constr_rule)
+
+        # TES: Storage balance
+        def tes_eta_constr_rule(block, n, t):
+            if t != 0:
+                return (self.tes_level[n, t] ==
+                        self.tes_level[n, t-1] + n.params['tau'] *
+                        (self.tes_e_in[n, t] - self.tes_e_out[n, t]))
+            else:
+                return (self.tes_level[n, t] ==
+                        n.params['tau'] * (self.tes_e_in[n, t] - self.tes_e_out[n, t]))
+        self.tes_eta_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=tes_eta_constr_rule)
+
+        # TES: Upper bound
+        def tes_ub_constr_rule(block, n, t):
+            return (self.tes_level[n, t] <= n.params['tes_level_max'])
+        self.tes_ub_constr = Constraint(
+            self.GENERICCAES, m.TIMESTEPS, rule=tes_ub_constr_rule)
 
 # ------------------------------------------------------------------------------
 # End of CAES block
