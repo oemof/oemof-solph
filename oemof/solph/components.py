e910edb0e2fb2069fa0d7e87101436d8c48581c4
uwe <uwe.krien@rl-institut.de>
2017-11-24 11:38:29 +0100

    fix docstring

--- oemof/solph/components.py
+++ oemof/solph/components.py
@@ -808,17 +808,14 @@ class GenericCHPBlock(SimpleBlock):
 
 class ExtractionTurbineCHP(Transformer):
     r"""
-    Component `GenericCHP` to model combined heat and power plants.
-
-    A linear transformer with more than one output, where the fraction of
-    the output flows is variable. By now it is restricted to two output flows.
+    A CHP with an extraction turbine in a linear model. For more options see
+    the :class:`~oemof.solph.components.GenericCHP` class.
 
     One main output flow has to be defined and is tapped by the remaining flow.
-    Thus, the main output will be reduced if the tapped output increases.
-    Therefore a loss index has to be defined. Furthermore a maximum efficiency
-    has to be specified if the whole flow is led to the main output
-    (tapped_output = 0). The state with the maximum tapped_output is described
-    through conversion factors equivalent to the LinearTransformer.
+    The conversion factors have to be defined for the maximum tapped flow (
+    full CHP mode) and for no tapped flow (full condensing mode). Even though it
+    is possible to limit the variability of the tapped flow, so that the full
+    condensing mode will never be reached.
 
     Parameters
     ----------
@@ -827,7 +824,7 @@ class ExtractionTurbineCHP(Transformer):
         to specified outflow. Keys are output bus objects.
         The dictionary values can either be a scalar or a sequence with length
         of time horizon for simulation.
-    conversion_factor_single_flow : dict
+    conversion_factor_full_condensation : dict
         The efficiency of the main flow if there is no tapped flow. Only one
         key is allowed. Use one of the keys of the conversion factors. The key
         indicates the main flow. The other output flow is the tapped flow.
@@ -868,14 +865,14 @@ class ExtractionTurbineCHP(Transformer):
 
 class ExtractionTurbineCHPBlock(SimpleBlock):
     r"""Block for the linear relation of nodes with type
-    :class:`~oemof.solph.network.ExtractionTurbineCHP`
+    :class:`~oemof.solph.components.ExtractionTurbineCHP`
 
     **The following sets are created:** (-> see basic sets at
     :class:`.Model` )
 
     VARIABLE_FRACTION_TRANSFORMERS
         A set with all
-        :class:`~oemof.solph.network.ExtractionTurbineCHP` objects.
+        :class:`~oemof.solph.components.ExtractionTurbineCHP` objects.
 
     **The following constraints are created:**
 
