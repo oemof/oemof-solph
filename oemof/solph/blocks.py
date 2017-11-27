02ba1566fd41e0b2f38eae477008af46fd3c6d0f
uwe <uwe.krien@rl-institut.de>
2017-11-27 14:30:29 +0100

    add gradient costs to objective function

--- oemof/solph/blocks.py
+++ oemof/solph/blocks.py
@@ -237,13 +237,11 @@ class Flow(SimpleBlock):
                 fixed_costs += (m.flows[i, o].nominal_value *
                                 m.flows[i, o].fixed_costs)
 
-
-
         self.fixed_costs = Expression(expr=fixed_costs)
         self.variable_costs = Expression(expr=variable_costs)
         self.gradient_costs = Expression(expr=gradient_costs)
 
-        return fixed_costs + variable_costs
+        return fixed_costs + variable_costs + gradient_costs
 
 
 class InvestmentFlow(SimpleBlock):
