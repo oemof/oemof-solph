fff95f46104952d94bdc55e89868378b1ed8938e
hendrik <hendrik.huyskens@rl-institut.de>
2017-11-30 16:16:34 +0100

    Added functions to filter results by node options

--- oemof/outputlib/views.py
+++ oemof/outputlib/views.py
@@ -5,7 +5,11 @@ Modules for providing convenient views for solph results.
 Information about the possible usage is provided within the examples.
 """
 
+from enum import Enum
+from itertools import zip_longest
 import pandas as pd
+
+from oemof.network import Bus
 
 
 def convert_keys_to_strings(results):
@@ -65,3 +69,52 @@ def node(results, node):
         filtered['sequences'].sort_index(axis=1, inplace=True)
 
     return filtered
+
+
+class NodeOption(Enum):
+    All = 0
+    HasOutputs = 1
+    HasInputs = 2
+    HasOnlyOutputs = 3
+    HasOnlyInputs = 4
+
+
+def get_nodes(results, option=NodeOption.All, exclude_busses=False):
+    """
+    Get set of nodes from results-dict for given node option
+
+    See NodeOption for all options. This function filters nodes from results
+    for special needs.
+
+    Parameters
+    ----------
+    results: dict
+    option: NodeOption
+    exclude_busses: bool
+        If set all bus nodes are excluded from resulting node set
+
+    Returns
+    -------
+    :obj:'set' of Node
+    """
+    node_from, node_to = map(
+        lambda x: set(x) - {None},
+        zip_longest(*list(results))
+    )
+    if option == NodeOption.All:
+        nodes = node_from.union(node_to)
+    elif option == NodeOption.HasOutputs:
+        nodes = node_from
+    elif option == NodeOption.HasInputs:
+        nodes = node_to
+    elif option == NodeOption.HasOnlyOutputs:
+        nodes = node_from - node_to
+    elif option == NodeOption.HasOnlyInputs:
+        nodes = node_to - node_from
+    else:
+        raise ValueError('Invalid node option "' + str(option) + '"')
+
+    if exclude_busses:
+        return {n for n in nodes if not isinstance(n, Bus)}
+    else:
+        return nodes
