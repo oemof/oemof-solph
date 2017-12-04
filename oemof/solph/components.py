5df32936cad9c94fb80273c534d4518159d18eb4
simnh <simon.hilpert@uni-flensburg.de>
2017-12-04 12:04:54 +0100

    Add lopf components!

--- oemof/solph/components.py
+++ oemof/solph/components.py
@@ -10,17 +10,16 @@ from pyomo.environ import (Binary, Set, NonNegativeReals, Var, Constraint,
                            Expression, BuildAction)
 import numpy as np
 import warnings
-from oemof.network import Bus, Transformer
-from oemof.solph import Flow, Transformer
+import oemof.network as on
+from .network import Bus, Transformer
 from .options import Investment
 from .plumbing import sequence
-
 
 # ------------------------------------------------------------------------------
 # Start of generic storage component
 # ------------------------------------------------------------------------------
 
-class GenericStorage(Transformer):
+class GenericStorage(on.Transformer):
     """
     Component `GenericStorage` to model with basic characteristics of storages.
 
@@ -474,7 +473,7 @@ class GenericInvestmentStorageBlock(SimpleBlock):
 # Start of generic CHP component
 # ------------------------------------------------------------------------------
 
-class GenericCHP(Transformer):
+class GenericCHP(on.Transformer):
     r"""
     Component `GenericCHP` to model combined heat and power plants.
 
@@ -806,7 +805,7 @@ class GenericCHPBlock(SimpleBlock):
 # Start of ExtractionTurbineCHP component
 # ------------------------------------------------------------------------------
 
-class ExtractionTurbineCHP(Transformer):
+class ExtractionTurbineCHP(on.Transformer):
     r"""
     A CHP with an extraction turbine in a linear model. For more options see
     the :class:`~oemof.solph.components.GenericCHP` class.
@@ -986,7 +985,7 @@ class ExtractionTurbineCHPBlock(SimpleBlock):
 # Start of generic CAES component
 # ------------------------------------------------------------------------------
 
-class GenericCAES(Transformer):
+class GenericCAES(on.Transformer):
     r"""
     Component `GenericCAES` to model arbitrary compressed air energy storages.
 
@@ -1074,6 +1073,147 @@ class GenericCAESBlock(SimpleBlock):
 # End of CAES block
 # ------------------------------------------------------------------------------
 
+# ------------------------------------------------------------------------------
+# ElectricalBus
+# ------------------------------------------------------------------------------
+class ElectricalBus(Bus):
+    r"""A electrical bus object. Every node has to be connected to Bus. This
+    Bus is used in combination with ElectricalLine objects for linear optimal
+    power flow (lopf) simulations
+
+    Notes
+    -----
+    The following sets, variables, constraints and objective parts are created
+     * :py:class:`~oemof.solph.blocks.Bus`
+    The objects are also used inside:
+     * :py:class:`~oemof.solph.blocks.ElectricalLine`
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.slack = kwargs.get('slack', True)
+        self.v_max = kwargs.get('v_max', 1000)
+        self.v_min = kwargs.get('v_min', -1000)
+
+# ------------------------------------------------------------------------------
+#  Electrical Line
+# ------------------------------------------------------------------------------
+class ElectricalLine(Transformer):
+    r"""A Electrical Line to used in linear optimal power flow calculations.
+
+    Parameters
+    ----------
+    reactance : float or array of floats
+        Reactance of the line to be modelled
+
+    Notes
+    ------
+    * To use this object the connected buses need to be of the type
+   `py:class:`~oemof.solph.network.ElectricalBus`.
+    * This object uses the outflow as reference. Please ignore the inflow
+    of this component for parameter setting.
+
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.reactance = sequence(kwargs.get('reactance', 0.00001))
+
+        if len(self.inputs) > 1 or len(self.outputs) > 1:
+            raise ValueError("Component ElectricLine must not have more than \
+                             one input and one output!")
+        self.input = self._input()
+        self.output = self._output()
+
+    def _input(self):
+        """ Returns the first (and only!) input of the line object
+        """
+        return [i for i in self.inputs][0]
+
+    def _output(self):
+        """ Returns the first (and only!) output of the line object
+        """
+        return [o for o in self.outputs][0]
+
+
+class ElectricalLineBlock(SimpleBlock):
+    r"""Block for the linear relation of nodes with type
+    class:`.LinearTransformer`
+
+
+    **The following constraints are created:**
+
+    Linear relation :attr:`om.ElectricalLine.electrical_flow[n,t]`
+        .. math::
+            flow(n, o, t) =  1 / reactance(n, t) \\cdot ()
+            voltage_angle(i(n), t) - volatage_angle(o(n), t), \\\\
+            \\forall t \\in \\textrm{TIMESTEPS}, \\\\
+            \\forall n \\in \\textrm{ELECTRICAL\_LINES}.
+    """
+
+    CONSTRAINT_GROUP = True
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _create(self, group=None):
+        """ Creates the linear constraint for the class:`ElectricalLine`
+        block.
+
+        Parameters
+        ----------
+        group : list
+            List of oemof.solph.ElectricalLine (eline) objects for which
+            the linear relation of inputs and outputs is created
+            e.g. group = [eline1, eline2, ...]. The components inside the
+            list need to hold a attribute `reactance` of type Sequence
+            containing the reactance of the line.
+        """
+        if group is None:
+            return None
+
+        m = self.parent_block()
+
+        I = {n: n._input() for n in group}
+        O = {n: n._output() for n in group}
+
+        # create voltage angle variables
+        self.ELECTRICAL_BUSES = Set(initialize=[n for n in m.es.nodes
+                                    if isinstance(n, ElectricalBus)])
+
+        def _voltage_angle_bounds(block, b, t):
+            return (b.v_min, b.v_max)
+        self.voltage_angle = Var(self.ELECTRICAL_BUSES, m.TIMESTEPS,
+                                 bounds=_voltage_angle_bounds)
+        # TODO: Add bounds for variable
+
+        # add voltage angle constraint
+        self.electrical_flow = Constraint(group, noruleinit=True)
+
+        self._equate_electrical_flows = Constraint(group, noruleinit=True)
+        def _voltage_angle_relation(block):
+            for t in m.TIMESTEPS:
+                for n in group:
+                    try:
+                        lhs = m.flow[n, O[n], t]
+                        rhs = 1 / n.reactance[t] * (
+                            self.voltage_angle[I[n], t] -
+                            self.voltage_angle[O[n], t])
+                    except:
+                        raise ValueError("Error in constraint creation",
+                                         "of node {}".format(n.label))
+                    block.electrical_flow.add((n, t), (lhs == rhs))
+
+                    block._equate_electrical_flows.add((n, t), (m.flow[n, O[n],t] == m.flow[I[n], n, t]))
+
+        self.electrical_flow_build = BuildAction(rule=_voltage_angle_relation)
+
+        def _bound_in_out_flow(block):
+            return m
+# ------------------------------------------------------------------------------
+#  End of ElectricalLine block
+# ------------------------------------------------------------------------------
+
 
 def component_grouping(node):
     if isinstance(node, GenericStorage) and isinstance(node.investment,
@@ -1086,3 +1226,5 @@ def component_grouping(node):
         return GenericCHPBlock
     if isinstance(node, ExtractionTurbineCHP):
         return ExtractionTurbineCHPBlock
+    if isinstance(node, ElectricalLine):
+        return ElectricalLineBlock
--- oemof/solph/models.py
+++ oemof/solph/models.py
@@ -5,7 +5,7 @@
 import pyomo.environ as po
 from pyomo.opt import SolverFactory
 from pyomo.core.plugins.transform.relax_integrality import RelaxIntegrality
-from oemof.solph import blocks
+from oemof.solph import blocks, components
 from oemof.solph.plumbing import sequence
 from oemof.outputlib import processing
 import logging
@@ -96,11 +96,15 @@ class Model(po.ConcreteModel):
 
         # non-negative pyomo variable for all existing flows in energysystem
         self.flow = po.Var(self.FLOWS, self.TIMESTEPS,
-                           within=po.NonNegativeReals)
+                           within=po.Reals)
+
 
         # loop over all flows and timesteps to set flow bounds / values
         for (o, i) in self.FLOWS:
             for t in self.TIMESTEPS:
+                if not isinstance(i, components.ElectricalLine) and \
+                    not isinstance(o, components.ElectricalLine):
+                    self.flow[o, i, t].setlb(0)
                 if self.flows[o, i].actual_value[t] is not None and (
                         self.flows[o, i].nominal_value is not None):
                     # pre- optimized value of flow variable
