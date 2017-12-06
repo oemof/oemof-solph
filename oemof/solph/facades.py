77d8990669afe7c135179e5c46d403bd38aa6bab
simnh <simon.hilpert@uni-flensburg.de>
2017-12-06 12:02:06 +0100

    Add custom and facades module to solph

---
+++ oemof/solph/facades.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+""" This module is desined to contain classes that act as simplified / reduced
+energy specific interfaces (facades) for solph components to simplify its
+application.
+"""
