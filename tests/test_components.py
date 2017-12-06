252756fb012943f11f277b2f1442183a9fffcb50
uwe <uwe.krien@rl-institut.de>
2017-12-06 17:24:34 +0100

    fix tests

--- tests/test_components.py
+++ tests/test_components.py
@@ -3,9 +3,9 @@ from oemof import solph
 
 
 @tools.raises(AttributeError)
-def test_generic_storage():
+def test_generic_storage_1():
     """Duplicate definition inflow."""
-    bel = solph.Bus(label='bel')
+    bel = solph.Bus()
     solph.components.GenericStorage(
         label='storage',
         nominal_capacity=45,
@@ -19,9 +19,9 @@ def test_generic_storage():
 
 
 @tools.raises(AttributeError)
-def test_generic_storage():
+def test_generic_storage_2():
     """Duplicate definition inflow."""
-    bel = solph.Bus(label='bel')
+    bel = solph.Bus()
     solph.components.GenericStorage(
         label='storage',
         nominal_capacity=45,
@@ -35,9 +35,9 @@ def test_generic_storage():
 
 
 @tools.raises(AttributeError)
-def test_generic_storage():
+def test_generic_storage_3():
     """Nominal value defined with investment model."""
-    bel = solph.Bus(label='bel')
+    bel = solph.Bus()
     solph.components.GenericStorage(
         label='storage',
         nominal_capacity=45,
@@ -49,3 +49,17 @@ def test_generic_storage():
         inflow_conversion_factor=1, outflow_conversion_factor=0.8,
         fixed_costs=35,
         investment=solph.Investment(ep_costs=23))
+
+
+def test_generic_storage_4():
+    """Nominal value defined with investment model."""
+    bel = solph.Bus()
+    solph.components.GenericStorage(
+        label='storage',
+        nominal_capacity=45,
+        inputs={bel: solph.Flow(nominal_value=23, variable_costs=10e10)},
+        outputs={bel: solph.Flow(variable_costs=10e10)},
+        capacity_loss=0.00, initial_capacity=0,
+        nominal_output_capacity_ratio=1/6,
+        inflow_conversion_factor=1, outflow_conversion_factor=0.8,
+        fixed_costs=35)
