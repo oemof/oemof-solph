1b7a66b50dc4f5ac4f6b2d5382bf9c1a6627dfea
simnh <simon.hilpert@uni-flensburg.de>
2017-12-06 12:47:50 +0100

    Update Generator in facades

--- oemof/solph/facades.py
+++ oemof/solph/facades.py
@@ -3,3 +3,22 @@
 energy specific interfaces (facades) for solph components to simplify its
 application.
 """
+from oemof.solph import Source, Flow
+
+
+class Generator(Source):
+    """
+    """
+    def __init__(self, **kwargs):
+        super().__init__()
+
+        self.Pmax = kwargs.get('Pmax', None)
+
+        self.bus = kwargs.get('bus', None)
+
+        self.opex = kwargs.get('opex', 0)
+
+        self.flh = kwargs.get('flh', (None, None))
+
+        self.outputs.update({self.bus: Flow(nominal_value=self.Pmax,
+                                            variable_costs=self.opex)})
