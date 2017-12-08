2a192137b68fad9e80ad913b08eed70cbc54f3d8
simnh <simon.hilpert@uni-flensburg.de>
2017-12-08 17:32:48 +0100

    Add Link component and block

--- oemof/solph/custom.py
+++ oemof/solph/custom.py
@@ -115,6 +115,16 @@ class ElectricalLineBlock(SimpleBlock):
             voltage_angle(i(n), t) - volatage_angle(o(n), t), \\
             \forall t \\in \\textrm{TIMESTEPS}, \\
             \forall n \\in \\textrm{ELECTRICAL\_LINES}.
+
+    TODO: Add equate constraint of flows
+
+    **The following variable are created:**
+
+    TODO: Add voltage angle variable
+
+    TODO: Add fix slack bus voltage angle to zero constraint / bound
+
+    TODO: Add tests
     """
 
     CONSTRAINT_GROUP = True
@@ -186,6 +196,118 @@ class ElectricalLineBlock(SimpleBlock):
                                          rule=_voltage_angle_relation)
 
 
+class Link(Transformer):
+    """A Link object with 1...2 inputs and 1...2 outputs.
+
+    Parameters
+    ----------
+    conversion_factors : dict
+        Dictionary containing conversion factors for conversion of each flow.
+        Keys are the connected tuples (input, output) bus objects.
+        The dictionary values can either be a scalar or a sequence with length
+        of time horizon for simulation.
+
+    Examples
+    --------
+    Defining an linear transformer:
+
+    >>> from oemof import solph
+    >>> bel0 = solph.Bus(label="el0")
+    >>> bel1 = solph.Bus(label="el1")
+
+    >>> link = solph.custom.Link(
+    ...    label="transshipment_link",
+    ...    inputs={bel0: solph.Flow(), bel1: solph.Flow()},
+    ...    outputs={be0: solph.Flow(), bel1: solph.Flow()},
+    ...    conversion_factors={(bel0, bel1): 0.92, (bel1, bel0): 0.99})
+    >>> print(sorted([x[1][5] for x in link.conversion_factors.items()]))
+    [0.92, 0.99]
+
+    >>> type(link)
+    <class 'oemof.solph.network.Transformer'>
+
+    >>> sorted([str(i) for i in link.inputs])
+    ['el1', 'el0']
+
+    >>> link.conversion_factors[(bel0, bel1)][3]
+    0.92
+
+    Notes
+    -----
+    The following sets, variables, constraints and objective parts are created
+     * :py:class:`~oemof.solph.custom.LinkBlock`
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if len(self.inputs) > 2 or len(self.outputs) > 2:
+            raise ValueError("Component `Link` must not have more than \
+                             2 inputs and 2 outputs!")
+
+        self.conversion_factors = {
+            k: sequence(v)
+            for k, v in kwargs.get('conversion_factors', {}).items()}
+
+
+class LinkBlock(SimpleBlock):
+    r"""Block for the relation of nodes with type
+    :class:`~oemof.solph.custom.Link`
+
+    **The following constraints are created:**
+
+    TODO: Add description for constrainst
+
+    TODO: Add tests
+
+    """
+    CONSTRAINT_GROUP = True
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _create(self, group=None):
+        """ Creates the relation for the class:`Link`.
+
+        Parameters
+        ----------
+        group : list
+            List of oemof.solph.custom.Link objects for which
+            the relation of inputs and outputs is created
+            e.g. group = [link1, link2, link3, ...]. The components inside
+            the list need to hold an attribute `conversion_factors` of type
+            dict containing the conversion factors for all inputs to outputs.
+        """
+        if group is None:
+            return None
+
+        m = self.parent_block()
+
+        all_conversions = {}
+        for n in group:
+            all_conversions[n] = {
+                            k: v for k, v in n.conversion_factors.items()}
+
+        def _input_output_relation(block):
+            for t in m.TIMESTEPS:
+                for n, conversion in all_conversions.items():
+                    for cidx, c in conversion.items():
+                        try:
+                            expr = (m.flow[n, cidx[1], t] ==
+                                    c[t] * m.flow[cidx[0], n, t])
+                        except ValueError:
+                            raise ValueError(
+                                "Error in constraint creation",
+                                "from: {0}, to: {1}, via: {3}".format(
+                                    cidx[0], cidx[1], n))
+                    block.relation.add((n, cidx[0], cidx[1], t), (expr))
+
+        self.relation = po.Constraint(group, noruleinit=True)
+
+        self.relation_build = po.BuildAction(rule=_input_output_relation)
+
+
 def custom_component_grouping(node):
     if isinstance(node, ElectricalLine):
         return ElectricalLineBlock
+    if type(node) is Link:
+        return LinkBlock
