923a489005491a0f06845e01065258991302fa7b
uwe <uwe.krien@rl-institut.de>
2018-01-09 21:46:22 +0100

    remove graph plot

--- oemof/tools/graph.py
+++ oemof/tools/graph.py
@@ -5,7 +5,6 @@
 __copyright__ = "oemof developer group"
 __license__ = "GPLv3"
 
-import warnings
 import networkx as nx
 
 try:
@@ -123,82 +122,6 @@ def create_graph(energy_system=None, optimization_model=None, remove_nodes=None,
                             if i in v.label]
             grph.remove_nodes_from(remove_nodes)
     return grph
-
-
-def draw_graph(grph, edge_labels=True, node_color='#AFAFAF',
-               edge_color='#CFCFCF', plot=True, node_size=2000,
-               with_labels=True, arrows=True, layout='neato'):
-    """
-    Draw a graph. This function will be removed in future versions.
-
-    Parameters
-    ----------
-    grph : networkxGraph
-        A graph to draw.
-    edge_labels : boolean
-        Use nominal values of flow as edge label
-    node_color : dict or string
-        Hex color code oder matplotlib color for each node. If string, all
-        colors are the same.
-
-    edge_color : string
-        Hex color code oder matplotlib color for edge color.
-
-    plot : boolean
-        Show matplotlib plot.
-
-    node_size : integer
-        Size of nodes.
-
-    with_labels : boolean
-        Draw node labels.
-
-    arrows : boolean
-        Draw arrows on directed edges. Works only if an optimization_model has
-        been passed.
-    layout : string
-        networkx graph layout, one of: neato, dot, twopi, circo, fdp, sfdp.
-    """
-    warnings.warn(
-        "\nThe function to plot the graph is deprecated and will be removed "
-        "in oemof >= 0.3.0.")
-
-    if plt is not None or pygraphviz is not None:
-
-        if type(node_color) is dict:
-            node_color = [node_color.get(g, '#AFAFAF') for g in grph.nodes()]
-
-        # set drawing options
-        options = {
-         'prog': 'dot',
-         'with_labels': with_labels,
-         'node_color': node_color,
-         'edge_color': edge_color,
-         'node_size': node_size,
-         'arrows': arrows
-        }
-
-        # draw graph
-        pos = nx.drawing.nx_agraph.graphviz_layout(grph, prog=layout)
-
-        nx.draw(grph, pos=pos, **options)
-
-        # add edge labels for all edges
-        if edge_labels is True and plt:
-            labels = nx.get_edge_attributes(grph, 'weight')
-            nx.draw_networkx_edge_labels(grph, pos=pos, edge_labels=labels)
-
-        # show output
-        if plot is True:
-            plt.show()
-
-    else:
-        if plt is None:
-            warnings.warn(
-                "Graph cannot be drawn due to the missing matplotlib package.")
-        if pygraphviz is None:
-            warnings.warn(
-                "Graph cannot be drawn due to the missing pygraphviz package.")
 
 
 if __name__ == '__main__':
