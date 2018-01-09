f07874163247d7c0e587e964d6bc4455a574fb5b
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2018-01-09 15:00:13 +0100

    Fix components module due to wrong merge

--- oemof/solph/components.py
+++ oemof/solph/components.py
@@ -71,10 +71,7 @@ class GenericStorage(network.Transformer):
 
     Examples
     --------
-    Basic usage examples of the GenericStorage with a random selection of# ------------------------------------------------------------------------------
-# Start of generic storage component
-# ------------------------------------------------------------------------------
-
+    Basic usage examples of the GenericStorage with a random selection of
     attributes. See the Flow class for all Flow attributes.
 
     >>> from oemof import solph
@@ -551,15 +548,7 @@ class GenericCHP(network.Transformer):
         self.outputs.update(kwargs.get('electrical_output'))
         self.outputs.update(kwargs.get('heat_output'))
 
-    def _calculate_alphas(self):# ------------------------------------------------------------------------------
-# End of generic storage invest block
-# ------------------------------------------------------------------------------
-
-
-# ------------------------------------------------------------------------------
-# Start of generic CHP component
-# ------------------------------------------------------------------------------
-
+    def _calculate_alphas(self):
         """
         Calculate alpha coefficients.
 
@@ -951,6 +940,83 @@ class ExtractionTurbineCHPBlock(SimpleBlock):
         self.out_flow_relation = Constraint(group, noruleinit=True)
         self.out_flow_relation_build = BuildAction(
                 rule=_out_flow_relation_rule)
+
+
+class GenericCAES(network.Transformer):
+    r"""
+    Component `GenericCAES` to model arbitrary compressed air energy storages.
+
+    The full set of equations is described in:
+    Kaldemeyer, C.; Boysen, C.; Tuschy, I.
+    A Generic Formulation of Compressed Air Energy Storage as
+    Mixed Integer Linear Program – Unit Commitment of Specific
+    Technical Concepts in Arbitrary Market Environments
+    Materials Today: Proceedings 00 (2018) 0000–0000
+    [currently in review]
+
+    Parameters
+    ----------
+    fuel_input : dict
+        Dictionary with key-value-pair of `oemof.Bus` and `oemof.Flow` object
+        for the fuel input.
+    electrical_output : dict
+        Dictionary with key-value-pair of `oemof.Bus` and `oemof.Flow` object
+        for the electrical output.
+    heat_output : dict
+        Dictionary with key-value-pair of `oemof.Bus` and `oemof.Flow` object
+        for the electrical output.
+
+    Notes
+    -----
+    The following sets, variables, constraints and objective parts are created
+     * :py:class:`~oemof.solph.components.GenericCAESBlock`
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fuel_input = kwargs.get('fuel_input')
+        self.electrical_output = kwargs.get('electrical_output')
+        self.heat_output = kwargs.get('electrical_output')
+        self.params = kwargs.get('params')
+
+
+class GenericCAESBlock(SimpleBlock):
+    r"""Block for nodes of class:`.GenericCAES`."""
+
+    CONSTRAINT_GROUP = True
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _create(self, group=None):
+        """
+        Create constraints for GenericCAESBlock.
+
+        Parameters
+        ----------
+        group : list
+            List containing `.GenericCAES` objects.
+            e.g. groups=[gcaes1, gcaes2,..]
+        """
+        m = self.parent_block()
+
+        if group is None:
+            return None
+
+        self.GENERICCAES = Set(initialize=[n for n in group])
+
+        # variables
+        self.H_F = Var(self.GENERICCHPS, m.TIMESTEPS, within=NonNegativeReals)
+
+        def _H_flow_rule(block, n, t):
+            """Link fuel consumption to component inflow."""
+            expr = 0
+            expr += self.H_F[n, t]
+            expr += - m.flow[list(n.fuel_input.keys())[0], n, t]
+            return expr == 0
+        self.H_flow = Constraint(self.GENERICCHPS, m.TIMESTEPS,
+                                 rule=_H_flow_rule)
 
 
 def component_grouping(node):
