20d9e827cf72024272ca8deb5605526d28e26d0a
hendrik <hendrik.huyskens@rl-institut.de>
2018-01-16 15:14:54 +0100

    Param_results can now detect sequences with default-only values as scalars

--- oemof/outputlib/processing.py
+++ oemof/outputlib/processing.py
@@ -260,9 +260,12 @@ def __separate_attrs(om, get_flows=False, exclude_none=True):
                 com['scalars'][key] = value
                 del com['sequences'][key]
             else:
-                if len(value) == 0:
-                    com['scalars'][key] = value.default
-                    del com['sequences'][key]
+                try:
+                    if not value.default_changed:
+                        com['scalars'][key] = value.default
+                        del com['sequences'][key]
+                except AttributeError:
+                    pass
 
     def remove_nones(com):
         for key, value in list(com['scalars'].items()):
