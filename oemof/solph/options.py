4b0f1ac14085e672c21fad3b275f38b512c545cb
Cord Kaldemeyer <cord.kaldemeyer@fh-flensburg.de>
2018-02-07 16:08:55 +0100

    Adapt docstring

--- oemof/solph/options.py
+++ oemof/solph/options.py
@@ -38,12 +38,21 @@ class NonConvex:
         Costs associated with the shutdown of the flow (representing a unit).
     minimum_uptime : numeric (1 or positive integer)
         Minimum time that a flow must be greater then its minimum flow after
-        startup.
+        startup. Be aware that minimum up and downtimes can contradict each
+        other and may lead to infeasible problems.
     minimum_downtime : numeric (1 or positive integer)
         Minimum time a flow is forced to zero after shutting down.
+        Be aware that minimum up and downtimes can contradict each
+        other and may to infeasible problems.
     initial_status : numeric (0 or 1)
         Integer value indicating the status of the flow in the first time step
-        (0 = off, 1 = on).
+        (0 = off, 1 = on). For minimum up and downtimes, the initial status
+        is set for the respective values in the edge regions e.g. if a
+        minimum uptime of four timesteps is defined, the initial status is
+        fixed for the four first and last timesteps of the optimization period.
+        If both, up and downtimes are defined, the initial status is set for
+        the maximum of both e.g. for six timesteps if a minimum downtime of
+        six timesteps is defined in addition to a four timestep minimum uptime.
     """
     def __init__(self, **kwargs):
         # super().__init__(self, **kwargs)
