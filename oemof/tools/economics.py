c16ea7ac85e0935cad4741106a05f670e985c9a3
caroline.moeller <caroline.moeller@rl-institut.de>
2018-11-23 16:02:21 +0100

    Adapt docstring

--- oemof/tools/economics.py
+++ oemof/tools/economics.py
@@ -13,16 +13,34 @@ SPDX-License-Identifier: GPL-3.0-or-later
 def annuity(capex, n, wacc, **kwargs):
     """Calculate the annuity.
 
+    In case of a single initial investment:
+
     annuity = capex * (wacc * (1 + wacc) ** n) / ((1 + wacc) ** n - 1)
+    
+    or in case of repeated investments at fixed intervals 'u'
+        with decreasing costs 'cost_decrease':
+
+    annuity = capex * (wacc * (1 + wacc) ** n) / ((1 + wacc) ** n - 1)*
+              ((1-((1-cost_decrease)/(1+wacc))**n)/
+              (1-((1-cost_decrease)/(1+wacc))**u)))
 
     Parameters
     ----------
     capex : float
-        Capital expenditure (NPV of investment)
+        Capital expenditure for first investment (NPV of investment)
     n : int
-        Number of years that the investment is used (economic lifetime)
+        Number of years investigated; might be an integer multiple of technical
+        lifetime (u) in case of repeated investments; in case of a single
+        investment n must equal u (economic lifetime)
     wacc : float
         Weighted average cost of capital
+    u : int
+        Number of years that a single investment is used, i.e. the technical
+        lifetime of a single investment
+    cost_decrease : float
+        Annual rate of cost_decrease for repeated investments; takes the value '0'
+        if not set otherwise, that is in case of a single investment or in case
+        of no cost decrease for repeated investments
 
     Returns
     -------
@@ -32,6 +50,6 @@ def annuity(capex, n, wacc, **kwargs):
     u  = kwargs.get('u', n)
     cost_decrease = kwargs.get('cost_decrease', 0)
 
-    return (capex * (wacc * (1 + wacc) ** n) / ((1 + wacc) ** n - 1)
-                    *((1-((1-cost_decrease)/(1+wacc))**n)/
+    return (capex * (wacc * (1 + wacc) ** n) / ((1 + wacc) ** n - 1)*
+                    ((1-((1-cost_decrease)/(1+wacc))**n)/
                     (1-((1-cost_decrease)/(1+wacc))**u)))
