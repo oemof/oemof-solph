3ad080ced9f79c0d5d9bc65009b348267847644f
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2019-06-18 14:15:34 +0200

    Print logfile

--- tests/build_doc.sh
+++ tests/build_doc.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-errorlog_file="doc_errors.log"
+errorlog_file="/tmp/doc_errors.log"
 
-sphinx-build -q doc/ doc/_build/ 2> doc_errors.log >/dev/null
+sphinx-build -q doc/ doc/_build/ 2> ${errorlog_file} >/dev/null
 
 numerrors=`grep -icE 'error|warning' ${errorlog_file}`
 
+echo ""
+cat ${errorlog_file}
+
 if [ ${numerrors} -ne 0 ]; then
-  echo Doc creation produced ${numerrors} errors and warnings:
-  echo ""
-  cat ${errorlog_file}
+  echo Doc creation produced ${numerrors} errors and warnings.
   exit 1
 fi
 
