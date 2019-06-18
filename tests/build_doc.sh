1123122eefa586e8f8defa744a3801e60c6a91e8
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2019-06-18 15:13:55 +0200

    Improve readability of doc creation error log

--- tests/build_doc.sh
+++ tests/build_doc.sh
@@ -6,11 +6,13 @@ sphinx-build -q doc/ doc/_build/ 2> ${errorlog_file} >/dev/null
 
 numerrors=`grep -icE 'error|warning' ${errorlog_file}`
 
-echo ""
-cat ${errorlog_file}
 
 if [ ${numerrors} -ne 0 ]; then
-  echo Doc creation produced ${numerrors} errors and warnings.
+  echo "Doc creation error log:"
+  sed 's/^/>\t/' ${errorlog_file}
+  echo ""
+  echo ""
+  echo "Doc creation produced ${numerrors} errors and warnings."
   exit 1
 fi
 
