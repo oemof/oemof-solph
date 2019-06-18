b4e431fe184dc06a55e00bdbd340752a7f053d97
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2019-06-18 13:54:25 +0200

    Make doc test more pedantic

--- tests/build_doc.sh
+++ tests/build_doc.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-numerrors=`sphinx-build -q doc/ doc/_build/ 2>&1 >/dev/null | grep -ic 'error'`
+errorlog_file="doc_errors.log"
+
+sphinx-build -q doc/ doc/_build/ 2> doc_errors.log >/dev/null
+
+numerrors=`grep -icE 'error|warning' ${errorlog_file}`
 
 if [ ${numerrors} -ne 0 ]; then
+  echo Doc creation produced ${numerrors} errors and warnings:
+  echo ""
+  cat ${errorlog_file}
   exit 1
 fi
 
