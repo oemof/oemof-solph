ad13902716c8dcaa52e12f98d84031527556bf33
uvchik <uvchik.git@posteo.eu>
2019-12-06 09:57:35 +0100

    Add a UserWarning for typical problems to facilitate debugging
    A UserWarning should be raised for untypical uses even though this kind
    of usage is valid if you really know what you are doing, whereas in 90%
    of the cases it would be a mistake. This will help users to debug their
    code but can be turned of for experienced users.

---
+++ oemof/tools/debugging.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Module contains tools facilitating debugging
+
+This file is part of project oemof (github.com/oemof/oemof). It's copyrighted
+by the contributors recorded in the version control history of the file,
+available from its original location oemof/oemof/tools/economics.py
+
+SPDX-License-Identifier: MIT
+"""
+
