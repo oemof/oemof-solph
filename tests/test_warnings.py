60519d5bb03dce1ffabf3e833ede1c1b439f9830
Stephan Günther <gnn.code@gmail.com>
2019-12-06 12:07:58 +0100

    Test that the Sink warns about missing inputs
    The test fails for now, which is intentional. The code to make it pass
    should be written afterwards, so that one can have some confidence that
    the code actually does what it is supposed to do.

--- tests/test_warnings.py
+++ tests/test_warnings.py
@@ -9,8 +9,16 @@ available from its original location oemof/tests/tool_tests.py
 SPDX-License-Identifier: MIT
 """
 
+from nose.tools import assert_raises
+
 from oemof import network
+from oemof.tools.debugging import SuspiciousUsageWarning
 
 
-def test_sink_warning():
-    network.Sink(imputs={'a': 5})
+def test_that_the_sink_warnings_actually_get_raised():
+    """ Sink doesn't warn about potentially erroneous usage.
+    """
+    with assert_raises(
+        SuspiciousUsageWarning, msg="Sink constructed without `inputs`."
+    ):
+        network.Sink(imputs={"Look out!": "A typo!"})
