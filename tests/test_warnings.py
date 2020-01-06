a9031fe5e9e344b436f1cd72d49bb6f5e502f5ab
uvchik <uvchik.git@posteo.eu>
2020-01-06 11:39:49 +0100

    Add real Node to avoid error and adapt to warn

--- tests/test_warnings.py
+++ tests/test_warnings.py
@@ -10,6 +10,7 @@ SPDX-License-Identifier: MIT
 """
 
 from nose.tools import assert_raises_regexp
+import warnings
 from oemof import network
 from oemof.tools.debugging import SuspiciousUsageWarning
 
@@ -17,25 +18,36 @@ from oemof.tools.debugging import SuspiciousUsageWarning
 def test_that_the_sink_warnings_actually_get_raised():
     """ Sink doesn't warn about potentially erroneous usage.
     """
+    look_out = network.Bus()
     msg = "`Sink` constructed without `inputs`."
-    with assert_raises_regexp(SuspiciousUsageWarning, msg):
-        network.Sink(outputs={"Look out!": "A typo!"})
+    with warnings.catch_warnings(record=True) as w:
+        network.Sink(outputs={look_out: "A typo!"})
+        assert len(w) == 1
+        assert msg in str(w[-1].message)
 
 
 def test_that_the_source_warnings_actually_get_raised():
     """ Source doesn't warn about potentially erroneous usage.
     """
+    look_out = network.Bus()
     msg = "`Source` constructed without `outputs`."
-    with assert_raises_regexp(SuspiciousUsageWarning, msg):
-        network.Source(inputs={"Look out!": "A typo!"})
+    with warnings.catch_warnings(record=True) as w:
+        network.Source(inputs={look_out: "A typo!"})
+        assert len(w) == 1
+        assert msg in str(w[-1].message)
 
 
 def test_that_the_transformer_warnings_actually_get_raised():
     """ Transformer doesn't warn about potentially erroneous usage.
     """
+    look_out = network.Bus()
     msg = "`Transformer` constructed without `inputs"
-    with assert_raises_regexp(SuspiciousUsageWarning, msg):
-        network.Transformer(outputs={"Look out!": "No inputs!"})
+    with warnings.catch_warnings(record=True) as w:
+        network.Transformer(outputs={look_out: "No inputs!"})
+        assert len(w) == 1
+        assert msg in str(w[-1].message)
     msg = "`Transformer` constructed without `outputs"
-    with assert_raises_regexp(SuspiciousUsageWarning, msg):
-        network.Transformer(inputs={"Look out!": "No outputs!"})
+    with warnings.catch_warnings(record=True) as w:
+        network.Transformer(inputs={look_out: "No outputs!"})
+        assert len(w) == 1
+        assert msg in str(w[-1].message)
