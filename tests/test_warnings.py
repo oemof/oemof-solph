af153e99687ff6a8ffdf849c3b21be981032c772
uvchik <uvchik.git@posteo.eu>
2020-01-06 17:09:16 +0100

    Use setup function to activate warnings

--- tests/test_warnings.py
+++ tests/test_warnings.py
@@ -9,16 +9,21 @@ available from its original location oemof/tests/tool_tests.py
 SPDX-License-Identifier: MIT
 """
 
-from nose.tools import eq_, ok_
+from nose.tools import eq_, ok_, with_setup
 import warnings
 from oemof import network
 from oemof.tools.debugging import SuspiciousUsageWarning
 
 
+def setup_func():
+    """Explicitly activate the warnings."""
+    warnings.filterwarnings("always", category=SuspiciousUsageWarning)
+
+
+@with_setup(setup_func)
 def test_that_the_sink_warnings_actually_get_raised():
     """ Sink doesn't warn about potentially erroneous usage.
     """
-    warnings.filterwarnings("always", category=SuspiciousUsageWarning)
     look_out = network.Bus()
     msg = "`Sink` constructed without `inputs`."
     with warnings.catch_warnings(record=True) as w:
@@ -27,6 +32,7 @@ def test_that_the_sink_warnings_actually_get_raised():
         eq_(msg, str(w[-1].message))
 
 
+@with_setup(setup_func)
 def test_filtered_warning():
     """ Sink doesn't warn about potentially erroneous usage.
     """
@@ -35,9 +41,9 @@ def test_filtered_warning():
     with warnings.catch_warnings(record=True) as w:
         network.Sink(outputs={look_out: "A typo!"})
         ok_(len(w) == 0)
-    warnings.filterwarnings("always", category=SuspiciousUsageWarning)
 
 
+@with_setup(setup_func)
 def test_that_the_source_warnings_actually_get_raised():
     """ Source doesn't warn about potentially erroneous usage.
     """
@@ -49,6 +55,7 @@ def test_that_the_source_warnings_actually_get_raised():
         eq_(msg, str(w[-1].message))
 
 
+@with_setup(setup_func)
 def test_that_the_transformer_warnings_actually_get_raised():
     """ Transformer doesn't warn about potentially erroneous usage.
     """
