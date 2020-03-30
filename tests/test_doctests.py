a212194ff91df1d306e27d1d4b99f13dd304b50a
uvchik <uvchik.git@posteo.eu>
2020-03-30 12:01:58 +0200

    Fix doctest integration in nose

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -1,6 +1,7 @@
 import doctest
 
-from nose import main
+from nose import run
+from nose.tools import eq_
 from nose.suite import LazySuite
 
 from oemof.solph import components
@@ -10,8 +11,48 @@ from oemof.solph import network
 from oemof.solph import plumbing
 
 
-def run_my_tests():
-    """Collect doctest and integrate them in nose's test run."""
-    modules = [components, constraints, custom, network, plumbing]
-    suite = LazySuite([doctest.DocTestSuite(x) for x in modules])
-    main(suite=suite)
+def test_doctests_in_components_module():
+    """
+    Doctest in components module failed. See nose protocol for more
+    information.
+    """
+    module = components
+    status = run(suite=LazySuite(doctest.DocTestSuite(module)))
+    eq_((module.__name__, status), (module.__name__, True))
+
+
+def test_doctests_in_constraints_module():
+    """
+    Doctest in constraints module failed. See nose protocol for more
+    information.
+    """
+    module = constraints
+    status = run(suite=LazySuite(doctest.DocTestSuite(module)))
+    eq_((module.__name__, status), (module.__name__, True))
+
+
+def test_doctests_in_custom_module():
+    """
+    Doctest in custom module failed. See nose protocol for more information.
+    """
+    module = custom
+    status = run(suite=LazySuite(doctest.DocTestSuite(module)))
+    eq_((module.__name__, status), (module.__name__, True))
+
+
+def test_doctests_in_network_module():
+    """
+    Doctest in network module failed. See nose protocol for more information.
+    """
+    module = network
+    status = run(suite=LazySuite(doctest.DocTestSuite(module)))
+    eq_((module.__name__, status), (module.__name__, True))
+
+
+def test_doctests_in_plumbing_module():
+    """
+    Doctest in plumbing module failed. See nose protocol for more information.
+    """
+    module = plumbing
+    status = run(suite=LazySuite(doctest.DocTestSuite(module)))
+    eq_((module.__name__, status), (module.__name__, True))
