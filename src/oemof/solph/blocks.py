86b9feded964425f82deb72096b2c8e1cdf4d119
philipp235 <70669937+philipp235@users.noreply.github.com>
2020-09-09 00:06:58 +0200

    Update blocks.py

--- src/oemof/solph/blocks.py
+++ src/oemof/solph/blocks.py
@@ -26,6 +26,85 @@ from pyomo.core import Var
 from pyomo.core.base.block import SimpleBlock
 
 
+class GasLineBlock(SimpleBlock):
+    CONSTRAINT_GROUP = True
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _create(self, group=None):
+        if group is None:
+            return None
+
+        m = self.parent_block()
+
+        self.GASLINE_PL = Set(initialize=[n for n in group])
+
+        self.GAS_BUSES = Set(initialize=[s for s in m.es.nodes
+                                         if isinstance(s, GasBus)])
+
+        self.pressure = Var(self.GAS_BUSES, m.TIMESTEPS, bounds=(0, 1))
+
+        self.delta_pressure = Var(self.GASLINE_PL, m.TIMESTEPS, bounds=(-1, 1))
+
+        self.energy_flow = Var(self.GASLINE_PL, m.TIMESTEPS)
+
+        for n in self.GASLINE_PL:
+            for t in m.TIMESTEPS:
+                for ob in list(n.outputs.keys()):
+                    if ob.slack is True:
+                        self.pressure[ob, t].value = 1
+                        self.pressure[ob, t].fix()
+
+                for ob in list(n.inputs.keys()):
+                    if ob.slack is True:
+                        self.pressure[ob, t].value = 1
+                        self.pressure[ob, t].fix()
+
+        self.piecewise = Piecewise(self.GASLINE_PL,
+                                   m.TIMESTEPS,
+                                   self.energy_flow,
+                                   self.delta_pressure,
+                                   pw_pts=n.input_list,
+                                   pw_constr_type='EQ',
+                                   pw_repn='CC',
+                                   f_rule=n.output_list)
+
+        def flow_eq_pressure(block, n, t):
+            expr = 0
+            expr += (self.pressure[list(n.outputs.keys())[0], t] -
+                     self.pressure[list(n.inputs.keys())[0], t])
+            expr += self.delta_pressure[n, t]
+
+            return expr == 0
+
+        self.flow_eq_pressure = Constraint(self.GASLINE_PL,
+                                           m.TIMESTEPS,
+                                           rule=flow_eq_pressure)
+
+        def energy_flow_out(block, n, t):
+            expr = 0
+            expr += - m.flow[n, list(n.outputs.keys())[0], t]
+            expr += self.energy_flow[n, t]*n.K_1
+
+            return expr == 0
+
+        self.energy_flow_out = Constraint(self.GASLINE_PL,
+                                          m.TIMESTEPS,
+                                          rule=energy_flow_out)
+
+        def energy_flow_in(block, n, t):
+            expr = 0
+            expr += - m.flow[list(n.inputs.keys())[0], n, t]*n.conv_factor
+            expr += self.energy_flow[n, t]*n.K_1
+
+            return expr == 0
+
+        self.energy_flow_in = Constraint(self.GASLINE_PL,
+                                         m.TIMESTEPS,
+                                         rule=energy_flow_in)
+        
+        
 class Flow(SimpleBlock):
     r""" Flow block with definitions for standard flows.
 
