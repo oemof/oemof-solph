f2b37539c42fe8b4811e5884ae8155ffa541d9fe
Johannes Kochems <kochems@tu-berlin.de>
2021-01-15 15:45:44 +0100

    Adjust blocks module for multiperiod modeling
    - Add MultiPeriodBus and MultiPeriodTransformer

--- src/oemof/solph/blocks.py
+++ src/oemof/solph/blocks.py
@@ -1038,6 +1038,54 @@ class Bus(SimpleBlock):
         self.balance_build = BuildAction(rule=_busbalance_rule)
 
 
+class MultiPeriodBus(SimpleBlock):
+    r"""Block for all balanced buses.
+
+    **The following constraints are build:**
+
+    Bus balance  :attr:`om.Bus.balance[i, o, t]`
+      .. math::
+        \sum_{i \in INPUTS(n)} flow(i, n, t) =
+        \sum_{o \in OUTPUTS(n)} flow(n, o, t), \\
+        \forall n \in \textrm{BUSES},
+        \forall t \in \textrm{TIMESTEPS}.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _create(self, group=None):
+        """Creates the balance constraints for the class:`Bus` block.
+
+        Parameters
+        ----------
+        group : list
+            List of oemof bus (b) object for which the bus balance is created
+            e.g. group = [b1, b2, b3, .....]
+        """
+        if group is None:
+            return None
+
+        m = self.parent_block()
+
+        ins = {}
+        outs = {}
+        for n in group:
+            ins[n] = [i for i in n.inputs]
+            outs[n] = [o for o in n.outputs]
+
+        def _busbalance_rule(block):
+            for p, t in m.TIMEINDEX:
+                for g in group:
+                    lhs = sum(m.flow[i, g, p, t] for i in ins[g])
+                    rhs = sum(m.flow[g, o, p, t] for o in outs[g])
+                    expr = (lhs == rhs)
+                    # no inflows no outflows yield: 0 == 0 which is True
+                    if expr is not True:
+                        block.balance.add((g, p, t), expr)
+        self.balance = Constraint(group, m.TIMEINDEX, noruleinit=True)
+        self.balance_build = BuildAction(rule=_busbalance_rule)
+
+
 class Transformer(SimpleBlock):
     r"""Block for the linear relation of nodes with type
     :class:`~oemof.solph.network.Transformer`
@@ -1121,6 +1169,92 @@ class Transformer(SimpleBlock):
                                     "source: {0}, target: {1}".format(
                                         n.label, o.label))
                             block.relation.add((n, i, o, t), (lhs == rhs))
+        self.relation_build = BuildAction(rule=_input_output_relation)
+
+
+class MultiPeriodTransformer(SimpleBlock):
+    r"""Block for the linear relation of nodes with type
+    :class:`~oemof.solph.network.Transformer`
+
+    **The following sets are created:** (-> see basic sets at
+    :class:`.Model` )
+
+    TRANSFORMERS
+        A set with all :class:`~oemof.solph.network.Transformer` objects.
+
+    **The following constraints are created:**
+
+    Linear relation :attr:`om.Transformer.relation[i,o,t]`
+        .. math::
+            \P_{i,n}(t) \times \eta_{n,o}(t) = \
+            \P_{n,o}(t) \times \eta_{n,i}(t), \\
+            \forall t \in \textrm{TIMESTEPS}, \\
+            \forall n \in \textrm{TRANSFORMERS}, \\
+            \forall i \in \textrm{INPUTS(n)}, \\
+            \forall o \in \textrm{OUTPUTS(n)},
+
+    ======================  ====================================  =============
+    symbol                  attribute                             explanation
+    ======================  ====================================  =============
+    :math:`P_{i,n}(t)`      :py:obj:`flow[i, n, t]`               Transformer
+                                                                  inflow
+
+    :math:`P_{n,o}(t)`      :py:obj:`flow[n, o, t]`               Transformer
+                                                                  outflow
+
+    :math:`\eta_{i,n}(t)`   :py:obj:`conversion_factor[i, n, t]`  Conversion
+                                                                  efficiency
+
+    ======================  ====================================  =============
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _create(self, group=None):
+        """ Creates the linear constraint for the class:`Transformer`
+        block.
+        Parameters
+        ----------
+        group : list
+            List of oemof.solph.Transformers objects for which
+            the linear relation of inputs and outputs is created
+            e.g. group = [trsf1, trsf2, trsf3, ...]. Note that the relation
+            is created for all existing relations of all inputs and all outputs
+            of the transformer. The components inside the list need to hold
+            an attribute `conversion_factors` of type dict containing the
+            conversion factors for all inputs to outputs.
+        """
+        if group is None:
+            return None
+
+        m = self.parent_block()
+
+        in_flows = {n: [i for i in n.inputs.keys()] for n in group}
+        out_flows = {n: [o for o in n.outputs.keys()] for n in group}
+
+        self.relation = Constraint(
+            [(n, i, o, p, t)
+             for p, t in m.TIMEINDEX
+             for n in group
+             for o in out_flows[n]
+             for i in in_flows[n]], noruleinit=True)
+
+        def _input_output_relation(block):
+            for p, t in m.TIMEINDEX:
+                for n in group:
+                    for o in out_flows[n]:
+                        for i in in_flows[n]:
+                            try:
+                                lhs = (m.flow[i, n, p, t] *
+                                       n.conversion_factors[o][t])
+                                rhs = (m.flow[n, o, p, t] *
+                                       n.conversion_factors[i][t])
+                            except ValueError:
+                                raise ValueError(
+                                    "Error in constraint creation",
+                                    "source: {0}, target: {1}".format(
+                                        n.label, o.label))
+                            block.relation.add((n, i, o, p, t), (lhs == rhs))
         self.relation_build = BuildAction(rule=_input_output_relation)
 
 
