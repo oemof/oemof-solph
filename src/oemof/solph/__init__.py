d908fb24454676aaa3da7f2495681a1316d6ccba
Johannes Kochems <kochems@tu-berlin.de>
2021-03-05 09:14:35 +0100

    Fix __init__.py
    - options.MultiPeriod is not existing
    - Fix PEP8 issue

--- src/oemof/solph/__init__.py
+++ src/oemof/solph/__init__.py
@@ -10,7 +10,7 @@ from .components import GenericStorage  # noqa: F401
 from .components import OffsetTransformer  # noqa: F401
 from .groupings import GROUPINGS  # noqa: F401
 from .models import Model  # noqa: F401
-from .models import MultiPeriodModel # noqa: F401
+from .models import MultiPeriodModel  # noqa: F401
 from .network import Bus  # noqa: F401
 from .network import EnergySystem  # noqa: F401
 from .network import Flow  # noqa: F401
@@ -18,8 +18,7 @@ from .network import Sink  # noqa: F401
 from .network import Source  # noqa: F401
 from .network import Transformer  # noqa: F401
 from .options import Investment  # noqa: F401
-from .options import MultiPeriod # noqa: F401
-from .options import MultiPeriodInvestment # noqa: F401
+from .options import MultiPeriodInvestment  # noqa: F401
 from .options import NonConvex  # noqa: F401
 from .plumbing import sequence  # noqa: F401
 from .processing import parameter_as_dict  # noqa: F401
