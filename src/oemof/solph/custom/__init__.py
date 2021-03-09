bcb2d1303bc4149374ecc9c36c81b79cc4c48786
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2021-03-09 11:33:47 +0100

    Double-ignore unused import in __init__

--- src/oemof/solph/custom/__init__.py
+++ src/oemof/solph/custom/__init__.py
@@ -4,7 +4,7 @@ from .electrical_line import ElectricalBus  # noqa: F401
 from .electrical_line import ElectricalLine  # noqa: F401
 from .generic_caes import GenericCAES  # noqa: F401
 from .link import Link  # noqa: F401
-from .piecewise_linear_transformer import (
+from .piecewise_linear_transformer import (  # noqa: F401
     PiecewiseLinearTransformer,  # noqa: F401
 )
 from .sink_dsm import SinkDSM  # noqa: F401
