b4ec2a2cc4baaad662b3b809b2246407efc7c804
Patrik Schönfeldt <patrik@sedna.snft.de>
2021-03-09 09:44:47 +0100

    Split custom into submodule

---
+++ src/oemof/solph/custom/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from .electrical_line import ElectricalBus  # noqa: F401
+from .electrical_line import ElectricalLine  # noqa: F401
+from .generic_caes import GenericCAES  # noqa: F401
+from .link import Link  # noqa: F401
+from .piecewise_linear_transformer import (
+    PiecewiseLinearTransformer)  # noqa: F401
+from .sink_dsm import SinkDSM  # noqa: F401
