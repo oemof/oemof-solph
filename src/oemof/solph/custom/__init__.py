b8f11561c43917e0aeabcc5ec3c577c16d3a2beb
Patrik Schönfeldt <patrik@sedna.snft.de>
2021-03-12 17:12:23 +0100

    Set isort to be compatile with black

--- src/oemof/solph/custom/__init__.py
+++ src/oemof/solph/custom/__init__.py
@@ -4,6 +4,7 @@ from .electrical_line import ElectricalBus  # noqa: F401
 from .electrical_line import ElectricalLine  # noqa: F401
 from .generic_caes import GenericCAES  # noqa: F401
 from .link import Link  # noqa: F401
-from .piecewise_linear_transformer import \
-    PiecewiseLinearTransformer  # noqa: F401
+from .piecewise_linear_transformer import (  # noqa: F401
+    PiecewiseLinearTransformer,
+)
 from .sink_dsm import SinkDSM  # noqa: F401
