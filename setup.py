916aa76d5fb1413885f8440bba1788fd3723aa40
uvchik <uvchik.git@posteo.eu>
2021-05-11 17:37:44 +0200

    Exclude pyomo version that causes a too long computing time

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup(
         "dill",
         "numpy",
         "pandas",
-        "pyomo >= 5.7.0, < 6.0",
+        "pyomo >= 5.7.0, !=5.7.3",
         "networkx",
         "oemof.tools",
         "oemof.network",
