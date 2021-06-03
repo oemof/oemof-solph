21afe9024e1d8e527c7c344d83e52f264fa4424d
Patrik Schönfeldt <patrik@sedna.snft.de>
2021-06-03 09:45:42 +0200

    Set version to "0.4.5.dev0"

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ long_description = "%s" % (
 
 setup(
     name="oemof.solph",
-    version="0.4.4",
+    version="0.4.5.dev0",
     license="MIT",
     description=(
         "A model generator for energy system modelling and optimisation."
