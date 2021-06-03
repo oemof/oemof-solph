9c85d76e6463be4c7954f1d433a2512f91e52dac
Johannes Kochems <jokochems@web.de>
2021-06-03 17:48:53 +0200

    Optimize imports

--- src/oemof/solph/blocks/__init__.py
+++ src/oemof/solph/blocks/__init__.py
@@ -9,8 +9,8 @@ from .flow import Flow  # noqa: F401
 from .investment_flow import InvestmentFlow  # noqa: F401
 from .multiperiod_bus import MultiPeriodBus  # noqa: F401
 from .multiperiod_flow import MultiPeriodFlow  # noqa: F401
+from .multiperiod_non_convex_flow import MultiPeriodNonConvexFlow  # noqa: F401
 from .multiperiod_transformer import MultiPeriodTransformer  # noqa: F401
 from .multiperiodinvestment_flow import MultiPeriodInvestmentFlow  # noqa: F401
 from .non_convex_flow import NonConvexFlow  # noqa: F401
-from .multiperiod_non_convex_flow import MultiPeriodNonConvexFlow  # noqa: F401
 from .transformer import Transformer  # noqa: F401
