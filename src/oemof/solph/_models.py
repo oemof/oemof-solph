ef938a7c29c7382d46c957e47a6c37a96b6835d0
Johannes Kochems <jokochems@web.de>
2021-06-05 16:12:58 +0200

    Revise / fix docs for models module

--- src/oemof/solph/_models.py
+++ src/oemof/solph/_models.py
@@ -7,6 +7,7 @@ SPDX-FileCopyrightText: Simon Hilpert
 SPDX-FileCopyrightText: Cord Kaldemeyer
 SPDX-FileCopyrightText: gplssm
 SPDX-FileCopyrightText: Patrik Schönfeldt
+SPDX-FileCopyrightText: Johannes Kochems (jokochems)
 
 SPDX-License-Identifier: MIT
 
@@ -41,7 +42,8 @@ class BaseModel(po.ConcreteModel):
     objective_weighting : array like (optional)
         Weights used for temporal objective function
         expressions. If nothing is passed `timeincrement` will be used which
-        is calculated from the freq length of the energy system timeindex .
+        is calculated from the freq length of the energy system timeindex or
+        can be directly passed as a sequence.
     auto_construct : boolean
         If this value is true, the set, variables, constraints, etc. are added,
         automatically when instantiating the model. For sequential model
@@ -49,8 +51,8 @@ class BaseModel(po.ConcreteModel):
         and use methods `_add_parent_block_sets`,
         `_add_parent_block_variables`, `_add_blocks`, `_add_objective`
 
-    Attributes:
-    -----------
+    Attributes
+    ----------
     timeincrement : sequence
         Time increments.
     flows : dict
@@ -61,14 +63,19 @@ class BaseModel(po.ConcreteModel):
         Energy system of the model.
     meta : `pyomo.opt.results.results_.SolverResults` or None
         Solver results.
-    dual : ... or None
-    rc : ... or None
-
+    dual : `pyomo.core.base.suffix.Suffix` or None
+        Store the dual variables of the model if pyomo suffix is set to IMPORT
+    rc : `pyomo.core.base.suffix.Suffix` or None
+        Store the reduced costs of the model if pyomo suffix is set to IMPORT
     """
 
     CONSTRAINT_GROUPS = []
+    """The default list of constraint groups to be used for a model."""
 
     def __init__(self, energysystem, **kwargs):
+        """Initialize a BaseModel, using its energysystem as well as
+        optional kwargs for specifying the timeincrement, objective_weigting
+        and constraint groups."""
         super().__init__()
 
         # ########################  Arguments #################################
@@ -117,30 +124,33 @@ class BaseModel(po.ConcreteModel):
             self._construct()
 
     def _construct(self):
-        """ """
+        """Construct a BaseModel by adding parent block sets and variables
+        as well as child blocks and variables to it."""
         self._add_parent_block_sets()
         self._add_parent_block_variables()
         self._add_child_blocks()
         self._add_objective()
 
     def _add_parent_block_sets(self):
-        """ " Method to create all sets located at the parent block, i.e. the
-        model itself as they are to be shared across all model components.
+        """Method to create all sets located at the parent block, i.e. in the
+        model itself, as they are to be shared across all model components.
+        See the class :py:class:~oemof.solph.models.Model for the sets created.
         """
         pass
 
     def _add_parent_block_variables(self):
-        """ " Method to create all variables located at the parent block,
+        """Method to create all variables located at the parent block,
         i.e. the model itself as these variables  are to be shared across
-        all model components.
+        all model components. See the class :py:class:~oemof.solph.models.Model
+        for the `flow` variable created.
         """
         pass
 
     def _add_child_blocks(self):
         """Method to add the defined child blocks for components that have
-        been grouped in the defined constraint groups.
+        been grouped in the defined constraint groups. This collects all the
+        constraints from the component blocks and adds them to the model.
         """
-
         for group in self._constraint_groups:
             # create instance for block
             block = group()
@@ -171,7 +181,6 @@ class BaseModel(po.ConcreteModel):
         """Method sets solver suffix to extract information about dual
         variables from solver. Shadow prices (duals) and reduced costs (rc) are
         set as attributes of the model.
-
         """
         # shadow prices
         self.dual = po.Suffix(direction=po.Suffix.IMPORT)
@@ -179,7 +188,9 @@ class BaseModel(po.ConcreteModel):
         self.rc = po.Suffix(direction=po.Suffix.IMPORT)
 
     def results(self):
-        """Returns a nested dictionary of the results of this optimization"""
+        """Returns a nested dictionary of the results of this optimization.
+        See the processing module for more information on results extraction.
+        """
         return processing.results(self)
 
     def solve(self, solver="cbc", solver_io="lp", **kwargs):
@@ -188,7 +199,7 @@ class BaseModel(po.ConcreteModel):
         Parameters
         ----------
         solver : string
-            solver to be used e.g. "glpk","gurobi","cplex"
+            solver to be used e.g. "cbc", "glpk","gurobi","cplex"
         solver_io : string
             pyomo solver interface file format: "lp","python","nl", etc.
         \**kwargs : keyword arguments
@@ -202,10 +213,9 @@ class BaseModel(po.ConcreteModel):
         cmdline_options : dict
             Dictionary with command line options for solver e.g.
             {"mipgap":"0.01"} results in "--mipgap 0.01"
-            {"interior":" "} results in "--interior"
-            Gurobi solver takes numeric parameter values such as
+            \{"interior":" "} results in "--interior"
+            \Gurobi solver takes numeric parameter values such as
             {"method": 2}
-
         """
         solve_kwargs = kwargs.get("solve_kwargs", {})
         solver_cmdline_options = kwargs.get("cmdline_options", {})
@@ -247,7 +257,7 @@ class BaseModel(po.ConcreteModel):
 
 
 class Model(BaseModel):
-    """An  energy system model for operational and investment
+    """An  energy system model for operational and/or investment
     optimization.
 
     Parameters
@@ -257,17 +267,18 @@ class Model(BaseModel):
     constraint_groups : list
         Solph looks for these groups in the given energy system and uses them
         to create the constraints of the optimization problem.
-        Defaults to `Model.CONSTRAINTS`
+        Defaults to `Model.CONSTRAINT_GROUPS`
+
 
     **The following basic sets are created**:
 
-    NODES :
+    NODES
         A set with all nodes of the given energy system.
 
-    TIMESTEPS :
+    TIMESTEPS
         A set with all timesteps of the given time horizon.
 
-    FLOWS :
+    FLOWS
         A 2 dimensional set with all flows. Index: `(source, target)`
 
     **The following basic variables are created**:
@@ -291,7 +302,7 @@ class Model(BaseModel):
         super().__init__(energysystem, **kwargs)
 
     def _add_parent_block_sets(self):
-        """ """
+        """Add all basic sets to the model, i.e. NODES, TIMESTEPS and FLOWS."""
         # set with all nodes
         self.NODES = po.Set(initialize=[n for n in self.es.nodes])
 
@@ -334,7 +345,8 @@ class Model(BaseModel):
         )
 
     def _add_parent_block_variables(self):
-        """ """
+        """Add the parent block variables, which is the `flow` variable,
+        indexed by FLOWS and TIMESTEPS."""
         self.flow = po.Var(self.FLOWS, self.TIMESTEPS, within=po.Reals)
 
         for (o, i) in self.FLOWS:
