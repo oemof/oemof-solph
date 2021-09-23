8adeb58707a2a65bc70a586e95555f10ca33b137
uvchik <uvchik.git@posteo.eu>
2021-09-23 09:32:26 +0200

    Test the new error message

---
+++ tests/flow_tests.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -
+
+"""Testing the flows.
+
+This file is part of project oemof (github.com/oemof/oemof). It's copyrighted
+by the contributors recorded in the version control history of the file,
+available from its original location oemof/tests/test_components.py
+
+SPDX-License-Identifier: MIT
+"""
+
+import warnings
+
+import pytest
+from oemof.tools.debugging import SuspiciousUsageWarning
+
+from oemof.solph import Flow
+
+
+def test_error_in_gradient_attribute():
+    msg = "Only the key 'ub' is allowed for the '{0}' attribute"
+    with pytest.raises(AttributeError, match=msg.format("negative_gradient")):
+        Flow(negative_gradient={"costs": 5})
+    with pytest.raises(AttributeError, match=msg.format("positive_gradient")):
+        Flow(positive_gradient={"something": 5})
