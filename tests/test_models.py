fd8c1c69093712ebea4d3656bc4f958fcb21013f
uvchik <uvchik.git@posteo.eu>
2021-12-20 22:33:55 +0100

    Revise and add tests for the new indexes

--- tests/test_models.py
+++ tests/test_models.py
@@ -11,83 +11,9 @@ SPDX-License-Identifier: MIT
 
 import warnings
 
-import pandas as pd
 import pytest
 
 from oemof import solph
-from oemof.solph.helpers import calculate_timeincrement
-
-
-def test_timeincrement_with_valid_timeindex():
-    datetimeindex = pd.date_range("1/1/2012", periods=1, freq="H")
-    es = solph.EnergySystem(timeindex=datetimeindex)
-    m = solph._models.BaseModel(es)
-    assert m.timeincrement[0] == 1
-    assert es.timeindex.freq.nanos / 3.6e12 == 1
-
-
-def test_timeincrement_with_non_valid_timeindex():
-    with pytest.raises(AttributeError):
-        es = solph.EnergySystem(timeindex=4)
-        solph._models.BaseModel(es)
-
-
-def test_timeincrement_value():
-    es = solph.EnergySystem(timeindex=4)
-    m = solph._models.BaseModel(es, timeincrement=3)
-    assert m.timeincrement[0] == 3
-
-
-def test_timeincrement_list():
-    es = solph.EnergySystem(timeindex=4)
-    m = solph._models.BaseModel(es, timeincrement=[0, 1, 2, 3])
-    assert m.timeincrement[3] == 3
-
-
-def test_nonequ_timeincrement():
-    timeindex_hourly = pd.date_range("1/1/2019", periods=2, freq="H")
-    timeindex_30mins = pd.date_range(
-        "1/1/2019 03:00:00", periods=2, freq="30min"
-    )
-    timeindex_2h = pd.date_range("1/1/2019 04:00:00", periods=2, freq="2H")
-    timeindex = timeindex_hourly.append([timeindex_30mins, timeindex_2h])
-    timeincrement = calculate_timeincrement(timeindex=timeindex)
-    assert timeincrement == solph.sequence([1.0, 1.0, 2.0, 0.5, 0.5, 2.0])
-
-
-def test_nonequ_timeincrement_fill():
-    timeindex_hourly = pd.date_range("1/1/2019", periods=2, freq="H")
-    timeindex_30mins = pd.date_range(
-        "1/1/2019 03:00:00", periods=2, freq="30min"
-    )
-    timeindex_2h = pd.date_range("1/1/2019 04:00:00", periods=2, freq="2H")
-    timeindex = timeindex_hourly.append([timeindex_30mins, timeindex_2h])
-    fvalue = pd.Timedelta(hours=9)
-    timeincrement = calculate_timeincrement(
-        timeindex=timeindex, fill_value=fvalue
-    )
-    assert timeincrement == solph.sequence([9.0, 1.0, 2.0, 0.5, 0.5, 2.0])
-
-
-def test_nonequ_duplicate_timeindex():
-    with pytest.raises(IndexError):
-        timeindex_hourly = pd.date_range("1/1/2019", periods=2, freq="H")
-        timeindex_45mins = pd.date_range("1/1/2019", periods=2, freq="45min")
-        timeindex = timeindex_hourly.append([timeindex_45mins])
-        calculate_timeincrement(timeindex=timeindex)
-
-
-def test_nonequ_with_non_valid_timeindex():
-    with pytest.raises(AttributeError):
-        timeindex = [5, 4]
-        calculate_timeincrement(timeindex=timeindex)
-
-
-def test_nonequ_with_non_valid_fill():
-    with pytest.raises(AttributeError):
-        timeindex = pd.date_range("1/1/2019", periods=2, freq="H")
-        fill_value = 2
-        calculate_timeincrement(timeindex=timeindex, fill_value=fill_value)
 
 
 def test_optimal_solution():
---
+++ tests/test_non_equidistant_time_index.py
@@ -0,0 +1,259 @@
+# -*- coding: utf-8 -
+
+"""Test the definition of the time index of the model.
+
+SPDX-FileCopyrightText: Uwe Krien <krien@uni-bremen.de>
+
+SPDX-License-Identifier: MIT
+"""
+import copy
+import datetime
+import random
+
+import pandas as pd
+
+from oemof.solph import EnergySystem
+from oemof.solph import Investment
+from oemof.solph import Model
+from oemof.solph import buses
+from oemof.solph import components as cmp
+from oemof.solph import flows
+from oemof.solph import processing
+
+
+class TestParameterResult:
+    @classmethod
+    def setup_class(cls):
+        dtindex1 = pd.date_range("1/1/2012", periods=24, freq="H")
+        dtindex2 = pd.date_range("1/2/2012", periods=49, freq="30min")
+        dtindex = dtindex1.union(dtindex2)
+        es = EnergySystem(timeindex=dtindex, infer_last_interval=False)
+
+        # BUSSES
+        b_el1 = buses.Bus(label="b_el1")
+        b_diesel = buses.Bus(label="b_diesel", balanced=False)
+        es.add(b_el1, b_diesel)
+
+        # TEST DIESEL:
+        dg = cmp.Transformer(
+            label="diesel_generator",
+            inputs={b_diesel: flows.Flow(variable_costs=2)},
+            outputs={
+                b_el1: flows.Flow(
+                    variable_costs=1, investment=Investment(ep_costs=500)
+                )
+            },
+            conversion_factors={b_el1: 0.5},
+        )
+
+        batt = cmp.GenericStorage(
+            label="storage",
+            nominal_storage_capacity=1000,
+            inputs={b_el1: flows.Flow(variable_costs=3)},
+            outputs={b_el1: flows.Flow(variable_costs=2.5)},
+            loss_rate=0.00,
+            invest_relation_input_capacity=1 / 6,
+            invest_relation_output_capacity=1 / 6,
+            inflow_conversion_factor=1,
+            outflow_conversion_factor=0.9,
+        )
+
+        random.seed(1)
+        demand_values = random.sample(range(40, 120), 72)
+        demand = cmp.Sink(
+            label="demand_el",
+            inputs={
+                b_el1: flows.Flow(
+                    nominal_value=1,
+                    fix=demand_values,
+                )
+            },
+        )
+        es.add(dg, batt, demand)
+        model = Model(es)
+        model.receive_duals()
+        model.solve()
+        results = processing.results(model, remove_last_time_point=False)
+        cls.flows = {k: v for k, v in results.items() if k[1] is not None}
+        cls.comp = {k: v for k, v in results.items() if k[1] is None}
+        cls.es = es
+        cls.model = model
+
+    def test_timesteps_timeincrements_with_storage_charging(self):
+        storage_content = [
+            v["sequences"]["storage_content"]
+            for k, v in self.comp.items()
+            if k[0].label == "storage"
+        ][0]
+        assert storage_content[0] == storage_content[-1]
+
+        charge = [
+            v["sequences"]["flow"]
+            for k, v in self.flows.items()
+            if k[1].label == "storage"
+        ][0]
+        # Calculate the next storage content and verify it with the storage
+        # content of the results (charging).
+        # Charging - timestep (ts) with its timeincrement (ti)
+        time = [(23, 1), (24, 0.5)]
+        for ts, ti in time:
+            assert round(storage_content[ts] + charge[ts] * ti, 5) == round(
+                storage_content[ts + 1], 5
+            )
+            assert self.es.timeincrement[ts] == ti
+        assert charge.isnull().any()
+
+    def test_timesteps_timeincrements_with_storage_discharging(self):
+        storage_content = [
+            v["sequences"]["storage_content"]
+            for k, v in self.comp.items()
+            if k[0].label == "storage"
+        ][0]
+        # Storage content at the last time point is equal to the content of
+        # the first time point because the storage is balanced.
+        assert storage_content[0] == storage_content[-1]
+
+        discharge = [
+            v["sequences"]["flow"]
+            for k, v in self.flows.items()
+            if k[0].label == "storage"
+        ][0]
+
+        # Calculate the next storage content and verify it with the storage
+        # content of the results (discharging).
+        # Discharging - timestep (ts) with its timeincrement (ti)
+        time = [(7, 1), (40, 0.5)]
+        for ts, ti in time:
+            assert (
+                round(
+                    storage_content[ts]
+                    - (discharge[ts] + (discharge[ts] * 1 / 9)) * ti,
+                    5,
+                )
+                == round(storage_content[ts + 1], 5)
+            )
+            assert self.es.timeincrement[ts] == ti
+
+    def test_timeincrements(self):
+        assert self.es.timeincrement.sum() == 48
+
+    def test_without_last_time_point(self):
+        results = processing.results(self.model, remove_last_time_point=True)
+        flow = {k: v for k, v in results.items() if k[1] is not None}
+        comp = {k: v for k, v in results.items() if k[1] is None}
+        storage_content = [
+            v["sequences"]["storage_content"]
+            for k, v in comp.items()
+            if k[0].label == "storage"
+        ][0]
+        charge = [
+            v["sequences"]["flow"]
+            for k, v in flow.items()
+            if k[1].label == "storage"
+        ][0]
+        # The first and the last value are not the same because the last value
+        # of the storage is missing. Adding the charging of the last time step
+        # will result the final storage content, which is equal to the first
+        assert storage_content[0] != storage_content[-1]
+        assert storage_content[0] == storage_content[-1] + charge[-1] / 2
+        assert not charge.isnull().any()
+        assert storage_content.index[0] == datetime.datetime(
+            2012, 1, 1, 0, 0, 0
+        )
+        assert charge.index[0] == datetime.datetime(2012, 1, 1, 0, 0, 0)
+        assert storage_content.index[-1] == datetime.datetime(
+            2012, 1, 2, 23, 30, 0
+        )
+        assert charge.index[-1] == datetime.datetime(2012, 1, 2, 23, 30, 0)
+
+    def test_time_index_with_last_time_point(self):
+        storage_content = [
+            v["sequences"]["storage_content"]
+            for k, v in self.comp.items()
+            if k[0].label == "storage"
+        ][0]
+        assert storage_content[0] == storage_content[-1]
+
+        charge = [
+            v["sequences"]["flow"]
+            for k, v in self.flows.items()
+            if k[1].label == "storage"
+        ][0]
+        assert storage_content.index[0] == datetime.datetime(
+            2012, 1, 1, 0, 0, 0
+        )
+        assert charge.index[0] == datetime.datetime(2012, 1, 1, 0, 0, 0)
+        assert storage_content.index[-1] == datetime.datetime(
+            2012, 1, 3, 0, 0, 0
+        )
+        assert charge.index[-1] == datetime.datetime(2012, 1, 3, 0, 0, 0)
+
+    def test_numeric_index(self):
+        self.es.timeindex = None
+        model = Model(self.es)
+        model.receive_duals()
+        model.solve()
+        results = processing.results(self.model)
+        flow = {k: v for k, v in results.items() if k[1] is not None}
+        diesel_generator_out = [
+            v["sequences"]["flow"]
+            for k, v in flow.items()
+            if k[0].label == "diesel_generator"
+        ][0]
+        assert diesel_generator_out.index[0] == 0
+        assert diesel_generator_out.index[-1] == 72
+        assert len(diesel_generator_out.index) == 73
+
+        storage_content = [
+            v["sequences"]["storage_content"]
+            for k, v in self.comp.items()
+            if k[0].label == "storage"
+        ][0]
+        assert storage_content[0] == storage_content[-1]
+
+        charge = [
+            v["sequences"]["flow"]
+            for k, v in self.flows.items()
+            if k[1].label == "storage"
+        ][0]
+        # Calculate the next storage content and verify it with the storage
+        # content of the results (charging).
+        # Charging - timestep (ts) with its timeincrement (ti)
+        time = [(23, 1), (24, 0.5)]
+        for ts, ti in time:
+            assert round(storage_content[ts] + charge[ts] * ti, 5) == round(
+                storage_content[ts + 1], 5
+            )
+            assert self.es.timeincrement[ts] == ti
+        assert charge.isnull().any()
+
+    def test_default_value_explicit(self):
+        """
+        In the explicit mode the default value for the processing is to not
+        remove the last value and allow nan-values. The explicit mode is
+        triggered by setting `infer_last_interval` to `False`.
+        """
+        assert self.es.timemode == "explicit"
+        model = Model(self.es)
+        model.receive_duals()
+        model.solve()
+        results = processing.results(model)
+        flow = {k: v for k, v in results.items() if k[1] is not None}
+        assert 73 == len([v["sequences"]["flow"] for k, v in flow.items()][0])
+
+    def test_default_value_implicit(self):
+        """
+        In the implicit mode the default value for the processing is to remove
+        the last value and allow nan-values.
+        """
+        assert self.es.timemode == "explicit"
+        test_es = EnergySystem()
+        assert test_es.timemode == "implicit"
+        my_es = copy.copy(self.es)
+        my_es.timemode = test_es.timemode
+        model = Model(my_es)
+        model.receive_duals()
+        model.solve()
+        results = processing.results(model)
+        flow = {k: v for k, v in results.items() if k[1] is not None}
+        assert 72 == len([v["sequences"]["flow"] for k, v in flow.items()][0])
