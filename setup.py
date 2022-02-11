3335b4e04d43b49535cab9d8812279e4e4ca215f
uvchik <uvchik.git@posteo.eu>
2022-02-11 12:57:53 +0100

    Add feature branch of tools to fix logging problem.

--- setup.py
+++ setup.py
@@ -82,7 +82,8 @@ setup(
         "pandas",
         "pyomo >= 5.7.0, < 7.0",
         "networkx",
-        "oemof.tools",
+        ("oemof.tools@https://github.com/oemof/oemof.tools/archive/feature/"
+         "set-minimal-lggin-level.zip"),
         "oemof.network",
     ],
     extras_require={
