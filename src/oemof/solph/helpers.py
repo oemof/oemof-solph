9d9d07832ea50b6de9fcd77a3880078c3c643630
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2022-05-23 22:11:59 +0200

    Remove obsolete helper function

--- src/oemof/solph/helpers.py
+++ src/oemof/solph/helpers.py
@@ -64,37 +64,3 @@ def flatten(d, parent_key="", sep="_"):
         else:
             items.append((new_key, v))
     return dict(items)
-
-
-def calculate_timeincrement(timeindex, fill_value=None):
-    """
-    Calculates timeincrement for `timeindex`
-
-    Parameters
-    ----------
-    timeindex: pd.DatetimeIndex
-        timeindex of energysystem
-    fill_value: numerical
-        timeincrement for first timestep in hours
-    """
-    if (isinstance(timeindex, pd.DatetimeIndex)
-        and (fill_value and isinstance(fill_value, pd.Timedelta)
-             or fill_value is None)):
-        if len(set(timeindex)) != len(timeindex):
-            raise IndexError("No equal DatetimeIndex allowed!")
-        timeindex = timeindex.to_series()
-        timeindex_sorted = timeindex.sort_values()
-        if fill_value:
-            timeincrement = timeindex_sorted.diff().fillna(value=fill_value)
-        else:
-            timeincrement = timeindex_sorted.diff().fillna(method='bfill')
-        timeincrement_sec = timeincrement.map(dt.timedelta.total_seconds)
-        timeincrement_hourly = list(timeincrement_sec.map(
-                                    lambda x: x / 3600))
-        timeincrement = timeincrement_hourly
-        return timeincrement
-    else:
-        raise AttributeError(
-            "'timeindex' must be of type 'DatetimeIndex'"
-            + " and 'fill_value' of type 'Timedelta'."
-        )
