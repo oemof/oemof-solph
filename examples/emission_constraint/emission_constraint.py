06f0407523f78d6de53645a3a0057caaf189b518
uvchik <uvchik.git@posteo.eu>
2022-05-23 17:26:20 +0200

    Add more examples

---
+++ examples/emission_constraint/emission_constraint.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+"""
+General description
+-------------------
+Example that shows how to add an emission constraint in a model.
+
+Installation requirements
+-------------------------
+
+This example requires oemof.solph (v0.5.x), install by:
+
+    pip install oemof.solph[examples]
+
+
+License
+-------
+`MIT license <https://github.com/oemof/oemof-solph/blob/dev/LICENSE>`_
+
+"""
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from oemof import solph
+from oemof.solph import constraints
+
+# create energy system
+energysystem = solph.EnergySystem(
+    timeindex=pd.date_range("1/1/2012", periods=3, freq="H")
+)
+
+# create gas bus
+bgas = solph.Bus(label="gas")
+
+# create electricity bus
+bel = solph.Bus(label="electricity")
+
+# adding the buses to the energy system
+energysystem.add(bel, bgas)
+
+# create fixed source object representing biomass plants
+energysystem.add(
+    solph.components.Source(
+        label="biomass",
+        outputs={
+            bel: solph.Flow(
+                nominal_value=100,
+                variable_costs=10,
+                emission_factor=0.01,
+                fix=[0.1, 0.2, 0.3],
+            )
+        },
+    )
+)
+
+# create source object representing the gas commodity
+energysystem.add(
+    solph.components.Source(
+        label="gas-source",
+        outputs={bgas: solph.Flow(variable_costs=10, emission_factor=0.2)},
+    )
+)
+
+energysystem.add(
+    solph.components.Sink(
+        label="demand",
+        inputs={
+            bel: solph.Flow(
+                nominal_value=200, variable_costs=10, fix=[0.1, 0.2, 0.3]
+            )
+        },
+    )
+)
+
+# create simple transformer object representing a gas power plant
+energysystem.add(
+    solph.components.Transformer(
+        label="pp_gas",
+        inputs={bgas: solph.Flow()},
+        outputs={bel: solph.Flow(nominal_value=200)},
+        conversion_factors={bel: 0.58},
+    )
+)
+
+# initialise the operational model
+model = solph.Model(energysystem)
+
+# add the emission constraint
+constraints.emission_limit(model, limit=100)
+
+# print out the emission constraint
+model.integral_limit_emission_factor_constraint.pprint()
+model.integral_limit_emission_factor.pprint()
+
+# solve the model
+model.solve()
+
+# print out the amount of emissions from the emission constraint
+print(model.integral_limit_emission_factor())
+
+results = solph.processing.results(model)
+
+data = solph.views.node(results, "electricity")["sequences"]
+ax = data.plot(kind="line", grid=True)
+ax.set_xlabel("Time (h)")
+ax.set_ylabel("P (MW)")
+plt.show()
---
+++ examples/storage_investment/v2_invest_optimize_only_gas_and_storage.py
@@ -0,0 +1,206 @@
+# -*- coding: utf-8 -*-
+
+"""
+General description
+-------------------
+This example shows how to perform a capacity optimization for
+an energy system with storage. The following energy system is modeled:
+
+                input/output  bgas     bel
+                     |          |        |       |
+                     |          |        |       |
+ wind(FixedSource)   |------------------>|       |
+                     |          |        |       |
+ pv(FixedSource)     |------------------>|       |
+                     |          |        |       |
+ gas_resource        |--------->|        |       |
+ (Commodity)         |          |        |       |
+                     |          |        |       |
+ demand(Sink)        |<------------------|       |
+                     |          |        |       |
+                     |          |        |       |
+ pp_gas(Transformer) |<---------|        |       |
+                     |------------------>|       |
+                     |          |        |       |
+ storage(Storage)    |<------------------|       |
+                     |------------------>|       |
+
+The example exists in four variations. The following parameters describe
+the main setting for the optimization variation 2:
+
+    - optimize gas_resource and storage
+    - set installed capacities for wind and pv
+    - set investment cost for storage
+    - set gas price for kWh
+
+    Results show a higher renewable energy share than in variation 1
+    (78% compared to 51%) due to preinstalled renewable capacities.
+    Storage is not installed as the gas resource is cheaper.
+
+    Have a look at different parameter settings. There are four variations
+    of this example in the same folder.
+
+Installation requirements
+-------------------------
+
+This example requires oemof.solph (v0.5.x), install by:
+
+    pip install oemof.solph[examples]
+
+
+License
+-------
+`MIT license <https://github.com/oemof/oemof-solph/blob/dev/LICENSE>`_
+
+"""
+
+
+###############################################################################
+# Imports
+###############################################################################
+
+import logging
+import os
+import pprint as pp
+import warnings
+
+import pandas as pd
+
+# Default logger of oemof
+from oemof.tools import economics
+from oemof.tools import logger
+
+from oemof import solph
+
+# Read data file
+filename = os.path.join(os.getcwd(), "storage_investment.csv")
+try:
+    data = pd.read_csv(filename)
+except FileNotFoundError:
+    msg = "Data file not found: {0}. Only one value used!"
+    warnings.warn(msg.format(filename), UserWarning)
+    data = pd.DataFrame({"pv": [0.3], "wind": [0.6], "demand_el": [500]})
+
+number_timesteps = len(data)
+
+##########################################################################
+# Initialize the energy system and read/calculate necessary parameters
+##########################################################################
+
+logger.define_logging()
+logging.info("Initialize the energy system")
+date_time_index = solph.create_time_index(2012, number=number_timesteps)
+
+energysystem = solph.EnergySystem(
+    timeindex=date_time_index, infer_last_interval=False
+)
+
+price_gas = 0.04
+
+# If the period is one year the equivalent periodical costs (epc) of an
+# investment are equal to the annuity. Use oemof's economic tools.
+epc_storage = economics.annuity(capex=1000, n=20, wacc=0.05)
+
+##########################################################################
+# Create oemof objects
+##########################################################################
+
+logging.info("Create oemof objects")
+# create natural gas bus
+bgas = solph.Bus(label="natural_gas")
+
+# create electricity bus
+bel = solph.Bus(label="electricity")
+
+energysystem.add(bgas, bel)
+
+# create excess component for the electricity bus to allow overproduction
+excess = solph.components.Sink(label="excess_bel", inputs={bel: solph.Flow()})
+
+# create source object representing the natural gas commodity (annual limit)
+gas_resource = solph.components.Source(
+    label="rgas", outputs={bgas: solph.Flow(variable_costs=price_gas)}
+)
+
+# create fixed source object representing wind power plants
+wind = solph.components.Source(
+    label="wind",
+    outputs={bel: solph.Flow(fix=data["wind"], nominal_value=1000000)},
+)
+
+# create fixed source object representing pv power plants
+pv = solph.components.Source(
+    label="pv", outputs={bel: solph.Flow(fix=data["pv"], nominal_value=600000)}
+)
+
+# create simple sink object representing the electrical demand
+demand = solph.components.Sink(
+    label="demand",
+    inputs={bel: solph.Flow(fix=data["demand_el"], nominal_value=1)},
+)
+
+# create simple transformer object representing a gas power plant
+pp_gas = solph.components.Transformer(
+    label="pp_gas",
+    inputs={bgas: solph.Flow()},
+    outputs={bel: solph.Flow(nominal_value=10e10, variable_costs=0)},
+    conversion_factors={bel: 0.58},
+)
+
+# create storage object representing a battery
+storage = solph.components.GenericStorage(
+    label="storage",
+    inputs={bel: solph.Flow(variable_costs=0.0001)},
+    outputs={bel: solph.Flow()},
+    loss_rate=0.00,
+    initial_storage_level=0,
+    invest_relation_input_capacity=1 / 6,
+    invest_relation_output_capacity=1 / 6,
+    inflow_conversion_factor=1,
+    outflow_conversion_factor=0.8,
+    investment=solph.Investment(ep_costs=epc_storage),
+)
+
+energysystem.add(excess, gas_resource, wind, pv, demand, pp_gas, storage)
+
+##########################################################################
+# Optimise the energy system
+##########################################################################
+
+logging.info("Optimise the energy system")
+
+# initialise the operational model
+om = solph.Model(energysystem)
+
+# if tee_switch is true solver messages will be displayed
+logging.info("Solve the optimization problem")
+om.solve(solver="cbc", solve_kwargs={"tee": True})
+
+##########################################################################
+# Check and plot the results
+##########################################################################
+
+# check if the new result object is working for custom components
+results = solph.processing.results(om)
+
+custom_storage = solph.views.node(results, "storage")
+electricity_bus = solph.views.node(results, "electricity")
+
+meta_results = solph.processing.meta_results(om)
+pp.pprint(meta_results)
+
+my_results = electricity_bus["scalars"]
+
+# installed capacity of storage in GWh
+my_results["storage_invest_GWh"] = (
+    results[(storage, None)]["scalars"]["invest"] / 1e6
+)
+
+# resulting renewable energy share
+my_results["res_share"] = (
+    1
+    - results[(pp_gas, bel)]["sequences"].sum()
+    / results[(bel, demand)]["sequences"].sum()
+)
+
+pp.pprint(my_results)
