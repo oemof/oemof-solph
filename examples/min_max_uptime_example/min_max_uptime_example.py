fd5d862a69b1115e4248500f33f76195371291ab
Johannes Roeder <johannes.roeder@uni-bremen.de>
2022-09-14 15:52:58 +0200

    Add example description

--- examples/min_max_uptime_example/min_max_uptime_example.py
+++ examples/min_max_uptime_example/min_max_uptime_example.py
@@ -2,29 +2,39 @@
 """
 Example that shows how to use a combination of minimum and maximum uptime.
 
+The model consists of a volatile renewable energy source with
+a fixed profile (Source: 'renewable_energy'), a cyclic industrial process
+modelled as a Sink with a NonConvex flow with negative variable costs as
+revenues, and an excess for surplus of renewable energy.
+
+The cyclic industrial process has a fixed block profile and a certain time
+within each cycle is required. With the attributes `maximum_uptime`,
+`minimum_uptime` and `minimum_downtime`, and a given `nominal_value`
+combination with the `min` attribute it is possible to model
+this scheduling problem.
+
 SPDX-FileCopyrightText: Johannes Röder <johannes.roeder@uni-bremen.de>
 
 SPDX-License-Identifier: MIT
 """
-
 import os
+import logging
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 from oemof.solph import components
 from oemof.solph import Flow, Bus, EnergySystem, Model
-from oemof.solph._options import Investment, NonConvex
-
-import oemof.network
+from oemof.solph._options import NonConvex
 from oemof.solph import processing, views
 from oemof.solph import helpers
-import logging
 
-
-data_demand = [1, 2, 4, 5, 6, 4, 3, 2, 6, 7,
-               7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
-               8, 8, 2, 1, 0, 5, 6]
+data_demand = [
+    1, 1, 2, 4, 5, 6, 4, 6, 2, 6, 7, 6,
+    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+    8, 8, 2, 1, 0, 5, 6, 7, 6, 7, 3,
+    3, 1, 1,
+]
 
 # select periods
 periods = len(data_demand)
@@ -32,37 +42,39 @@ periods = len(data_demand)
 # create an energy system
 idx = pd.date_range('1/1/2017', periods=periods, freq='H')
 es = EnergySystem(timeindex=idx)
-# oemof.network.Node.registry = es
 
-# bus_0 = solph.Bus(label='bus_0')
 bus_1 = Bus(label='bus')
 
 es.add(bus_1)
 
 es.add(components.Source(
-    label='NonConvexSource',
+    label='renewable_energy',
+    inputs={bus_1: Flow(
+        fix=data_demand,
+        nominal_value=1,
+    )}
+))
+
+es.add(components.Sink(
+    label='cyclic_process',
     outputs={bus_1: Flow(
-        variable_costs=10,
+        variable_costs=-10,
         nominal_value=4,
         min=1,
         nonconvex=NonConvex(
             minimum_downtime=2,
-            minimum_uptime=3,
-            maximum_uptime=3,
+            minimum_uptime=4,
+            maximum_uptime=4,
         ),
     )},
 ))
 
-es.add(components.Source(
-    label='Shortage_Source',
+es.add(components.Sink(
+    label='excess',
     outputs={bus_1: Flow(
-        variable_costs=1000,
+        variable_costs=0,
     )},
 ))
-
-es.add(components.Sink(label='demand', inputs={
-    bus_1: Flow(fix=data_demand,  nominal_value=1)}))
-
 
 # create an optimization problem and solve it
 om = Model(es)
@@ -77,11 +89,10 @@ om.write(filename, io_options={'symbolic_solver_labels': True})
 om.solve(solver='cbc', solve_kwargs={'tee': True})
 
 # create result object
-
-
 results = processing.results(om)
 
 bus1 = views.node(results, 'bus')["sequences"]
 bus1.plot(kind='line', drawstyle='steps-mid')
+plt.ylim(None, 10)
 plt.legend()
 plt.show()
