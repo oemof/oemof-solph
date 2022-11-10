c5695b2a014e34801a9c8ebde63f7c28283de6ab
jnnr <32454596+jnnr@users.noreply.github.com>
2022-11-10 16:57:48 +0100

    Extend docstring of Sink

--- src/oemof/solph/components/_sink.py
+++ src/oemof/solph/components/_sink.py
@@ -19,8 +19,36 @@ from oemof.solph._helpers import check_node_object_for_missing_attribute
 
 
 class Sink(on.Sink):
-    """An object with one input flow."""
+    """A component which is designed for one input flow.
 
+    Parameters
+    ----------
+    label : str
+        String holding the label of the Sink object.
+        The label of each object must be unique.
+
+    Examples
+    --------
+    Defining a Sink:
+
+    >>> from oemof import solph
+    >>> bel = solph.buses.Bus(label='electricity')
+
+    >>> electricity_export = solph.components.Sink(
+    ...    label='el_export',
+    ...    inputs={bel: solph.flows.Flow()})
+
+    >>> type(electricity_export)
+    <class 'oemof.solph.components._sink.Sink'>
+
+    >>> str(electricity_export.inputs())
+    ['electricity']
+
+    Notes
+    -----
+    It is theoretically possible to use the Sink object with multiple outputs.
+    However, we strongly recommend using multiple Sink objects instead.
+    """
     def __init__(self, label=None, inputs=None, **kwargs):
         if inputs is None:
             inputs = {}
