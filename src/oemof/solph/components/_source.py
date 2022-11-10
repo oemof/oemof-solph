0381a9ada415f4cee84d4b78ea38a026ccb8d735
Sarah Berendes <Sarah.Berendes@rl-institut.de>
2022-11-10 13:15:11 +0100

    Make kwargs explicit for Source component

--- src/oemof/solph/components/_source.py
+++ src/oemof/solph/components/_source.py
@@ -21,9 +21,14 @@ from oemof.solph._helpers import check_node_object_for_missing_attribute
 class Source(on.Source):
     """An object with one output flow."""
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, *args, label=None, outputs=None, **kwargs):
+
+        if outputs is None:
+            outputs = {}
+
+        super().__init__(*args, label=label, outputs=outputs, **kwargs)
         check_node_object_for_missing_attribute(self, "outputs")
 
+        
     def constraint_group(self):
         pass
