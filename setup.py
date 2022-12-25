5a84c031d7f9c11b0d0dc9c7334029a0a479c364
Patrik Schönfeldt <patrik@salacia.snft.de>
2022-12-25 22:33:53 +0100

    Limit numpy version
    Numpy 1.24 does not allow creating an ndarray from ragged
    nested sequences (which is a list-or-tuple of lists-or-tuples-or
    ndarrays with different lengths or shapes) anymore.
    Is this should not be fixed in a feature-branch, I am limiting the
    numpy version to lower ones.

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     install_requires=[
         "blinker",
         "dill",
-        "numpy",
+        "numpy < 1.24",
         "pandas",
         "pyomo >= 6.0.0, < 7.0",
         "networkx",
