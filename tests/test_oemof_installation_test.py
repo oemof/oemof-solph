11a7c1c9339c755222ecc99a9d6896be0a117621
tobirohrer <tobias.rohrer@outlook.com>
2023-02-22 10:28:02 +0100

    removed nonsense type definition of `complelted_process

--- tests/test_oemof_installation_test.py
+++ tests/test_oemof_installation_test.py
@@ -1,5 +1,5 @@
 import subprocess
 
 def test_oemof_installation_test_runs_without_errors():
-    completed_process: subprocess = subprocess.run(['oemof_installation_test'])
+    completed_process = subprocess.run(['oemof_installation_test'])
     assert completed_process.returncode == 0
