cb9a1827b7673d239c5cac4523e5bcbd829efc47
Patrik Schönfeldt <patrik@salacia.snft.de>
2023-08-31 12:13:47 +0200

    Release v0.5.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ long_description = "%s" % (
 
 setup(
     name="oemof.solph",
-    version="0.5.1rc1",
+    version="0.5.1",
     license="MIT",
     description=(
         "A model generator for energy system modelling and optimisation."
--- src/oemof/solph/__init__.py
+++ src/oemof/solph/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.5.1rc1"
+__version__ = "0.5.1"
 
 from . import buses
 from . import components
