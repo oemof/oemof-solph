011471b6cc60a93b8fa76c97644a7e85ac6d3de5
Johannes Kochems <jokochems@web.de>
2023-11-01 14:49:39 +0100

    Add / refactor test(s)

---
+++ tests/test_scripts/test_solph/test_multi_period_model/test_multi_period_investment_fixed.py
@@ -0,0 +1,22 @@
+from oemof.solph import processing
+from test_multi_period_investment_model import (
+    set_up_multi_period_investment_model,
+)
+
+
+def test_multi_period_investment_fixed(solver="cbc"):
+    """test fixing investments in a repeated solve"""
+    om = set_up_multi_period_investment_model(approach="DLR")
+    om.receive_duals()
+    om.solve(solver=solver)
+
+    assert om.solver_results is not None
+    om.fix_investments()
+    om.solve(solver=solver)
+    for (i, o) in om.InvestmentFlowBlock.INVESTFLOWS:
+        for p in om.PERIODS:
+            assert om.InvestmentFlowBlock.invest[i, o, p].fixed
+            assert om.InvestmentFlowBlock.total[i, o, p].fixed
+            assert om.InvestmentFlowBlock.old[i, o, p].fixed
+            assert om.InvestmentFlowBlock.old_end[i, o, p].fixed
+            assert om.InvestmentFlowBlock.old_exo[i, o, p].fixed
