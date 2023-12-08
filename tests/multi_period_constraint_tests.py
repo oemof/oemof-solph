91669ff9bacad1cb5d49ce606122957710adc8a7
Johannes Kochems <jokochems@web.de>
2023-12-08 17:40:20 +0100

    Alter import order

--- tests/multi_period_constraint_tests.py
+++ tests/multi_period_constraint_tests.py
@@ -16,11 +16,11 @@ from os import path as ospath
 import pandas as pd
 import pytest
 from pyomo.repn.tests.lp_diff import lp_diff
-
-from oemof import solph
 from test_scripts.test_solph.test_multi_period.test_investment_model import (
     set_up_multi_period_investment_model,
 )
+
+from oemof import solph
 
 logging.disable(logging.INFO)
 
