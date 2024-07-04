de5fd709c6aedd6954e7d3b84ade556c839dccd9
Patrik Schönfeldt <patrik@proxima.snft.de>
2024-07-04 09:38:10 +0200

    Fix test_check_age_and_lifetime
    The test was confusing, as it added a nominal_value to a Sink.
    This, however, did not cause an error to be raised as the error
    that was tested before was first.

--- tests/test_options.py
+++ tests/test_options.py
@@ -16,6 +16,6 @@ def test_check_age_and_lifetime():
     """Check error being thrown if age > lifetime"""
     msg = "A unit's age must be smaller than its expected lifetime."
     with pytest.raises(AttributeError, match=msg):
-        solph.components.Sink(
+        solph.Flow(
             nominal_value=solph.Investment(age=41, lifetime=40)
         )
