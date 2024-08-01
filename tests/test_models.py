91aca5452d8fecd87e8f8024439e78d8069c929f
Patrik Schönfeldt <patrik@salacia.snft.de>
2024-08-01 14:16:12 +0200

    Refactor model solving tests
    The function "test_optimal_solution" didn't actually test anything,
    so I deleted it. The function "test_infeasible_model" now only tests
    for the infeasibility warning.

--- tests/test_models.py
+++ tests/test_models.py
@@ -17,55 +17,31 @@ import pytest
 from oemof import solph
 
 
-def test_optimal_solution():
+def test_infeasible_model():
+    # FutureWarning is i.e. emitted by network Entity registry
+    warnings.simplefilter(action="ignore", category=FutureWarning)
+
     es = solph.EnergySystem(timeincrement=[1])
     bel = solph.buses.Bus(label="bus")
     es.add(bel)
     es.add(
         solph.components.Sink(
-            label="sink",
-            inputs={bel: solph.flows.Flow(nominal_value=5, fix=[1])},
+            inputs={bel: solph.flows.Flow(nominal_value=5, fix=[1])}
         )
     )
     es.add(
         solph.components.Source(
-            label="source",
-            outputs={bel: solph.flows.Flow(variable_costs=5)},
+            outputs={
+                bel: solph.flows.Flow(
+                    nominal_value=4, variable_costs=5
+                )
+            }
         )
     )
     m = solph.Model(es)
-    m.solve("cbc")
-    m.results()
-    solph.processing.meta_results(m)
-
-
-def test_infeasible_model():
-    # FutureWarning is i.e. emitted by network Entity registry
-    warnings.simplefilter(action="ignore", category=FutureWarning)
-
-    with pytest.raises(ValueError, match=""):
-        with warnings.catch_warnings(record=True) as w:
-            es = solph.EnergySystem(timeincrement=[1])
-            bel = solph.buses.Bus(label="bus")
-            es.add(bel)
-            es.add(
-                solph.components.Sink(
-                    inputs={bel: solph.flows.Flow(nominal_value=5, fix=[1])}
-                )
-            )
-            es.add(
-                solph.components.Source(
-                    outputs={
-                        bel: solph.flows.Flow(
-                            nominal_value=4, variable_costs=5
-                        )
-                    }
-                )
-            )
-            m = solph.Model(es)
-            m.solve(solver="cbc")
-            assert "Optimization ended with status" in str(w[0].message)
-            solph.processing.meta_results(m)
+    with warnings.catch_warnings(record=True) as w:
+        m.solve(solver="cbc")
+        assert "Optimization ended with status" in str(w[0].message)
 
 
 @pytest.mark.filterwarnings(
