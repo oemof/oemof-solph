f8af61ead6645a32ce5d868c7adfcf396ddbddf5
Patrik Schönfeldt <patrik@salacia.snft.de>
2024-08-08 16:24:46 +0200

    Fix test_initial_status_on

--- tests/test_flows/test_non_convex_flow.py
+++ tests/test_flows/test_non_convex_flow.py
@@ -32,7 +32,6 @@ def test_initial_status_off():
     ).all()
 
 
-@pytest.mark.skip(reason="Reported issue (see #1099).")
 def test_initial_status_on():
     date_time_index = pd.date_range("1/1/2012", periods=10, freq="h")
     energysystem = solph.EnergySystem(
@@ -45,6 +44,7 @@ def test_initial_status_on():
     # positive costs but turned on initially
     flow = solph.flows.Flow(
         nominal_value=10,
+        min=0.5,
         nonconvex=solph.NonConvex(initial_status=1, minimum_uptime=3),
         variable_costs=1,
     )
@@ -57,5 +57,5 @@ def test_initial_status_on():
     results = solph.processing.results(model)
 
     assert (
-        results[(bus, bus)]["sequences"]["flow"][:-1] == 3 * [10] + 7 * [0]
+        results[(bus, bus)]["sequences"]["flow"][:-1] == 3 * [5] + 7 * [0]
     ).all()
