516277e80ed20464d1b3917344ef1bc8fcc16b23
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2024-08-14 10:10:02 +0200

    Add storage power investment tests

--- tests/test_components/test_storage.py
+++ tests/test_components/test_storage.py
@@ -48,3 +48,96 @@ def test_relative_losses():
             == pytest.approx(cases[1]["result"][2 * i])
             == pytest.approx(cases[2]["result"][4 * i])
         )
+
+
+def test_invest_power_uncoupled():
+    es = solph.EnergySystem(
+        timeindex=solph.create_time_index(
+            year=2023,
+            number=10,
+        ),
+        infer_last_interval=False,
+    )
+
+    bus = solph.Bus("slack_bus", balanced=False)
+    es.add(bus)
+
+    storage = solph.components.GenericStorage(
+        "storage",
+        inputs={
+            bus: solph.Flow(
+                variable_costs=-1, nominal_value=solph.Investment(ep_costs=0.1)
+            )
+        },
+        outputs={
+            bus: solph.Flow(
+                variable_costs=1, nominal_value=solph.Investment(ep_costs=0.1)
+            )
+        },
+        nominal_storage_capacity=10,
+        initial_storage_level=0,
+        balanced=False,
+    )
+    es.add(storage)
+
+    model = solph.Model(es)
+    model.solve("cbc")
+
+    result = solph.processing.results(model)
+    storage_content = result[(storage, None)]["sequences"]["storage_content"]
+    assert (storage_content == np.arange(0, 10.5, 1)).all()
+
+    invest_inflow = result[(bus, storage)]["scalars"]["invest"]
+    assert invest_inflow == pytest.approx(1)
+
+    invest_outflow = result[(storage, bus)]["scalars"]["invest"]
+    assert invest_outflow == pytest.approx(0)
+
+    print(result)
+
+
+def test_invest_power_coupled():
+    es = solph.EnergySystem(
+        timeindex=solph.create_time_index(
+            year=2023,
+            number=10,
+        ),
+        infer_last_interval=False,
+    )
+
+    bus = solph.Bus("slack_bus", balanced=False)
+    es.add(bus)
+
+    storage = solph.components.GenericStorage(
+        "storage",
+        inputs={
+            bus: solph.Flow(
+                variable_costs=-1, nominal_value=solph.Investment(ep_costs=0.1)
+            )
+        },
+        outputs={
+            bus: solph.Flow(
+                variable_costs=1, nominal_value=solph.Investment(ep_costs=0.1)
+            )
+        },
+        nominal_storage_capacity=10,
+        invest_relation_input_output=0.5,
+        initial_storage_level=0,
+        balanced=False,
+    )
+    es.add(storage)
+
+    model = solph.Model(es)
+    model.solve("cbc")
+
+    result = solph.processing.results(model)
+    storage_content = result[(storage, None)]["sequences"]["storage_content"]
+    assert (storage_content == np.arange(0, 10.5, 1)).all()
+
+    invest_inflow = result[(bus, storage)]["scalars"]["invest"]
+    assert invest_inflow == pytest.approx(1)
+
+    invest_outflow = result[(storage, bus)]["scalars"]["invest"]
+    assert invest_outflow == pytest.approx(2)
+
+    print(result)
