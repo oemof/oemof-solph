7ffc81f1d399e9c5d513125319db920d6d6e2bba
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2024-08-14 13:17:55 +0200

    Adjust test_results_with_recent_dump()

--- tests/test_scripts/test_solph/test_storage_investment/test_storage_investment.py
+++ tests/test_scripts/test_solph/test_storage_investment/test_storage_investment.py
@@ -197,8 +197,8 @@ def test_results_with_recent_dump():
     assert meta["problem"]["Lower bound"] == 4.231675777e17
     assert meta["problem"]["Upper bound"], 4.231675777e17
     assert meta["problem"]["Number of variables"] == 2808
-    assert meta["problem"]["Number of constraints"] == 2808
-    assert meta["problem"]["Number of nonzeros"] == 1197
+    assert meta["problem"]["Number of constraints"] == 2809
+    assert meta["problem"]["Number of nonzeros"] == 1199
     assert meta["problem"]["Number of objectives"] == 1
     assert str(meta["problem"]["Sense"]) == "minimize"
 
