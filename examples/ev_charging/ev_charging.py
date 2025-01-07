165b0eacc72a0e586cac3008f1680497835c2dde
Patrik Schönfeldt <Patrik.Schoenfeldt@dlr.de>
2025-01-07 15:50:26 +0100

    Ad stub for the EV charging tutorial.

---
+++ examples/ev_charging/ev_charging.py
@@ -0,0 +1,104 @@
+# %%
+"""
+First of all, we create some input data. We use Pandas to do so and will also
+import matplotlib to plot the data right away.
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+
+time_index = pd.date_range(
+    start="2025-01-01",
+    end="2025-01-02",
+    freq="5min",
+    inclusive="both",
+)
+
+# Define the trip demand series for the real trip scenario.
+# As the demand is a power time series, it has N-1 entries
+# when compared to the N entires of time axis for the energy.
+ev_demand = pd.Series(0, index=time_index[:-1])
+
+# Morning Driving: 07:00 to 08:00
+driving_start_morning = pd.Timestamp("2025-01-01 07:10")
+driving_end_morning = pd.Timestamp("2025-01-01 08:10")
+ev_demand.loc[driving_start_morning:driving_end_morning] = 10  # kW
+
+# Evening Driving: 17:00 to 18:00.
+# Note that time points work even if they are not in the index.
+driving_start_evening = pd.Timestamp("2025-01-01 17:13:37")
+driving_end_evening = pd.Timestamp("2025-01-01 18:47:11")
+ev_demand.loc[driving_start_evening:driving_end_evening] = 9  # kW
+
+plt.figure(figsize=(5, 2))
+plt.plot(ev_demand)
+plt.ylabel("Power (kW)")
+plt.gcf().autofmt_xdate()
+plt.show()
+
+# %%
+"""
+Now, let's create an energy system model of the electric vehicle that
+follows the driving pattern. It uses the same time index defined above
+and consists of a Battery (Charged in the beginning) and an electricity demand.
+"""
+import oemof.solph as solph
+
+def create_base_system():
+    energy_system = solph.EnergySystem(
+        timeindex=time_index,
+        infer_last_interval=False,
+    )
+
+    b_el = solph.Bus(label="Car Electricity")
+    energy_system.add(b_el)
+
+    # As we have a demand time series which is actually in kW,
+    # we use a common "hack" here: We set the nominal capacity to 1 (kW),
+    # so that multiplication by the time series will just yield the correct result.
+    demand_driving = solph.components.Sink(
+        label="Driving Demand",
+        inputs={b_el: solph.Flow(nominal_capacity=1, fix=ev_demand)},
+    )
+
+    energy_system.add(demand_driving)
+
+    car_battery = solph.components.GenericStorage(
+        label="Car Battery",
+        nominal_capacity=50,  # kWh
+        inputs={b_el: solph.Flow()},
+        outputs={b_el: solph.Flow()},
+        initial_storage_level=1,  # full in the beginning
+        loss_rate=0.001,  # 0.1 % / hr
+        balanced=False,  # True: content at beginning and end need to be equal
+    )
+    energy_system.add(car_battery)
+
+    return energy_system
+
+
+es = create_base_system()
+
+# %%
+"""
+Solve the model and show results
+"""
+model = solph.Model(es)
+model.solve(solve_kwargs={"tee": False})
+results = solph.processing.results(model)
+
+battery_series = solph.views.node(results, "Car Battery")["sequences"]
+
+plt.plot(battery_series[(("Car Battery", "None"), "storage_content")])
+plt.ylabel("Energy (kWh)")
+plt.ylim(0, 51)
+plt.twinx()
+energy_leaves_battery = battery_series[
+    (("Car Battery", "Car Electricity"), "flow")
+]
+plt.step(energy_leaves_battery.index, energy_leaves_battery, "r-")
+plt.ylabel("Power (kW)")
+plt.gcf().autofmt_xdate()
+plt.show()
