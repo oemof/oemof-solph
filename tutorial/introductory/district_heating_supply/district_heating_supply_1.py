64a98d5a0487d04798dcb82d43ac0230a187759e
Jonas Freißmann <j.freissmann@web.de>
2025-04-04 12:06:24 +0200

    Improve dispatch plot and add darkmode option.

--- tutorial/introductory/district_heating_supply/district_heating_supply_1.py
+++ tutorial/introductory/district_heating_supply/district_heating_supply_1.py
@@ -68,19 +68,23 @@ data_heat_bus = solph.views.node(results, 'heat network')['sequences']
 # %%[sec_8_start]
 import matplotlib.pyplot as plt
 
+# plt.style.use('dark_background')
+
 fig, ax = plt.subplots(figsize=[10, 6])
 
 ax.bar(
     data_heat_bus.index,
     data_heat_bus[(('gas boiler', 'heat network'), 'flow')],
-    label='gas boiler'
+    label='gas boiler',
+    color='#EC6707'
 )
 
 ax.legend(loc='upper right')
 ax.grid(axis='y')
 ax.set_ylabel('Hourly heat production in MWh')
 
-# plt.savefig('intro_tut_dhs_1_hourly_heat_production.pdf')
+# plt.tight_layout()
+# plt.savefig('intro_tut_dhs_1_hourly_heat_production.svg')
 plt.show()
 # %%[sec_8_end]
 
