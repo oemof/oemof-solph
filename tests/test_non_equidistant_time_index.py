5554cb03fa0c4bc8dcfddd03622dc468f02a4338
Patrik Schönfeldt <patrik@salacia.snft.de>
2025-05-26 15:57:18 +0200

    Disuse processing module in non-equidistant time step test
    The test "test_without_last_time_point" was actually a test of processing
    functionality. The test "test_numeric_index" cannot be placed in the same
    template anymore as overwrriting the index is no longer supported.

--- tests/test_non_equidistant_time_index.py
+++ tests/test_non_equidistant_time_index.py
@@ -12,14 +12,64 @@ import random
 import pandas as pd
 import pytest
 
+from oemof import solph
 from oemof.solph import EnergySystem
 from oemof.solph import Investment
 from oemof.solph import Model
 from oemof.solph import buses
 from oemof.solph import components as cmp
 from oemof.solph import flows
-from oemof.solph import processing
 
+def _setup_energy_system(dtindex, timeincrement=None):
+    es = EnergySystem(
+        timeindex=dtindex,
+        timeincrement=timeincrement,
+        infer_last_interval=False,
+    )
+
+    # BUSSES
+    b_el1 = buses.Bus(label="b_el1")
+    b_diesel = buses.Bus(label="b_diesel", balanced=False)
+    es.add(b_el1, b_diesel)
+
+    # TEST DIESEL:
+    dg = cmp.Converter(
+        label="diesel_generator",
+        inputs={b_diesel: flows.Flow(variable_costs=2)},
+        outputs={
+            b_el1: flows.Flow(
+                variable_costs=1, nominal_capacity=Investment(ep_costs=500)
+            )
+        },
+        conversion_factors={b_el1: 0.5},
+    )
+
+    batt = cmp.GenericStorage(
+        label="storage",
+        nominal_capacity=1000,
+        inputs={b_el1: flows.Flow(variable_costs=3)},
+        outputs={b_el1: flows.Flow(variable_costs=2.5)},
+        loss_rate=0.00,
+        invest_relation_input_capacity=1 / 6,
+        invest_relation_output_capacity=1 / 6,
+        inflow_conversion_factor=1,
+        outflow_conversion_factor=0.9,
+    )
+
+    random.seed(1)
+    demand_values = random.sample(range(40, 120), 72)
+    demand = cmp.Sink(
+        label="demand_el",
+        inputs={
+            b_el1: flows.Flow(
+                nominal_capacity=1,
+                fix=demand_values,
+            )
+        },
+    )
+    es.add(dg, batt, demand)
+
+    return es
 
 class TestParameterResult:
     @classmethod
@@ -27,157 +77,68 @@ class TestParameterResult:
         dtindex1 = pd.date_range("1/1/2012", periods=24, freq="h")
         dtindex2 = pd.date_range("1/2/2012", periods=49, freq="30min")
         dtindex = dtindex1.union(dtindex2)
-        es = EnergySystem(timeindex=dtindex, infer_last_interval=False)
-
-        # BUSSES
-        b_el1 = buses.Bus(label="b_el1")
-        b_diesel = buses.Bus(label="b_diesel", balanced=False)
-        es.add(b_el1, b_diesel)
-
-        # TEST DIESEL:
-        dg = cmp.Converter(
-            label="diesel_generator",
-            inputs={b_diesel: flows.Flow(variable_costs=2)},
-            outputs={
-                b_el1: flows.Flow(
-                    variable_costs=1, nominal_capacity=Investment(ep_costs=500)
-                )
-            },
-            conversion_factors={b_el1: 0.5},
-        )
-
-        batt = cmp.GenericStorage(
-            label="storage",
-            nominal_capacity=1000,
-            inputs={b_el1: flows.Flow(variable_costs=3)},
-            outputs={b_el1: flows.Flow(variable_costs=2.5)},
-            loss_rate=0.00,
-            invest_relation_input_capacity=1 / 6,
-            invest_relation_output_capacity=1 / 6,
-            inflow_conversion_factor=1,
-            outflow_conversion_factor=0.9,
-        )
-
-        random.seed(1)
-        demand_values = random.sample(range(40, 120), 72)
-        demand = cmp.Sink(
-            label="demand_el",
-            inputs={
-                b_el1: flows.Flow(
-                    nominal_capacity=1,
-                    fix=demand_values,
-                )
-            },
-        )
-        es.add(dg, batt, demand)
+        es = _setup_energy_system(dtindex)
         model = Model(es)
         model.receive_duals()
         model.solve()
-        results = processing.results(model, remove_last_time_point=False)
-        cls.flows = {k: v for k, v in results.items() if k[1] is not None}
-        cls.comp = {k: v for k, v in results.items() if k[1] is None}
+        results = solph.Results(model)
+        cls.flows = results["flow"]
+        cls.storage_content = results["storage_content"]
         cls.es = es
         cls.model = model
 
     def test_timesteps_timeincrements_with_storage_charging(self):
-        storage_content = [
-            v["sequences"]["storage_content"]
-            for k, v in self.comp.items()
-            if k[0].label == "storage"
-        ][0]
-        assert storage_content.iloc[0] == storage_content.iloc[-1]
 
-        charge = [
-            v["sequences"]["flow"]
-            for k, v in self.flows.items()
-            if k[1].label == "storage"
-        ][0]
+        assert (
+            self.storage_content.iloc[0] == self.storage_content.iloc[-1]
+        ).all()
+
+        charge = self.flows[(self.es.node["b_el1"], self.es.node["storage"])]
         # Calculate the next storage content and verify it with the storage
         # content of the results (charging).
         # Charging - timestep (ts) with its timeincrement (ti)
         time = [(23, 1), (24, 0.5)]
         for ts, ti in time:
-            assert storage_content.iloc[ts] + charge.iloc[
+            content_manual = self.storage_content.iloc[ts, 0] + charge.iloc[
                 ts
-            ] * ti == pytest.approx(storage_content.iloc[ts + 1])
+            ] * ti
+            content_model = self.storage_content.iloc[ts + 1, 0]
+            assert content_manual == pytest.approx(content_model)
             assert self.es.timeincrement[ts] == ti
-        assert charge.isnull().any()
 
     def test_timesteps_timeincrements_with_storage_discharging(self):
-        storage_content = [
-            v["sequences"]["storage_content"]
-            for k, v in self.comp.items()
-            if k[0].label == "storage"
-        ][0]
         # Storage content at the last time point is equal to the content of
         # the first time point because the storage is balanced.
-        assert storage_content.iloc[0] == storage_content.iloc[-1]
+        assert (
+            self.storage_content.iloc[0] == self.storage_content.iloc[-1]
+        ).all()
 
-        discharge = [
-            v["sequences"]["flow"]
-            for k, v in self.flows.items()
-            if k[0].label == "storage"
-        ][0]
+        discharge = self.flows[
+            (self.es.node["storage"], self.es.node["b_el1"])
+        ]
 
         # Calculate the next storage content and verify it with the storage
         # content of the results (discharging).
         # Discharging - timestep (ts) with its timeincrement (ti)
         time = [(7, 1), (40, 0.5)]
+
         for ts, ti in time:
-            assert (
-                storage_content.iloc[ts]
+            content_manual = (
+                self.storage_content.iloc[ts, 0]
                 - (discharge.iloc[ts] + (discharge.iloc[ts] * 1 / 9)) * ti
-            ) == pytest.approx(storage_content.iloc[ts + 1])
+            )
+            content_model = self.storage_content.iloc[ts + 1, 0]
+            assert content_manual == pytest.approx(content_model)
             assert self.es.timeincrement[ts] == ti
 
     def test_timeincrements(self):
         assert self.es.timeincrement.sum() == 48
 
-    def test_without_last_time_point(self):
-        results = processing.results(self.model, remove_last_time_point=True)
-        flow = {k: v for k, v in results.items() if k[1] is not None}
-        comp = {k: v for k, v in results.items() if k[1] is None}
-        storage_content = [
-            v["sequences"]["storage_content"]
-            for k, v in comp.items()
-            if k[0].label == "storage"
-        ][0]
-        charge = [
-            v["sequences"]["flow"]
-            for k, v in flow.items()
-            if k[1].label == "storage"
-        ][0]
-        # The first and the last value are not the same because the last value
-        # of the storage is missing. Adding the charging of the last time step
-        # will result the final storage content, which is equal to the first
-        assert storage_content.iloc[0] != storage_content.iloc[-1]
-        assert (
-            storage_content.iloc[0]
-            == storage_content.iloc[-1] + charge.iloc[-1] / 2
-        )
-        assert not charge.isnull().any()
-        assert storage_content.index[0] == datetime.datetime(
-            2012, 1, 1, 0, 0, 0
-        )
-        assert charge.index[0] == datetime.datetime(2012, 1, 1, 0, 0, 0)
-        assert storage_content.index[-1] == datetime.datetime(
-            2012, 1, 2, 23, 30, 0
-        )
-        assert charge.index[-1] == datetime.datetime(2012, 1, 2, 23, 30, 0)
-
     def test_time_index_with_last_time_point(self):
-        storage_content = [
-            v["sequences"]["storage_content"]
-            for k, v in self.comp.items()
-            if k[0].label == "storage"
-        ][0]
+        storage_content = self.storage_content[self.es.node["storage"]]
         assert storage_content.iloc[0] == storage_content.iloc[-1]
 
-        charge = [
-            v["sequences"]["flow"]
-            for k, v in self.flows.items()
-            if k[1].label == "storage"
-        ][0]
+        charge = self.flows[(self.es.node["b_el1"], self.es.node["storage"])]
         assert storage_content.index[0] == datetime.datetime(
             2012, 1, 1, 0, 0, 0
         )
@@ -185,43 +146,35 @@ class TestParameterResult:
         assert storage_content.index[-1] == datetime.datetime(
             2012, 1, 3, 0, 0, 0
         )
-        assert charge.index[-1] == datetime.datetime(2012, 1, 3, 0, 0, 0)
+        assert charge.index[-1] == datetime.datetime(2012, 1, 2, 23, 30, 0)
 
-    def test_numeric_index(self):
-        self.es.timeindex = None
-        model = Model(self.es)
-        model.receive_duals()
-        model.solve()
-        results = processing.results(self.model)
-        flow = {k: v for k, v in results.items() if k[1] is not None}
-        diesel_generator_out = [
-            v["sequences"]["flow"]
-            for k, v in flow.items()
-            if k[0].label == "diesel_generator"
-        ][0]
-        assert diesel_generator_out.index[0] == 0
-        assert diesel_generator_out.index[-1] == 72
-        assert len(diesel_generator_out.index) == 73
 
-        storage_content = [
-            v["sequences"]["storage_content"]
-            for k, v in self.comp.items()
-            if k[0].label == "storage"
-        ][0]
-        assert storage_content.iloc[0] == storage_content.iloc[-1]
+def test_numeric_index():
+    es = _setup_energy_system(dtindex=None, timeincrement=24 * [1]+ 48 * [0.5])
 
-        charge = [
-            v["sequences"]["flow"]
-            for k, v in self.flows.items()
-            if k[1].label == "storage"
-        ][0]
-        # Calculate the next storage content and verify it with the storage
-        # content of the results (charging).
-        # Charging - timestep (ts) with its timeincrement (ti)
-        time = [(23, 1), (24, 0.5)]
-        for ts, ti in time:
-            assert (
-                storage_content.iloc[ts] + charge.iloc[ts] * ti
-            ) == pytest.approx(storage_content.iloc[ts + 1])
-            assert self.es.timeincrement[ts] == ti
-        assert charge.isnull().any()
+    model = Model(es)
+    model.receive_duals()
+    model.solve()
+    results = solph.Results(model)
+    flow = results["flow"]
+
+    assert flow.index[0] == 0
+    assert flow.index[-1] == pytest.approx(47.5)
+    assert len(flow) == 72
+
+    storage_content = results["storage_content"]
+    assert (storage_content.iloc[0] == storage_content.iloc[-1]).all()
+    assert len(storage_content) == 73
+
+    charge = flow[(es.node["b_el1"], es.node["storage"])]
+    # Calculate the next storage content and verify it with the storage
+    # content of the results (charging).
+    # Charging - timestep (ts) with its timeincrement (ti)
+    time = [(23, 1), (24, 0.5)]
+    for ts, ti in time:
+        content_manual = storage_content.iloc[ts, 0] + charge.iloc[
+            ts
+        ] * ti
+        content_model = storage_content.iloc[ts + 1, 0]
+        assert content_manual == pytest.approx(content_model)
+        assert es.timeincrement[ts] == ti
