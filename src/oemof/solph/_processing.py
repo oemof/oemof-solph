ae31e6016fb7daea26627fba07496f5c6281449e
Patrik Schönfeldt <patrik@salacia.snft.de>
2025-05-27 21:10:24 +0200

    Finalise compatible result re-sorter
    Compatibility is according to documentation and not fully tested.

--- src/oemof/solph/_processing.py
+++ src/oemof/solph/_processing.py
@@ -9,6 +9,9 @@ SPDX-License-Identifier: MIT
 """
 
 import pandas as pd
+import warnings
+
+from oemof.tools import debugging
 
 from ._models import Model
 from ._results import Results
@@ -17,8 +20,8 @@ from ._results import Results
 def results(
     model: Model,
     remove_last_time_point: bool = False,
-    scalars: list[str] | None = None,
-    sequences: list[str] | None = None,
+    scalar_data: list[str] | None = None,
+    sequence_data: list[str] | None = None,
 ):
     """Create a nested result dictionary from the result DataFrame
 
@@ -35,11 +38,10 @@ def results(
       (i.e. investments).
 
     Models with more than one time for investments are not supported.
-
-    Examples
-    --------
-    * *Standard model*: `results[idx]['scalars']`
-      and flows `results[n, n]['sequences']`.
+    In these models, investments are sequential data,
+    but with a second time imdex. As this is a compatibility layer,
+    we did not add support for this new feature.
+    Instead, use of the Results object is advised.
 
     Parameters
     ----------
@@ -54,37 +56,64 @@ def results(
         point will not be removed by default. In that case all interval
         variables will get one row with nan-values to have the same index
         for all variables.
+    scalar_data :  list[str]
+        List of variables to be treated as scalar data (see above).
+    sequence_data: list[str]
+        List of variables to be treated as sequential data (see above).
     """
 
     meta_result_keys = ["Solution", "Problem", "Solver"]
 
-    if scalars is None:
-        scalars = ["invest", "total"]
-    if sequences is None:
-        sequences = ["flow", "storage_content", "storage_losses"]
-
-    sorting = {
-        scalar_data: "scalars" for scalar_data in scalars
-    } | {
-        sequence_data: "sequences" for sequence_data in sequences
-    }
+    if scalar_data is None:
+        scalar_data = ["invest", "total"]
+    if sequence_data is None:
+        sequence_data = ["flow", "storage_content", "storage_losses"]
 
     result_dict = {}
-    result_object = Results(model)
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore", category=debugging.ExperimentalFeatureWarning
+        )
+        result_object = Results(model)
+
+    timeindex = model.es.timeindex
+
+    if remove_last_time_point:
+        timeindex = timeindex[:-1]
+
+    def _handle_scalar(data):
+        return data.iloc[0]
+
+    def _handle_sequence(data):
+        return data
 
     for result_key in result_object.keys():
         if result_key not in meta_result_keys:
+            if result_key in scalar_data:
+                result_type = "scalars"
+                data_handler = _handle_scalar
+            elif result_key in sequence_data:
+                result_type = "sequences"
+                data_handler = _handle_sequence
+            else:
+                warnings.warn(f"Unhandled data: {result_key}")
+                continue
+
             index = result_object[result_key].columns
             for item in index:
                 if isinstance(index, pd.MultiIndex):
                     node_tuple = item
                 else:
                     node_tuple = (item, None)
-                if item not in result_dict:
-                    result_dict[node_tuple] = {"scalars": {}, "sequences": {}}
+                if node_tuple not in result_dict:
+                    result_dict[node_tuple] = {
+                        "scalars": pd.Series(),
+                        "sequences": pd.DataFrame(index=timeindex)
+                    }
 
-                result_dict[node_tuple][sorting[result_key]][result_key] = (
-                    result_object[result_key][item]
-                )
+                data = result_object[result_key][item]
+                result_dict[node_tuple][result_type][
+                    result_key
+                ] = data_handler(data)
 
     return result_dict
