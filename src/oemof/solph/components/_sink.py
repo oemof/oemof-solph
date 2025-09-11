2de58dd4aa66f1cc110926f52a0da9b5be24cb8d
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2025-09-11 23:53:47 +0200

    Add parent_node argument for children class of Node class

--- src/oemof/solph/components/_sink.py
+++ src/oemof/solph/components/_sink.py
@@ -41,14 +41,14 @@ class Sink(Node):
 
     """
 
-    def __init__(self, label=None, *, inputs, custom_properties=None):
+    def __init__(self, label=None, *, inputs, parent_node=None, custom_properties=None):
         if inputs is None:
             inputs = {}
         if custom_properties is None:
             custom_properties = {}
 
         super().__init__(
-            label=label, inputs=inputs, custom_properties=custom_properties
+            label=label, inputs=inputs, parent_node=parent_node, custom_properties=custom_properties
         )
 
     def constraint_group(self):
