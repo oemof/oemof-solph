33a9f27ed58494d250fc9002c1075aec112e0c45
Patrik Schönfeldt <patrik@salacia.snft.de>
2025-11-04 20:36:41 +0100

    Delete double adds in subnetwork example
    Subnodes are now added automatically.

--- examples/subnetwork/subnetwork_example.py
+++ examples/subnetwork/subnetwork_example.py
@@ -130,7 +130,7 @@ def main(optimize=True):
         label=QualifiedLabel(("el_grid",)),
         outputs={el_bus: solph.Flow(variable_costs=0.3)},
     )
-    es.add(house, el_bus, el_source)
+    es.add(house, el_source)
 
     heat_demands = house.subnode(
         Node,
@@ -149,8 +149,8 @@ def main(optimize=True):
         local_name="d_sh",
         inputs={demand_bus_sh: solph.Flow(nominal_capacity=1, fix=[0.4, 2.1])},
     )
-    es.add(heat_demands)
-    hp = house.subnode(
+
+    house.subnode(
         HeatPump,
         local_name="hp",
         el_supply=el_bus,
@@ -159,7 +159,6 @@ def main(optimize=True):
         cpf=0.45,
         el_power_limit=3,
     )
-    es.add(hp)
 
     if optimize is False:
         return es
