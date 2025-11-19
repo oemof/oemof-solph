8136dd7d35c9ae84f3b8e2b142db2bf79305d3df
Patrik Schönfeldt <Patrik.Schoenfeldt@dlr.de>
2025-11-19 12:33:09 +0100

    Fix wrong kwarg in flow test

--- tests/test_flows/test_flow_class.py
+++ tests/test_flows/test_flow_class.py
@@ -24,7 +24,7 @@ def test_custom_properties():
         FutureWarning,
         match="For backward compatibility,",
     ):
-        flow2 = Flow(custom_properties={"attribute": 1})
+        flow2 = Flow(custom_attributes={"attribute": 1})
         assert flow2.attribute == 1
         assert flow2.custom_properties["attribute"] == 1
 
