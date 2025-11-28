28f8c1d55c2f963a3ca7fdc40bdb88012dbadbed
Patrik Schönfeldt <patrik.schoenfeldt@dlr.de>
2025-11-28 16:58:53 +0100

    Add data licensing information to download_input_data
    Added a print statement to inform about data licensing and source.

--- tutorials/advanced/time_index/shared.py
+++ tutorials/advanced/time_index/shared.py
@@ -11,5 +11,11 @@ def download_input_data():
     url_temperature = "https://oemof.org/wp-content/uploads/2025/11/temperature.csv"
     url_energy = "https://oemof.org/wp-content/uploads/2025/11/energy.csv"
 
+    print(
+        "Data is licensed under Creative Commons Attribution 4.0 International\n"
+        "from M. Schlemminger, T. Ohrdes, E. Schneider, and M. Knoop.\n"
+        "It is also available at doi: 10.5281/zenodo.5642902."
+    )
+    
     urlretrieve(url_temperature, Path(file_path, "temperature.csv"))
     urlretrieve(url_energy, Path(file_path, "energy.csv"))
