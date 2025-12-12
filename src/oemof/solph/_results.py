96ee2cf18d5b020568bd962762bcd210d38b8954
Stephan Günther <gnn.code@gmail.com>
2025-12-12 02:00:26 +0100

    Don't use `in` in `Results.__getattr__`
    Don't check for membership via `in` to test whether a computed attribute
    can be returned from `Results.__getattr__`. Just try item access
    directly, catch any `KeyError`s raised, and re-raise them as
    `AttributeErrors`.

--- src/oemof/solph/_results.py
+++ src/oemof/solph/_results.py
@@ -233,10 +233,11 @@ class Results:
         return self._model.es.timeindex
 
     def __getattr__(self, key: str) -> pd.DataFrame | ListContainer:
-        # maps to df
-        if key not in self:
+        try:
+            # maps to df
+            return self[key]
+        except KeyError:
             raise AttributeError(f"'{self}' has no attribute '{key}'")
-        return self[key]
 
     def __getitem__(self, key: str) -> pd.DataFrame | ListContainer:
         # backward-compatibility with returned results object from Pyomo
