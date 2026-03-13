20f19041083d900ba906e1c5b3321972d9517b3b
Uwe <uwe.krien@ifam.fraunhofer.de>
2026-03-13 16:50:27 +0100

    Revise example

--- examples/storage_soc_dependent_charging/soc_dependent_charging.py
+++ examples/storage_soc_dependent_charging/soc_dependent_charging.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """
-Test example for soc-dependent charging.
+Example showing the difference between constant charging and SOC-dependent
+charging of a storage.
+
+Dependencies:
+    * matplotlib
+    * oemof-tools
 
 Licence
 -------
@@ -10,83 +15,99 @@ Licence
 """
 
 import logging
+
+from matplotlib import pyplot as plt
 from oemof.tools import logger
-from oemof.network.graph import create_nx_graph
+
 from oemof import solph
 
 
-def main():
-    demand = [0] * (24 * 2) + [70] * (24 * 2)
-    volatile = [70] * (24 * 2) + [0] * (24 * 2)
-    number_timesteps = len(demand)
+def plotting(results):
+    fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+    axes = axes.flatten()
+    n = 0
+    for storage in results["storage_content"].sort_index(axis=1).columns:
+        storage = storage.label
+        s = len(results["flow"])
+        ax1 = (
+            results["flow"][
+                [c for c in results["flow"].columns if storage in str(c[1])]
+            ]
+            .reset_index(drop=True)
+            .plot(ax=axes[n], color="tab:green")
+        )
+        ax1.set_ylim(0, 10)
+        ax1.set_xlim(0, s)
+        ax2 = axes[n].twinx()
+        ax2.set_ylim(0, 100)
+        ax2.set_title(storage)
+        results["storage_content"][storage].reset_index(drop=True).plot(ax=ax2)
+        value = results["storage_content"][storage].max()
+        text = f"Final storage level: {value:.0f}%"
+        props = dict(boxstyle="round", facecolor="wheat", alpha=0.5)
+        axes[n].text(
+            0.25,
+            0.75,
+            text,
+            transform=axes[n].transAxes,
+            fontsize=10,
+            verticalalignment="top",
+            bbox=props,
+        )
 
-    logger.define_logging()
-    logging.info("Initialize the energy system")
-    date_time_index = solph.create_time_index(2012, number=number_timesteps)
+        lines1, labels1 = axes[n].get_legend_handles_labels()
+        lines2, labels2 = ax2.get_legend_handles_labels()
+        axes[n].legend(
+            lines1 + lines2,
+            ["Charging power (left)", "Storage Content (right)"],
+            loc="upper left",
+        )
+        n += 1
 
-    energysystem = solph.EnergySystem(
-        timeindex=date_time_index, infer_last_interval=False
-    )
-
-    logging.info("Create oemof objects")
-
-    # create electricity bus
-    bel = solph.Bus(label="electricity")
-
-    energysystem.add(bel)
-
-    excess = solph.components.Sink(
-        label="excess_bel", inputs={bel: solph.Flow(variable_costs=4)}
-    )
-
-    volatile = solph.components.Source(
-        label="volatile",
-        outputs={bel: solph.Flow(fix=volatile, nominal_capacity=1)},
-    )
-
-    shortage = solph.components.Source(
-        label="shortage",
-        outputs={bel: solph.Flow(variable_costs=999)},
-    )
-
-    # create simple sink object representing the electrical demand
-    demand = solph.components.Sink(
-        label="demand",
-        inputs={bel: solph.Flow(fix=demand, nominal_capacity=1)},
-    )
-
-    # create storage object representing a battery
-    storage = solph.components.GenericStorage(
-        label="storage",
-        inputs={bel: solph.Flow(35)},
-        outputs={bel: solph.Flow(40)},
-        nominal_capacity=1000,
-        initial_storage_level=0,
-        balanced=True,
-        constant_soc_until=0.4,
-        fraction_saturation_charging=0.2,
-    )
-
-    energysystem.add(excess, shortage, volatile, demand, storage)
-    # create_nx_graph(energysystem, filename="test.graphml")
-    logging.info("Optimise the energy system")
-
-    om = solph.Model(energysystem)
-
-    # if tee_switch is true solver messages will be displayed
-    logging.info("Solve the optimization problem")
-    om.solve(solver="cbc", solve_kwargs={"tee": True})
-
-    results = solph.Results(om)
-    from matplotlib import pyplot as plt
-
-    print(results["flow"].sum())
-    ax = results["flow"][
-        [c for c in results["flow"].columns if "storage" in str(c)]
-    ].plot()
-    results["storage_content"].div(10).plot(ax=ax)
+    for i in range(2, len(axes)):
+        axes[i].axis("off")
+    plt.tight_layout()
     plt.show()
 
 
+def optimise_storage():
+    """A minimal storage model, that just loads the storages."""
+    logger.define_logging()
+    logging.info("Initialize the energy system")
+
+    energysystem = solph.EnergySystem(
+        timeindex=solph.create_time_index(2012, number=20),
+        infer_last_interval=False,
+    )
+
+    logging.info("Create oemof objects")
+    bel = solph.Bus(label="electricity", balanced=False)
+    energysystem.add(bel)
+
+    storage = solph.components.GenericStorage(
+        label="Constant Charging Power",
+        inputs={bel: solph.Flow(10, variable_costs=-1, maximum=0.5)},
+        outputs={bel: solph.Flow(10, variable_costs=2)},
+        nominal_capacity=100,
+        balanced=False,
+    )
+    storage_new = solph.components.GenericStorage(
+        label="SOC-dependent Charging Power",
+        inputs={bel: solph.Flow(10, variable_costs=-1, maximum=0.5)},
+        outputs={bel: solph.Flow(10, variable_costs=2)},
+        nominal_capacity=100,
+        balanced=False,
+        constant_soc_until=0.2,
+        fraction_saturation_charging=0.3,
+    )
+    energysystem.add(storage, storage_new)
+
+    logging.info("Optimise the energy system")
+    om = solph.Model(energysystem)
+    om.solve(solver="cbc", solve_kwargs={"tee": False})
+
+    return solph.Results(om)
+
+
 if __name__ == "__main__":
-    main()
+    plotting(optimise_storage())
