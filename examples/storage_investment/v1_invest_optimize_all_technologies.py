d45844fe834b3465b0dd210bca8b4feb6c1d4797
Uwe <uwe.krien@ifam.fraunhofer.de>
2026-06-18 14:44:21 +0200

    Change solver name in simple example

--- examples/storage_investment/v1_invest_optimize_all_technologies.py
+++ examples/storage_investment/v1_invest_optimize_all_technologies.py
@@ -220,7 +220,7 @@ def main(optimize=True):
 
     # if tee_switch is true solver messages will be displayed
     logging.info("Solve the optimization problem")
-    om.solve(solver="cbc", solve_kwargs={"tee": True})
+    om.solve(solver="scip", solve_kwargs={"tee": True})
 
     ##########################################################################
     # Check and plot the results
