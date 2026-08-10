39699fde227b43998dad5e7684fec8713ee82c9a
Patrik Schönfeldt <patrik@proxima.snft.de>
2026-08-10 21:58:21 +0200

    Add capacity period example stub

---
+++ examples/capacity_periods/capacity_periods.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""Example for solph model with mutiple capacity periods.
+
+SPDX-FileCopyrightText: Patrik Schönfeldt
+
+SPDX-License-Identifier: MIT
+"""
+
+import numpy as np
+
+from oemof import solph
+
+def main():
+    es = solph.EnergySystem(
+        timeindex=np.linspace(start=0, stop=12, num=13),
+        investment_times=[0, 4, 12],
+    )
+
+    bus = solph.Bus(label="bus")
+
+    source = solph.components.Source(
+        label="source",
+        outputs={
+            bus: solph.Flow(
+                nominal_capacity=solph.Investment(maximum=4),
+            ),
+        },
+    )
+    sink = solph.components.Sink(
+        label="sink",
+        inputs={
+            bus: solph.Flow(
+                nominal_capacity=[4, 3],
+                variable_costs=-2,
+            ),
+        },
+    )
+    es.add(bus, sink, source)
+
+    model = solph.Model(es)
+
+    results = model.solve()
+
+    print(results["flow"])
+    print(results["objective"])
+
+
+if __name__ == "__main__":
+    main()
