2a23a993b6ee5b0344d50d9fca8ad700ed49ee98
Patrik Schönfeldt <patrik@proxima.snft.de>
2026-08-10 21:56:07 +0200

    Remove discounting from InvestmentFlowBlock

--- src/oemof/solph/flows/_investment_flow_block.py
+++ src/oemof/solph/flows/_investment_flow_block.py
@@ -710,71 +710,23 @@ class InvestmentFlowBlock(ScalarBlock):
                     )
 
         else:
-            interest = 0.02
-            msg = (
-                "You did not specify an interest rate.\n"
-                "It will be set to {}."
-            )
             for i, o in self.CONVEX_INVESTFLOWS:
-                lifetime = m.flows[i, o].investment.lifetime
-                warn(
-                    msg.format(interest),
-                    debugging.SuspiciousUsageWarning,
-                )
-
                 for p in m.CAPACITY_PERIODS:
 
-                    annuity = economics.annuity(
-                        capex=m.flows[i, o].investment.ep_costs[p],
-                        n=lifetime,
-                        wacc=interest,
-                    )
-                    duration = min(
-                        m.es.end_year_of_optimization
-                        - m.es.capacity_period_years[p],
-                        lifetime,
-                    )
-                    present_value_factor_remaining = 1 / economics.annuity(
-                        capex=1, n=duration, wacc=interest
-                    )
                     investment_costs_increment = (
                         self.invest[i, o, p]
-                        * annuity
-                        * present_value_factor_remaining
+                        * m.flows[i, o].investment.ep_costs[p]
                     )
                     investment_costs += investment_costs_increment
                     period_investment_costs[p] += investment_costs_increment
 
             for i, o in self.NON_CONVEX_INVESTFLOWS:
-                lifetime = m.flows[i, o].investment.lifetime
-                warn(
-                    msg.format(interest),
-                    debugging.SuspiciousUsageWarning,
-                )
                 for p in m.CAPACITY_PERIODS:
-                    annuity = economics.annuity(
-                        capex=m.flows[i, o].investment.ep_costs[p],
-                        n=lifetime,
-                        wacc=interest,
-                    )
-                    duration = min(
-                        m.es.end_year_of_optimization
-                        - m.es.capacity_period_years[p],
-                        lifetime,
-                    )
-                    present_value_factor_remaining = 1 / economics.annuity(
-                        capex=1, n=duration, wacc=interest
-                    )
-                    annuity_offset = economics.annuity(
-                        capex=m.flows[i, o].investment.offset[p],
-                        n=lifetime,
-                        wacc=interest,
-                    )
+
                     investment_costs_increment = (
                         self.invest[i, o, p]
-                        * annuity
-                        * present_value_factor_remaining
-                        + self.invest_status[i, o, p] * annuity_offset
+                        * m.flows[i, o].investment.ep_costs[p]
+                        + self.invest_status[i, o, p] * m.flows[i, o].investment.offset[p]
                     )
                     investment_costs += investment_costs_increment
                     period_investment_costs[p] += investment_costs_increment
